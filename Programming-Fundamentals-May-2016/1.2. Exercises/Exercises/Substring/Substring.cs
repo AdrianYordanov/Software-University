b0c6830af40a465b0c9c1df958a5da4257325892
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-05-26 21:35:44 +0300

    7. Debugging Exercise: Substring
    Judge - 100/100.

---
+++ Programming-Fundamentals-May-2016/1.2. Exercises/Exercises/Substring/Substring.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class Substring
+{
+	public static void Main()
+	{
+		string text = Console.ReadLine();
+		int jump = int.Parse(Console.ReadLine());
+
+		const char Search = 'p';
+		bool hasMatch = false;
+
+		for (int i = 0; i < text.Length; i++)
+		{
+            char currentCharacter = text[i];
+			if (currentCharacter == Search)
+			{
+                hasMatch = true;
+                int endIndex = i + jump;
+                string matchedString = string.Empty;
+
+				if (endIndex < text.Length)
+				{
+                    matchedString = text.Substring(i, jump + 1);
+				}
+                else
+                {
+                    matchedString = text.Substring(i, text.Length - i);
+                }
+
+                Console.WriteLine(matchedString);
+				i = endIndex;
+			}
+		}
+
+		if (!hasMatch)
+		{
+			Console.WriteLine("no");
+		}
+	}
+}
