d0144bd8a0dc27552e23f55a3d0b1a10397089af
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-05-29 13:03:30 +0300

    08. Day of Week
    Judge - 100/100.

---
+++ Programming-Fundamentals-May-2016/2.0. Data-Types-and-Methods/Lab/Day-Of-Week/DayOfWeek.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class DayOfWeek
+{
+    static void Main()
+    {
+        byte dayNumber = byte.Parse(Console.ReadLine());
+        string message = string.Empty;
+
+        switch (dayNumber)
+        {
+            case 1: message = "Monday"; break;
+            case 2: message = "Tuesday"; break;
+            case 3: message = "Wednesday"; break;
+            case 4: message = "Thursday"; break;
+            case 5: message = "Friday"; break;
+            case 6: message = "Saturday"; break;
+            case 7: message = "Sunday"; break;
+            default: message = "Error"; break;
+        }
+
+        Console.WriteLine(message);
+    }
+}
