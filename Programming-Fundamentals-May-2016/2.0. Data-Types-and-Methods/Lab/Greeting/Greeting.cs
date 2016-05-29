b21e296c0dfba8ee1f74985e5a2ed9c05a741b65
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-05-29 12:57:57 +0300

    07. Greeting
    Judge - 100/100.

---
+++ Programming-Fundamentals-May-2016/2.0. Data-Types-and-Methods/Lab/Greeting/Greeting.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Greeting
+{
+    static void Main()
+    {
+        string firstName = Console.ReadLine();
+        string lastName = Console.ReadLine();
+        byte age = byte.Parse(Console.ReadLine());
+
+        Console.WriteLine("Hello, {0} {1}. You are {2} years old.", firstName, lastName, age);
+        //Interpolated string -> Console.WriteLine($"Hello, {firstName} {lastName}. You are {age} years old.");
+    }
+}
