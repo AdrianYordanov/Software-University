d3289b130b1e8e67390d865e0c5c136617d4d0a0
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-05-29 12:41:59 +0300

    05. Special Numbers
    Judge - 100/100.

---
+++ Programming-Fundamentals-May-2016/2.0. Data-Types-and-Methods/Lab/Special-Numbers/SpecialNumbers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class SpecialNumbers
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= n; i++)
+        {
+            bool isSpecial = SpecialNumbersCheck(i);
+            Console.WriteLine("{0} -> {1}", i, isSpecial);
+        }
+    }
+
+    private static bool SpecialNumbersCheck(int number)
+    {
+        int result = SumOfDigits(number);
+
+        if (result == 5 || result == 7 || result == 11)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private static int SumOfDigits(int number)
+    {
+        int result = 0;
+
+        while (number > 0)
+        {
+            result += number % 10;
+            number /= 10;
+        }
+
+        return result;
+    }
+}
