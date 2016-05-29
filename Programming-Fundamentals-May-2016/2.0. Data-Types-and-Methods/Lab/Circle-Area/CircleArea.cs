5137b20b6c7f1c8a9a746dbe6ec86d7f0fd7cb83
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-05-29 11:58:23 +0300

    2. Circle Area (Precision 12)
    Judge - 100/100.

---
+++ Programming-Fundamentals-May-2016/2.0. Data-Types-and-Methods/Lab/Circle-Area/CircleArea.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class CircleArea
+{
+    static void Main()
+    {
+        double r = double.Parse(Console.ReadLine());
+        Console.WriteLine("{0:f12}", Math.PI * r * r);
+    }
+}
