bf97b86c619136c2b448eb2a2c92017954f68efd
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-05-31 01:10:13 +0300

    25. Master Numbers
    Judge - 100/100.

---
+++ Programming-Fundamentals-May-2016/2.1. Exercises/Exercises/Master-Numbers/MasterNumbers.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MasterNumbers
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= number; i++)
+        {
+            if(IsPalindrome(i) && (SumOfDigits(i) % 7 == 0) && ContainsEvenDigits(i))
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+
+    private static bool IsPalindrome(int number)
+    {
+        string numberToString = number.ToString();
+
+        for (int i = 0, j = numberToString.Length - 1; i < numberToString.Length; i++,--j)
+        {
+            if(!numberToString[i].Equals(numberToString[j]))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static byte SumOfDigits(int number)
+    {
+        byte sum = 0;
+
+        while (number > 0)
+        {
+            sum += (byte)(number % 10);
+            number /= 10;
+        }
+
+        return sum;
+    }
+
+    private static bool ContainsEvenDigits(int number)
+    {
+        while (number > 0)
+        {
+            int digit = number % 10;
+
+            if (digit % 2 == 0)
+            {
+                return true;
+            }
+
+            number /= 10;
+        }
+
+        return false;
+    }
+}
