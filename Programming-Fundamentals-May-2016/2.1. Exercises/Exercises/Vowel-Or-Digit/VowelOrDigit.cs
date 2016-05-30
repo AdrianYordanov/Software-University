7ee88a9fda6da666a68c500c767e9978015a43a4
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-05-30 19:54:24 +0300

    13. Vowel or Digit
    Judge - 100/100.

---
+++ Programming-Fundamentals-May-2016/2.1. Exercises/Exercises/Vowel-Or-Digit/VowelOrDigit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class VowelOrDigit
+{
+    static void Main()
+    {
+        char character = char.Parse(Console.ReadLine());
+        bool isVowel = "aeiouAEIOU".IndexOf(character) >= 0;
+        bool isDigit = char.IsDigit(character);
+
+        if (isVowel)
+        {
+            Console.WriteLine("vowel");
+        }
+        else
+        {
+            if (isDigit)
+            {
+                Console.WriteLine("digit");
+            }
+            else
+            {
+                Console.WriteLine("other");
+            }
+        }
+    }
+}
