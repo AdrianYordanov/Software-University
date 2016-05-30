4eed27ca93ebc842a3eab8e7aa4ecd82d674e789
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-05-30 20:01:38 +0300

    14. Integer to Hex and Binary
    Judge - 100/100.

---
+++ Programming-Fundamentals-May-2016/2.1. Exercises/Exercises/Integer-To-Hex-And-Binary/IntegerToHexAndBinary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class IntegerToHexAndBinary
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+        string hexadecimal = Convert
+            .ToString(number, 16)
+            .ToUpper();
+        string binary = Convert.ToString(number, 2);
+
+        Console.WriteLine(hexadecimal);
+        Console.WriteLine(binary);
+    }
+}
