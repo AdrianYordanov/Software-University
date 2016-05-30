a6f7f7da58942d6c551ab0cbda8957294b9d901d
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-05-30 20:59:13 +0300

    16. Print Part Of ASCII Table
    Judge - 100/100.

---
+++ Programming-Fundamentals-May-2016/2.1. Exercises/Exercises/Print-Part-Of-Ascii/PrintPartOfAscii.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class PrintPartOfAscii
+{
+    static void Main()
+    {
+        int start = int.Parse(Console.ReadLine());
+        int end = int.Parse(Console.ReadLine());
+
+        for (int i = start; i <= end; i++)
+        {
+            Console.Write((char)i + " ");
+        }
+    }
+}
