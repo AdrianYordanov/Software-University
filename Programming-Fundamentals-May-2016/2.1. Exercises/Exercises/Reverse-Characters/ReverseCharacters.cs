42ead17ac39361ec4729caa7b1d52612035493a3
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-05-30 18:18:36 +0300

    09. Reversed chars
    Judge - 100/100.

---
+++ Programming-Fundamentals-May-2016/2.1. Exercises/Exercises/Reverse-Characters/ReverseCharacters.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class ReverseCharacters
+{
+    static void Main()
+    {
+        char ch1 = char.Parse(Console.ReadLine());
+        char ch2 = char.Parse(Console.ReadLine());
+        char ch3 = char.Parse(Console.ReadLine());
+
+        string result = string.Empty + ch3 + ch2 + ch1;
+        Console.WriteLine(result);
+    }
+}
