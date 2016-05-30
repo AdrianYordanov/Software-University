64025272b582f4dca8538b631f7bbaa25bdca57c
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-05-30 23:32:36 +0300

    23. Prime Checker
    Judge - 100/100.

---
+++ Programming-Fundamentals-May-2016/2.1. Exercises/Exercises/Prime-Checker/PrimeChecker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class PrimeChecker
+{
+    static void Main()
+    {
+        long number = long.Parse(Console.ReadLine());
+        Console.WriteLine(IsPrime(number));
+    }
+
+    private static bool IsPrime(long number)
+    {
+        if (number < 2) return false;
+        if (number % 2 == 0) return (number == 2);
+        long root = (long)Math.Sqrt((double)number);
+        for (long i = 3; i <= root; i += 2)
+        {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
+}
