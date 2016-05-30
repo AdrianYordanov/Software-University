241a12acf6530e877516a2fae3bbf4eba98c491e
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-05-30 12:35:32 +0300

    02. Practice Floating Points
    Judge - 100/100.

---
+++ Programming-Fundamentals-May-2016/2.1. Exercises/Exercises/Practice-Floating-Point-Numbers/PracticeFloatingPointNumbers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class PracticeFloatingPointNumbers
+{
+    static void Main()
+    {
+        decimal num1 = 3.141592653589793238m;
+        double num2 = 1.60217657d;
+        decimal num3 = 7.8184261974584555216535342341m;
+
+        Console.WriteLine(num1);
+        Console.WriteLine(num2);
+        Console.WriteLine(num3);
+    }
+}
