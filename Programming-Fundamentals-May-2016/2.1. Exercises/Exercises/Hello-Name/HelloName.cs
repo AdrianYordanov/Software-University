5499d0949d82d61fb2e7f3f6e7e4ae5b9f0aca48
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-05-30 22:24:50 +0300

    18. Hello, Name!
    Judge - 100/100.

---
+++ Programming-Fundamentals-May-2016/2.1. Exercises/Exercises/Hello-Name/HelloName.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class HelloName
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        PrintName(input);
+    }
+
+    private static void PrintName(string name)
+    {
+        Console.WriteLine("Hello, {0}!", name);
+    }
+}
