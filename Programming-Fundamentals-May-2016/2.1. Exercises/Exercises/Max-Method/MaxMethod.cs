0980c2d6a770351a761a00ce1e825e19ad27342b
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-05-30 22:31:12 +0300

    19. Max Method
    Judge - 100/100.

---
+++ Programming-Fundamentals-May-2016/2.1. Exercises/Exercises/Max-Method/MaxMethod.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class MaxMethod
+{
+    static void Main()
+    {
+        int first = int.Parse(Console.ReadLine());
+        int second = int.Parse(Console.ReadLine());
+        int third = int.Parse(Console.ReadLine());
+
+        int bigger = GetMax(first, second);
+        int biggest = GetMax(bigger, third);
+
+        Console.WriteLine(biggest);
+    }
+
+    private static int GetMax(int a, int b)
+    {
+        if (a > b)
+        {
+            return a;
+        }
+        else
+        {
+            return b;
+        }
+    }
+}
