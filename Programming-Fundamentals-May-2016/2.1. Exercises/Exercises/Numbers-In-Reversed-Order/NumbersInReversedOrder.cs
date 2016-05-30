1b21669555a0b92b968570b463bce5cd0053aea3
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-05-30 22:55:54 +0300

    21. Numbers In Reversed Order
    Judge - 100/100.

---
+++ Programming-Fundamentals-May-2016/2.1. Exercises/Exercises/Numbers-In-Reversed-Order/NumbersInReversedOrder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class NumbersInReversedOrder
+{
+    static void Main()
+    {
+        double inputNumber = double.Parse(Console.ReadLine());
+        PrintReversedNumber(inputNumber);
+    }
+
+    private static void PrintReversedNumber(double number)
+    {
+        char[] charArray = number
+            .ToString()
+            .ToCharArray();
+        Array.Reverse(charArray);
+        string reversedNumber = new string(charArray);
+        Console.WriteLine(reversedNumber);
+    }
+}
