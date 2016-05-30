be65ed28155ba3413c75f0e78932e1c580f1bff3
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-05-30 19:32:29 +0300

    11. Convert Speed Units
    Judge - 100/100.

---
+++ Programming-Fundamentals-May-2016/2.1. Exercises/Exercises/Convert-Speed-Units/ConvertSpeedUnits.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class ConvertSpeedUnits
+{
+    static void Main()
+    {
+        float distanceInMeters = float.Parse(Console.ReadLine());
+        float distanceInKilometers = distanceInMeters / 1000;
+        float distanceInMiles = distanceInMeters / 1609;
+
+        uint hours = uint.Parse(Console.ReadLine());
+        uint minutes = uint.Parse(Console.ReadLine());
+        uint seconds = uint.Parse(Console.ReadLine());
+        uint totalSeconds = seconds + (( minutes + (hours * 60)) * 60);
+        float totalHours = (float)totalSeconds / 60 / 60;
+
+        Console.WriteLine("{0}", distanceInMeters / totalSeconds);
+        Console.WriteLine("{0}", distanceInKilometers / totalHours);
+        Console.WriteLine("{0}", distanceInMiles / totalHours);
+    }
+}
