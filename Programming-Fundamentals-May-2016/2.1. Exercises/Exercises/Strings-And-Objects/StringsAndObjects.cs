69f8c7523370bdb0532eb24c40574061abb4d407
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-05-30 17:53:05 +0300

    06. Strings And Objects
    Judge - 100/100.

---
+++ Programming-Fundamentals-May-2016/2.1. Exercises/Exercises/Strings-And-Objects/StringsAndObjects.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class StringsAndObjects
+{
+    static void Main()
+    {
+        string firstString = "Hello";
+        string secondString = "World";
+        object concatenated = firstString + " " + secondString;
+        string result = (string)concatenated;
+        Console.WriteLine(result);
+    }
+}
