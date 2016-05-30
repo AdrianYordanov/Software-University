5ba2fa87c0585c7c302b310a7ab6c1d50202079e
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-05-30 17:38:15 +0300

    03. Practice Chars And Strings
    Judge - 100/100.

---
+++ Programming-Fundamentals-May-2016/2.1. Exercises/Exercises/Practice-Characters-And-Strings/PracticeCharactersAndStrings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class PracticeCharactersAndStrings
+{
+    static void Main()
+    {
+        char ch1 = 'B';
+        char ch2 = 'y';
+        char ch3 = 'e';
+        string str1 = "Software University";
+        string str2 = "I love programming";
+
+        Console.WriteLine(str1);
+        Console.WriteLine(ch1);
+        Console.WriteLine(ch2);
+        Console.WriteLine(ch3);
+        Console.WriteLine(str2);
+    }
+}
