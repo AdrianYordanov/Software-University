72b3df20a1a892892a8a9933bd5fb1f64fe42c0a
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-05-30 17:44:08 +0300

    04. Variable in Hex Format
    Judge - 100/100.

---
+++ Programming-Fundamentals-May-2016/2.1. Exercises/Exercises/Variable-In-Hexadecimal-Format/VariableInHexadecimalFormat.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class VariableInHexadecimalFormat
+{
+    static void Main()
+    {
+        string hexadecimalFormat = Console.ReadLine();
+        int decimalFormat = Convert.ToInt32(hexadecimalFormat, 16);
+
+        Console.WriteLine(decimalFormat);
+    }
+}
