f94cb77f8aab4a53b45947838c33d8f398d71db1
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-05-30 22:17:40 +0300

    17. Different Integers Size
    Judge - 100/100.

---
+++ Programming-Fundamentals-May-2016/2.1. Exercises/Exercises/Different-Integers-Sizes/DifferentIntegersSizes.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+class DifferentIntegersSizes
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        object test;
+        long anotherTest = 0;
+
+        if (long.TryParse(input, out anotherTest))
+        {
+            Console.WriteLine("{0} can fit in:", input);
+        }
+        else
+        {
+            Console.WriteLine("{0} can't fit in any type", input);
+        }
+
+        try
+        {
+            test = sbyte.Parse(input);
+            Console.WriteLine("* sbyte");
+        }
+        catch (Exception) { ;}
+
+        try
+        {
+            test = byte.Parse(input);
+            Console.WriteLine("* byte");
+        }
+        catch (Exception) { ;}
+
+        try
+        {
+            test = short.Parse(input);
+            Console.WriteLine("* short");
+        }
+        catch (Exception) { ;}
+
+        try
+        {
+            test = ushort.Parse(input);
+            Console.WriteLine("* ushort");
+        }
+        catch (Exception) { ;}
+
+        try
+        {
+            test = int.Parse(input);
+            Console.WriteLine("* int");
+        }
+        catch (Exception) { ;}
+
+        try
+        {
+            test = uint.Parse(input);
+            Console.WriteLine("* uint");
+        }
+        catch (Exception) { ;}
+
+        try
+        {
+            test = long.Parse(input);
+            Console.WriteLine("* long");
+        }
+        catch (Exception) { ;}
+    }
+}
