d1b2dc1f625be9dd6a8e66094c8f4859b67762f2
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-06-03 00:51:24 +0300

    01. Day of Week
    Judge - 100/100.

---
+++ Programming-Fundamentals-May-2016/3.0. Programming-Fundamentals-Arrays/Lab/Day-Of-Week/DayOfWeek.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class DayOfWeek
+{
+    static void Main()
+    {
+        var days = new string[] { 
+                       "Monday", 
+                       "Tuesday", 
+                       "Wednesday", 
+                       "Thursday", 
+                       "Friday", 
+                       "Saturday", 
+                       "Sunday"
+                   };
+
+        var chosenDay = int.Parse(Console.ReadLine());
+
+        if (chosenDay >= 1 && chosenDay <= 7)
+        {
+            Console.WriteLine(days[chosenDay - 1]);
+        }
+        else
+        {
+            Console.WriteLine("Invalid Day!");
+        }
+    }
+}
