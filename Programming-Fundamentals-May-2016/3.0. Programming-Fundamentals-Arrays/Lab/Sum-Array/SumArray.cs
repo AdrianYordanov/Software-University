f6ed18842dc0ab011340f51efcc4d8d36cfecefb
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-06-04 01:03:30 +0300

    08. Sum Arrays
    Judge - 100/100.

---
+++ Programming-Fundamentals-May-2016/3.0. Programming-Fundamentals-Arrays/Lab/Sum-Array/SumArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+class SumArray
+{
+    static void Main()
+    {
+        var firstArray = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse)
+            .ToArray();
+        var secondArray = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse)
+            .ToArray();
+
+        var biggerLength = Math.Max(firstArray.Length, secondArray.Length);
+        var thirdArray = new long[biggerLength];
+
+        for (int i = 0; i < thirdArray.Length; i++)
+        {
+           thirdArray[i] = firstArray[i % firstArray.Length] +
+                secondArray[i % secondArray.Length];
+        }
+
+        Console.WriteLine(string.Join(" ", thirdArray));
+    }
+}
