a001be63bdbec7aca88ccdf9e0b422a13de29d36
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-06-04 01:13:25 +0300

    09. Condense Array to Number
    Judge - 100/100.

---
+++ Programming-Fundamentals-May-2016/3.0. Programming-Fundamentals-Arrays/Lab/Condense-Array-To-Number/CondenseArrayToNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+class CondenseArrayToNumber
+{
+    static void Main()
+    {
+        var array = Console.ReadLine()
+            .Split(' ')
+            .Select(long.Parse)
+            .ToArray();
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            for (int j = 0; j < array.Length - 1 - i; j++)
+            {
+                array[j] += array[j + 1];
+            }
+        }
+
+        Console.WriteLine(array[0]);
+    }
+}
