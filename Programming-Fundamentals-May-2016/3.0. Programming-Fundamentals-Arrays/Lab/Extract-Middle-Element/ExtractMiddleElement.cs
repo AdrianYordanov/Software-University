318fc52c7922d7c900aaa67b49042131af17d59a
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-06-04 01:27:07 +0300

    10. Extract Middle Elements
    Judge - 100/100.

---
+++ Programming-Fundamentals-May-2016/3.0. Programming-Fundamentals-Arrays/Lab/Extract-Middle-Element/ExtractMiddleElement.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+class ExtractMiddleElement
+{
+    static void Main()
+    {
+        var array = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse)
+            .ToArray();
+
+        PrintMiddleElements(array);
+    }
+
+    private static void PrintMiddleElements(int[] array)
+    {
+        if (array.Length == 1)
+        {
+            Console.WriteLine("{{ {0} }}", array[0]);
+        }
+        else
+        {
+            var middle = array.Length / 2;
+
+            if(array.Length % 2 == 0)
+            {
+                Console.WriteLine("{{ {0}, {1} }}", array[middle - 1], array[middle]);
+            }
+            else
+            {
+                Console.WriteLine("{{ {0}, {1}, {2} }}", array[middle - 1], array[middle], array[middle+1]);
+            }
+        }
+    }
+}
