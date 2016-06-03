2015ca39b1e8c94f017e3daa0e7470c8338369e3
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-06-04 00:43:23 +0300

    06. Rounding Numbers
    Judge - 100/100.

---
+++ Programming-Fundamentals-May-2016/3.0. Programming-Fundamentals-Arrays/Lab/Rounding-Away-From-Zero/RoundingAwayFromZero.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+class RoundingAwayFromZero
+{
+    static void Main()
+    {
+        var array = Console.ReadLine()
+            .Split(' ')
+            .Select(double.Parse)
+            .ToArray();
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            var rounded = Math.Round(array[i], MidpointRounding.AwayFromZero);
+            Console.WriteLine("{0} => {1}", array[i], rounded);
+        }
+    }
+}
