2d7f3d66e4fe57d31d11701775b57c51f3c500e8
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-06-09 00:01:36 +0300

    02. Rotate and Sum
    Judge - 100/100.

---
+++ Programming-Fundamentals-May-2016/3.1. Exercises/Exercises/Rotate-And-Sum/RotateAndSum.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+class RotateAndSum
+{
+    static void Main()
+    {
+        var array = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse)
+            .ToArray();
+
+        var k = int.Parse(Console.ReadLine());
+        var sum = new int[array.Length];
+
+        for (int rotations = 1; rotations <= k; rotations++)
+        {
+            Rotate(array);
+
+            for (int i = 0; i < sum.Length; i++)
+            {
+                sum[i] += array[i];
+            }
+        }
+
+        Console.WriteLine(string.Join(" ", sum));
+    }
+
+    private static void Rotate(int[] array)
+    {
+        if (array != null && array.Length > 0)
+        {
+            int temp = array[0];
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                int nestedTemp = array[i];
+                array[i] = temp;
+                temp = nestedTemp;
+            }
+
+            array[0] = temp;
+        }
+    }
+}
