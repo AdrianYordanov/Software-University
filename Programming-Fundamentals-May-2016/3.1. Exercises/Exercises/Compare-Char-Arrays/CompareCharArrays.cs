f92aaae18d3575fcf5315dd1ef28d0fef7948eb5
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-06-09 21:48:28 +0300

    05. Compare Char Arrays
    Judge - 100/100.

---
+++ Programming-Fundamentals-May-2016/3.1. Exercises/Exercises/Compare-Char-Arrays/CompareCharArrays.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+
+class CompareCharArrays
+{
+    static void Main()
+    {
+        var firstLine = Console.ReadLine()
+            .Split(' ')
+            .Select(char.Parse)
+            .ToArray();
+        var secondLine = Console.ReadLine()
+            .Split(' ')
+            .Select(char.Parse)
+            .ToArray();
+
+        bool equal = true;
+        int minLength = Math.Min(firstLine.Length, secondLine.Length);
+
+        for (int i = 0; i < minLength; i++)
+        {
+            if (firstLine[i].Equals(secondLine[i]))
+            {
+                continue;
+            }
+
+            equal = false;
+
+            if (firstLine[i].CompareTo(secondLine[i]) > 0)
+            {
+                Console.WriteLine(string.Join("", secondLine));
+                Console.WriteLine(string.Join("", firstLine));
+                break;
+            }
+            else
+            {
+                Console.WriteLine(string.Join("", firstLine));
+                Console.WriteLine(string.Join("", secondLine));
+                break;
+            }
+        }
+
+        if(equal)
+        {
+            if(firstLine.Length > secondLine.Length)
+            {
+                Console.WriteLine(string.Join("", secondLine));
+                Console.WriteLine(string.Join("", firstLine));
+            }
+            else
+            {
+                Console.WriteLine(string.Join("", firstLine));
+                Console.WriteLine(string.Join("", secondLine));
+            }
+        }
+    }
+}
