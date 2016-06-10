7ed363ae0e5ad70446b146821b1b77334b577d7c
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-06-10 19:22:54 +0300

    08. Most Frequent Number
    Judge - 100/100.

---
+++ Programming-Fundamentals-May-2016/3.1. Exercises/Exercises/Most-Frequent-Number/MostFrequentNumber.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+    class MostFrequentNumber
+    {
+        static void Main()
+        {
+            var array = Console.ReadLine()
+                .Split(' ')
+                .Select(ushort.Parse)
+                .ToArray();
+            var arrayElementIndicate = new bool[array.Length];
+
+            for (int i = 0; i < arrayElementIndicate.Length; i++)
+            {
+                arrayElementIndicate[i] = true;
+            }
+
+            int bestIndex = 0, 
+                bestCount = 1;
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (!arrayElementIndicate[i])
+                {
+                    continue;
+                }
+
+                int currentCount = 1;
+
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if(!arrayElementIndicate[i])
+                    {
+                        continue;
+                    }
+
+                    if(array[i].Equals(array[j]))
+                    {
+                        ++currentCount;
+                        arrayElementIndicate[j] = false;
+                    }
+                }
+
+                if(currentCount > bestCount)
+                {
+                    bestCount = currentCount;
+                    bestIndex = i;
+                }
+            }
+
+            Console.WriteLine(array[bestIndex]);
+        }
+    }
