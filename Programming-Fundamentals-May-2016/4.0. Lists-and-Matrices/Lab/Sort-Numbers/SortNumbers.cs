c84b6bb87638ca4c231ae817e46a558fd19faa88
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-06-14 22:24:57 +0300

    05. Sort Numbers
    Judge - 100/100.

---
+++ Programming-Fundamentals-May-2016/4.0. Lists-and-Matrices/Lab/Sort-Numbers/SortNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class SortNumbers
+{
+    static void Main()
+    {
+        var nums = Console.ReadLine()
+            .Split(' ')
+            .Select(decimal.Parse)
+            .ToList();
+
+        nums.Sort();
+
+        Console.WriteLine(string.Join(" <= ", nums));
+    }
+}
