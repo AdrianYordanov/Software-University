0c16c8b00000a06a3a6112b90aabfbeedf34c981
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-06-14 22:32:04 +0300

    06. Square Numbers
    Judge - 100/100.

---
+++ Programming-Fundamentals-May-2016/4.0. Lists-and-Matrices/Lab/Square-Numbers/SquareNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class SquareNumbers
+{
+    static void Main()
+    {
+        var nums = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse)
+            .ToList();
+        var result = new List<int>();
+
+        foreach (var num in nums)
+        {
+            double sqrt = Math.Sqrt(num);
+
+            if (sqrt == (int)sqrt)
+            {
+                result.Add(num);
+            }
+        }
+
+        result.Sort((a, b) => b.CompareTo(a));
+        Console.WriteLine(string.Join(" ", result));
+    }
+}
