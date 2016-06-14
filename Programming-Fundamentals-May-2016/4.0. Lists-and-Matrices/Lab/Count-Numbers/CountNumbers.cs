6396563e949f6785366c8568c27550383c18075a
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-06-14 23:07:21 +0300

    07. Count Numbers
    Judge - 100/100.

---
+++ Programming-Fundamentals-May-2016/4.0. Lists-and-Matrices/Lab/Count-Numbers/CountNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class CountNumbers
+{
+    static void Main()
+    {
+        var list = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse)
+            .ToList();
+        var array = new int[1001];
+
+        foreach (var num in list)
+        {
+            array[num]++;
+        }
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] > 0)
+            {
+                Console.WriteLine("{0} -> {1}", i, array[i]);
+            }
+        }
+    }
+}
