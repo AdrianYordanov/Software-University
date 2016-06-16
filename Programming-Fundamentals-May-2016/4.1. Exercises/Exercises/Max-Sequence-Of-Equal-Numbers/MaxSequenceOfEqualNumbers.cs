73944388d52b17409a1fecd5e0217067827a7e08
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-06-16 12:14:42 +0300

    01. Max Sequence of Equal Elements
    Judge - 100/100.

---
+++ Programming-Fundamentals-May-2016/4.1. Exercises/Exercises/Max-Sequence-Of-Equal-Numbers/MaxSequenceOfEqualNumbers.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class MaxSequenceOfEqualNumbers
+{
+    static void Main()
+    {
+        var list = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse)
+            .ToList();
+
+        int currentStartIndex = 0,
+            bestStartIndex = 0,
+            currentLength = 1,
+            bestLength = 1;
+
+        for (int i = 1; i < list.Count; i++)
+        {
+            bool setDefaultCurrentLengh = false;
+
+            if (list[i].Equals(list[i - 1]))
+            {
+                if (currentLength == 1)
+                {
+                    currentStartIndex = i - 1;
+                }
+                ++currentLength;
+            }
+            else
+            {
+                setDefaultCurrentLengh = true;
+            }
+
+            if (currentLength > bestLength)
+            {
+                bestLength = currentLength;
+                bestStartIndex = currentStartIndex;
+            }
+
+            if (setDefaultCurrentLengh)
+            {
+                currentLength = 1;
+            }
+        }
+
+        Console.WriteLine(string.Join(" ", list.GetRange(bestStartIndex, bestLength)));
+    }
+}
