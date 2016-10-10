070cbb7ae2ac0964b10d14d00117e893084b9710
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-10-10 18:28:41 +0300

    01. Sum Three Numbers
    Judge - 100/100.

---
+++ JS-Fundamentals-September-2016/1.0. Intro-to-JavaScript/Lab/SumThreeNumbers.js
@@ -0,0 +1,8 @@
+"use strict";
+
+function sumThreeNumbers([str1, str2, str3]) {
+    let sum = Number(str1) + Number(str2) + Number(str3);
+    console.log(sum);
+}
+
+sumThreeNumbers(['1.5', '1.5', '-1']);
