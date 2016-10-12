9e2c5fcf77814c65bc02dfd431d8aa716f804d99
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-10-12 17:22:01 +0300

    04. Day of Week
    Judge - 100/100.

---
+++ JS-Fundamentals-September-2016/5.0. Functions-and-Arrow-Functions/Lab/Day-Of-Week/Day-Of-Week.js
@@ -0,0 +1,20 @@
+"use sctrict";
+
+function dayOfWeek([day]) {
+    console.log(checkDay(day));
+
+    function checkDay(day) {
+        switch (day) {
+            case 'Monday': return 1;
+            case 'Tuesday': return 2;
+            case 'Wednesday': return 3;
+            case 'Thursday': return 4;
+            case 'Friday': return 5;
+            case 'Saturday': return 6;
+            case 'Sunday': return 7;
+            default: return 'error';
+        }
+    }
+}
+
+dayOfWeek(['Wednesday']);
