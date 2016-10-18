5d062e72d1ed8a5aa304af309cd7b0627b2bbb00
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-10-18 22:17:42 +0300

    10. Compose Tag
    Judge - 100/100.

---
+++ JS-Fundamentals-September-2016/3.0. Data-Expressions-Statements-Exercises/Exercises/Compose-Tag/Compose-Tag.js
@@ -0,0 +1,7 @@
+"use strict";
+
+function composeTag([fileLocation, alternateText]) {
+    console.log(`<img src="${fileLocation}" alt="${alternateText}">`);
+}
+
+composeTag(['smiley.gif', 'Smiley Face']);
