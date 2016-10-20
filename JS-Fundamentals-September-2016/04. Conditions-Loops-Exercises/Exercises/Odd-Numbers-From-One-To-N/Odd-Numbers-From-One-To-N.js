82a815470e5c7180b8bb362f27fe25421900795a
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-10-20 20:29:18 +0300

    03. Odd Numbers 1 to N
    Judge - 100/100.

---
+++ JS-Fundamentals-September-2016/04. Conditions-Loops-Exercises/Exercises/Odd-Numbers-From-One-To-N/Odd-Numbers-From-One-To-N.js
@@ -0,0 +1,12 @@
+"use strict";
+
+function oddNumbersFromOneToN([number]) {
+    let n = Number(number);
+
+    for (let i = 1; i <= n; ++i) {
+        if (i % 2 != 0)
+            console.log(i);
+    }
+}
+
+oddNumbersFromOneToN(['5']);
