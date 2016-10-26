f247add4a5fd04d982b668d28f29c09000b330d1
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-10-26 22:50:14 +0300

    04. Extensible Object
    Judge - 100/100.

---
+++ JS-Advanced-October-2016/07. Objects-and-Inheritance-Exercises/Exercises/Extensible-Object/Extensible-Object.js
@@ -0,0 +1,31 @@
+"use strict";
+
+function extensibleObject() {
+    let obj = {};
+
+    function extendFunction(template) {
+        let currentObj = this;
+
+        for (let prop in template) {
+            if (typeof template[prop] == 'function') {
+                let parent = Object.getPrototypeOf(currentObj);
+                parent[prop] = template[prop];
+            } else {
+                currentObj[prop] = 'da';
+            }
+        }
+    }
+
+    obj.extend = extendFunction;
+    return obj;
+}
+
+let obj = extensibleObject();
+let template = {
+    extensionMethod: function () {
+        return 'just function'
+    },
+    extensionProperty: 'someString'
+};
+obj.extend(template);
+console.log(obj);
