562222635674be816def8165e7c95f362ccadf6c
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-10-28 15:08:10 +0300

    01. Sum of Numbers
    Judge - 100/100.
    Changed file name.

--- JS-Advanced-October-2016/08. Unit-Testing-with-Mocha/Lab/Sum-Of-Numbers/test/Sum-Of-Numbers-Tests.js
+++ JS-Advanced-October-2016/08. Unit-Testing-with-Mocha/Lab/Sum-Of-Numbers/test/Sum-Of-Numbers-Tests.js
@@ -4,9 +4,9 @@ let sum = require("../Sum-Of-Numbers").sum;
 let expect = require("chai").expect;
 
 describe("Function sum(arr) tests.", function () {
-    it("should return 40 for [12, 28]", function () {
-        let expectValue = 40;
-        let actualValue = sum([12, 28]);
+    it("should return 99.9 for [12, 28, 59.9]", function () {
+        let expectValue = 99.9;
+        let actualValue = sum([12, 28, 59.9]);
         expect(actualValue).to.be.equal(expectValue);
     });
     it("should return 5 for [5]", function () {
@@ -14,9 +14,13 @@ describe("Function sum(arr) tests.", function () {
         let actualValue = sum([5]);
         expect(actualValue).to.be.equal(expectValue);
     });
-    it("should return 0 for []", function () {
+    it("should return 0 for empty array", function () {
         let expectValue = 0;
         let actualValue = sum([]);
         expect(actualValue).to.be.equal(expectValue);
     });
+    it("should return NaN for array of NaN values", function () {
+        let actualValue = sum['Invalid', 'Not a number'];
+        expect(actualValue).to.be.NaN;
+    })
 });
