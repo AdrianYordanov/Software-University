1123c47003bafcf90d941311cad8d0af318650e2
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-11-07 17:08:39 +0200

    03. Extending Prototype
    Judge - 100/100.

---
+++ JS-Advanced-October-2016/13. Class-Inheritance-and-Prototypes/Lab/Extend-Prototype/Extend-Prototype.js
@@ -0,0 +1,25 @@
+"use strict";
+
+function extendPrototype(classToExtend) {
+    classToExtend.prototype.species = "Human";
+    classToExtend.prototype.toSpeciesString = function () {
+        return `I am a ${this.species}. ${this.toString()}`;
+    }
+}
+
+class Person {
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
+    }
+    toString() {
+        let className = this.constructor.name;
+        return `${className} (name: ${this.name}, email: ${this.email})`;
+    }
+}
+
+extendPrototype(Person);
+
+let p = new Person("Pesho","email@hit.bg");
+console.log(p.species);
+console.log(p.toSpeciesString());
