907b81d9714e1217bd21b05c33ae77953fcd1363
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-11-14 13:38:53 +0200

    02. Add/Delete in List
    Judge - 100/100.

---
+++ JS-Advanced-October-2016/17. Exam-Preparation/Exam-Preparation/Add-Delete-Towns/test/Add-Delete-Towns-Tests.js
@@ -0,0 +1,80 @@
+"use strict";
+
+let expect = require("chai").expect;
+let listObject = require("../Add-Delete-Towns").list;
+
+describe("Test the function list.", function () {
+    let list = undefined;
+
+    beforeEach(function () {
+        list = listObject();
+    });
+
+    describe("Test the properties", function () {
+        it("should have property add", function () {
+            expect(list.hasOwnProperty("add")).equal(true);
+        });
+        it("should have property delete", function () {
+            expect(list.hasOwnProperty("delete")).equal(true);
+        });
+        it("should have property toString", function () {
+            expect(list.hasOwnProperty("toString")).equal(true);
+        });
+    });
+
+    describe("Test add function,", function () {
+        it("should add number", function () {
+            list.add(23);
+            expect(list.toString()).equal("23");
+        });
+        it("should add string", function () {
+            list.add("string test");
+            expect(list.toString()).equal("string test");
+        });
+        it("should add object", function () {
+            list.add({name: "Adrian"});
+            expect(list.toString()).equal("[object Object]");
+        });
+        it("with more items", function () {
+            list.add(23);
+            list.add("string");
+            list.add({name: "Adrian"});
+            expect(list.toString()).equal("23, string, [object Object]");
+        });
+    });
+
+    describe("Test delete function,", function () {
+        it("with valid index", function () {
+            list.add(23);
+            list.add(435);
+            list.add(999);
+            list.delete(1);
+            expect(list.toString()).equal("23, 999");
+        });
+        it("should return the deleted object", function () {
+            list.add(23);
+            list.add(435);
+            list.add(999);
+            expect(list.delete(1)).equal(435);
+        });
+
+        it("should return undefined for negative index", function () {
+            list.add(23);
+            list.add(435);
+            list.add(999);
+            expect(list.delete(-1)).equal(undefined);
+        });
+        it("should return undefined for greater index", function () {
+            list.add(23);
+            list.add(435);
+            list.add(999);
+            expect(list.delete(3)).equal(undefined);
+        });
+        it("should return undefined for string index", function () {
+            list.add(23);
+            list.add(435);
+            list.add(999);
+            expect(list.delete("1")).equal(undefined);
+        });
+    });
+});
