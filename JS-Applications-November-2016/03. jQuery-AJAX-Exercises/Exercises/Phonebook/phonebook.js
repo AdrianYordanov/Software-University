d5619f3a0652096ab2bcf2917df0cf28327a817f
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-11-27 18:24:45 +0200

    04. Phonebook
    Judge - 100/100.

---
+++ JS-Applications-November-2016/03. jQuery-AJAX-Exercises/Exercises/Phonebook/phonebook.js
@@ -0,0 +1,61 @@
+"use strict";
+
+function attachEvents() {
+    const apiUrl = 'https://phonebook-nakov.firebaseio.com/phonebook';
+    let phoneBook = $('#phonebook');
+
+    $('#btnLoad').on('click', loadPhoneNumbers);
+    $('#btnCreate').on('click', addNewPhoneNumber);
+
+    function loadPhoneNumbers() {
+        $.ajax({
+            method: 'GET',
+            url: apiUrl + '.json',
+            success: renderPhoneNumbers
+        });
+    }
+
+    function addNewPhoneNumber() {
+        let person = $('#person').val().trim();
+        let phone = $('#phone').val().trim();
+
+        if (person != '' && phone != '') {
+            $.ajax({
+                method: 'POST',
+                data: JSON.stringify({
+                    person, phone
+                }),
+                url: apiUrl + '.json',
+                success: function () {
+                    $('#person').val('');
+                    $('#phone').val('');
+
+                    loadPhoneNumbers();
+                }
+            })
+        }
+    }
+
+    function deleteEntry(id) {
+        $.ajax({
+            method: 'DELETE',
+            url: apiUrl + '/' + id + '.json',
+            success: loadPhoneNumbers
+        })
+    }
+
+    function renderPhoneNumbers(data) {
+        phoneBook.empty();
+        let keys = Object.keys(data);
+        for (let key of keys) {
+            let number = data[key];
+            let li = $('<li>')
+                .text(`${number.person}: ${number.phone} `)
+                .append($('<button>')
+                    .text('[Delete]')
+                    .on('click', () => deleteEntry(key)));
+
+            $('#phonebook').append(li);
+        }
+    }
+}
