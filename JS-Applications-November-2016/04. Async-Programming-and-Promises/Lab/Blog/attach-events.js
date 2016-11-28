53ea1c214a65827612d9bdb0c1da77344e6e634e
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-11-28 15:53:13 +0200

    02. Blog
    Judge - 100/100.

---
+++ JS-Applications-November-2016/04. Async-Programming-and-Promises/Lab/Blog/attach-events.js
@@ -0,0 +1,89 @@
+"use strict";
+
+function attachEvents() {
+    const appId = 'kid_BJRuignI';
+    const apiBaseUrl = `https://baas.kinvey.com/appdata/${appId}/`;
+    const userName = 'guest';
+    const password = 'guest';
+    const authorizationHeader = {Authorization: 'Basic ' + btoa(`${userName}:${password}`)};
+
+    $('#btnLoadPosts').on('click', loadPosts);
+    $('#btnViewPost').on('click', loadPostDetails);
+
+    function loadPosts() {
+        $.get({
+            url: apiBaseUrl + 'posts',
+            headers: authorizationHeader
+        })
+            .then(fillDropDownMenu)
+            .catch(renderError);
+    }
+
+    function fillDropDownMenu(posts) {
+        let menu = $('#posts');
+        menu.empty();
+        for (let post of posts) {
+            menu.append(createOptionItem(post.title, post._id));
+        }
+    }
+
+    function loadPostDetails() {
+        let postId = $('#posts').val();
+        let getPostDetails = $.get({
+            url: apiBaseUrl + 'posts/' + postId,
+            headers: authorizationHeader
+        });
+
+        let getComments = $.get({
+            url: apiBaseUrl + `comments/?query={"post_id":"${postId}"}`,
+            headers: authorizationHeader
+        });
+
+        Promise.all([getPostDetails, getComments])
+            .then(function ([posts, comments]) {
+                renderPost(posts);
+                renderComments(comments);
+            })
+            .catch(renderError);
+    }
+
+    function renderPost(postData) {
+        $('#post-title').text(postData.title);
+        $('#post-body').text(postData.body);
+    }
+
+    function renderComments(comments) {
+        let commentsList = $('#post-comments');
+        commentsList.empty();
+        for (let comment of comments) {
+            commentsList.append(createListItem(comment.text));
+        }
+    }
+
+    function renderError(error) {
+        let errorContainer = $('<div>')
+            .text(`Error: ${error.status} (${error.statusText})`)
+            .hide();
+        $('body')
+            .prepend(errorContainer);
+
+        errorContainer.slideDown(function () {
+            setTimeout(function () {
+                errorContainer.slideUp(function () {
+                    errorContainer.remove();
+                });
+            }, 1000);
+        });
+    }
+
+    function createOptionItem(text, value) {
+        return $('<option>')
+            .attr('value', value)
+            .text(text);
+    }
+
+    function createListItem(text) {
+        return $('<li>')
+            .text(text);
+    }
+}
