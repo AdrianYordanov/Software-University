c9a79d56001486703e7187c247792022b74c41bd
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2017-01-25 16:56:10 +0200

    07. Create Table People
    Constraints added.
    Judge - 1/1.

--- Databases Basics-January-2017/04. Data-Definition-And-Datatypes-Exercises/Exercises/Create-Table-People/Create-Table-People.sql
+++ Databases Basics-January-2017/04. Data-Definition-And-Datatypes-Exercises/Exercises/Create-Table-People/Create-Table-People.sql
@@ -1,18 +1,25 @@
 USE People
+
 CREATE TABLE People(
-	Id bigint IDENTITY PRIMARY KEY,
+	Id bigint IDENTITY,
 	Name nvarchar(200) NOT NULL,
-	Picture binary(2000),
+	Picture varbinary(max),
 	Height numeric(3, 2),
 	Weight float(2),
-	Gender varchar(1) NOT NULL,
+	Gender varchar(1) NOT NULL CHECK(gender in('f', 'm')),
 	Birthday date NOT NULL,
 	Biography ntext
 )
+
+ALTER TABLE People 
+ADD CONSTRAINT PK_People PRIMARY KEY(Id)
+ALTER TABLE People
+ADD CONSTRAINT CH_Picture_People CHECK(DATALENGTH(Picture) < 2 * 1024 * 1024)
+
 INSERT INTO People(Name, Picture, Height, Weight, Gender, Birthday, Biography)
 VALUES
 ('Adrian Yordanov', NULL, 1.80, 65, 'm', '1997-07-25', 'C# Developer'),
-('Petur Petkov', NULL, NULL, NULL, 'm', '1995-03-19', NULL),
-('Petya Raykova', NULL, 1.60, 56, 'f', '1986-07-25', NULL),
-('Ivan Raykov', NULL, 1.90, 120, 'm', '1984-04-12', NULL),
-('Tihomir Ivanov', NULL, 2, 100, 'm', '1999-09-4', NULL)
+('Petur Petkov', NULL, NULL, NULL, 'm', '1995-03-19', 'JAVA Developer'),
+('Petya Raykova', NULL, 1.60, 56, 'f', '1986-07-25', 'C++ Developer'),
+('Ivan Raykov', NULL, 1.90, 120, 'm', '1984-04-12', 'SQL Developer'),
+('Tihomir Ivanov', NULL, 2, 100, 'm', '1999-09-4', 'HTML/CSS DEVELOPER')
