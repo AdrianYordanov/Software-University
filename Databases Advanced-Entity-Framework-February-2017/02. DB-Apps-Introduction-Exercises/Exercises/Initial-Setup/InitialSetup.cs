bf39da34622a92e1a3984751afc678c17a23f3be
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2017-02-21 21:38:52 +0200

    01. Initial Setup

---
+++ Databases Advanced-Entity-Framework-February-2017/02. DB-Apps-Introduction-Exercises/Exercises/Initial-Setup/InitialSetup.cs
@@ -0,0 +1,29 @@
+﻿using System.Data.SqlClient;
+using System.IO;
+class InitialSetup
+{
+    static void Main()
+    {
+        string createDatabaseQuery = File.ReadAllText("../../queries/create-database.sql");
+        string createTablesQuery = File.ReadAllText("../../queries/create-tables.sql");
+
+        SqlConnection connection = new SqlConnection("Server=ASUS-ADRIAN\\SQLEXPRESS;Integrated Security=true");
+        connection.Open();
+
+        using (connection)
+        {
+            SqlCommand createDatabaseCommand = new SqlCommand(createDatabaseQuery, connection);
+            SqlCommand createTablesCommand = new SqlCommand(createTablesQuery, connection);
+
+            using (createDatabaseCommand)
+            {
+                createDatabaseCommand.ExecuteNonQuery();
+            }
+
+            using (createTablesCommand)
+            {
+                createTablesCommand.ExecuteNonQuery();
+            }
+        }
+    }
+}
