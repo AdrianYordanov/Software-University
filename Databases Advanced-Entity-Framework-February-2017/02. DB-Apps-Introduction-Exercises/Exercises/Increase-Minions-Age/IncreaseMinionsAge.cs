c9f0e05218b3710437c1ecaa0da0f7d553b804c5
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2017-02-27 22:51:46 +0200

    8. Increase Minions Age

---
+++ Databases Advanced-Entity-Framework-February-2017/02. DB-Apps-Introduction-Exercises/Exercises/Increase-Minions-Age/IncreaseMinionsAge.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+
+class IncreaseMinionsAge
+{
+    static void Main()
+    {
+        List<int> minionsIds = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse)
+            .ToList();
+
+        string updateQuery = "UPDATE Minions\nSET Age += 1, Name = UPPER(LEFT(Name,1)) + LOWER(SUBSTRING(Name, 2, LEN(Name)))\n";
+        string selectQuery = "SELECT Name, Age FROM Minions";
+        string whereStatement = "WHERE Id IN(" +
+            minionsIds.Select((id, index) => "@Id" + index).Aggregate((id1, id2) => id1 + ", " + id2) + ")";
+        updateQuery += whereStatement;
+
+        using (SqlConnection connection = new SqlConnection(@"Server=ASUS-ADRIAN\SQLEXPRESS;
+Database=MinionsDB;
+Integrated Security=true"))
+        {
+            connection.Open();
+            SqlCommand command = new SqlCommand(updateQuery, connection);
+
+            for (int i = 0; i < minionsIds.Count; i++)
+            {
+                command.Parameters.AddWithValue("@Id" + i, minionsIds.ElementAt(i));
+            }
+
+            command.ExecuteNonQuery();
+            command.CommandText = selectQuery;
+            var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine($"{reader["Name"]} {reader["Age"]}");
+            }
+        }
+    }
+}
