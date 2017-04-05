766f8a5fd8efd7b04c7b4e397fbc819725d895f5
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2017-04-05 16:23:48 +0300

    03. Employees full information
    Judge - 1/1.

---
+++ Databases Advanced-Entity-Framework-February-2017/04. Introduction-to-EntityFramework-Exercises/Exercises/Softuni-Database/Tasks/EmployeesFullInformation.cs
@@ -0,0 +1,16 @@
+﻿namespace Softuni_Database.Tasks
+{
+    using System;
+    using System.Linq;
+
+    class EmployeesFullInformation
+    {
+        public static void Execute()
+        {
+            var context = new SoftuniContext();
+            var employees = context.Employees.ToList();
+
+            employees.ForEach(e => Console.WriteLine($"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary}"));
+        }
+    }
+}
