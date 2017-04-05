b295457bee4536f8c52580134b7c2eb5fdf963be
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2017-04-05 17:01:10 +0300

    05. Employees from Seattle
    Judge - 1/1.

---
+++ Databases Advanced-Entity-Framework-February-2017/04. Introduction-to-EntityFramework-Exercises/Exercises/Softuni-Database/Tasks/EmployeesFromSeattle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Softuni_Database.Tasks
+{
+    class EmployeesFromSeattle
+    {
+        public static void Execute()
+        {
+            var context = new SoftuniContext();
+            var employees = context.Employees.ToList();
+            var filteredEmployees = employees
+                .Where(e => e.DepartmentID == 6)
+                .OrderBy(e => e.Salary)
+                .ThenByDescending(e => e.FirstName)
+                .ToList();
+            filteredEmployees
+                .ForEach(e => Console.WriteLine($"{e.FirstName} {e.LastName} from {e.Department.Name} - ${e.Salary.ToString("f2")}"));
+        }
+    }
+}
