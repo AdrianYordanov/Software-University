fb5ca3a0fbe5aec56f4ba2eb6af1aab51fb6d017
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2017-04-05 15:47:59 +0300

    All classes are extracted.

---
+++ Databases Advanced-Entity-Framework-February-2017/04. Introduction-to-EntityFramework-Exercises/Exercises/Softuni-Database/Startup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Softuni_Database
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
