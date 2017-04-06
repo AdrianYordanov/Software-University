717983351854038f00f33a365ab1f88dd7a58426
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2017-04-06 20:56:16 +0300

    08. Addresses by town name
    Judge - 1/1.

---
+++ Databases Advanced-Entity-Framework-February-2017/04. Introduction-to-EntityFramework-Exercises/Exercises/Softuni-Database/Tasks/AdressesByTownName.cs
@@ -0,0 +1,21 @@
+﻿namespace Softuni_Database.Tasks
+{
+    using System;
+    using System.Linq;
+
+    class AdressesByTownName
+    {
+        public static void Execute()
+        {
+            var context = new SoftuniContext();
+            var adresses = context.Addresses;
+            var orderedAdresses = adresses
+                .OrderByDescending(a => a.Employees.Count)
+                .ThenBy(a => a.Town.Name)
+                .Take(10)
+                .ToList();
+
+            orderedAdresses.ForEach(a => Console.WriteLine($"{a.AddressText}, {a.Town.Name} - {a.Employees.Count} employees"));
+        }
+    }
+}
