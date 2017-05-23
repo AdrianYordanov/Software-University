ba8ef83bdbbcc59297dd830ca88cc46270b27acd
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2017-05-23 15:56:59 +0300

    1.Reverse Strings
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/01. Stacks-And-Queues/Lab/Reverse-Strings/ReverseStrings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class ReverseStrings
+{
+    static void Main()
+    {
+        var input = Console.ReadLine();
+        var stack = new Stack<char>();
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            stack.Push(input[i]);
+        }
+
+        for (int i = 0; stack.Count > 0; i++)
+        {
+            Console.Write(stack.Pop());
+        }
+
+        Console.WriteLine();
+    }
+}
