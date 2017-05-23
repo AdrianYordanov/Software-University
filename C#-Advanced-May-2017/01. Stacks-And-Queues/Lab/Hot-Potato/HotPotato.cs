be5fcfbb714927851238bd7e148e935862e6b2c6
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2017-05-23 17:34:45 +0300

    5.Hot Potato
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/01. Stacks-And-Queues/Lab/Hot-Potato/HotPotato.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class HotPotato
+{
+    static void Main()
+    {
+        var names = Console.ReadLine().Split(' ');
+        var n = int.Parse(Console.ReadLine());
+        var queue = new Queue<string>(names);
+
+        while (queue.Count > 1)
+        {
+
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                queue.Enqueue(queue.Dequeue());
+            }
+
+            Console.WriteLine($"Removed {queue.Dequeue()}");
+        }
+
+        Console.WriteLine($"Last is {queue.Dequeue()}");
+    }
+}
