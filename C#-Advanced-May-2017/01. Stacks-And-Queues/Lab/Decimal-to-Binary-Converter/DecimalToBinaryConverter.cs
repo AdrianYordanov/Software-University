ae25b69e3c9039f60baa52f09ee20f221dc0b6f9
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2017-05-23 16:31:32 +0300

    3.Decimal To Binary Converter
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/01. Stacks-And-Queues/Lab/Decimal-to-Binary-Converter/DecimalToBinaryConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class DecimalToBinaryConverter
+{
+    static void Main()
+    {
+        var number = int.Parse(Console.ReadLine());
+        var stack = new Stack<int>();
+
+        if (number == 0)
+        {
+            Console.WriteLine(number);
+            return;
+        }
+
+        while (number > 0)
+        {
+            if (number % 2 == 0)
+            {
+                stack.Push(0);
+            }
+            else
+            {
+                stack.Push(1);
+            }
+
+            number /= 2;
+        }
+
+        while (stack.Count > 0)
+        {
+            Console.Write(stack.Pop());
+        }
+
+        Console.WriteLine();
+    }
+}
