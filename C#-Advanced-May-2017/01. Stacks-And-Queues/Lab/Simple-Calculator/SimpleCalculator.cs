0fcb2ba61870e2102f959a714b705b05b38be6e6
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2017-05-23 16:20:58 +0300

    2.Simple Calculator
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/01. Stacks-And-Queues/Lab/Simple-Calculator/SimpleCalculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class SimpleCalculator
+{
+    static void Main()
+    {
+        var input = Console.ReadLine();
+        var tokens = input.Split(' ');
+        var stack = new Stack<double>();
+
+
+        for (int i = 0; i < tokens.Length; i++)
+        {
+            if (stack.Count == 0)
+            {
+                stack.Push(double.Parse(tokens[i]));
+            }
+            else
+            {
+                var operation = tokens[i];
+                i++;
+
+                switch (operation)
+                {
+                    case "+": stack.Push(stack.Pop() + double.Parse(tokens[i])); break;
+                    case "-": stack.Push(stack.Pop() - double.Parse(tokens[i])); break;
+                }
+            }
+        }
+
+        Console.WriteLine(stack.Pop());
+    }
+}
