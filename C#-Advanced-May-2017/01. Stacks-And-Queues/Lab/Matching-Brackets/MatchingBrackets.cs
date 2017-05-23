9580d06d1e9feb95c94fd79fbbd3e4c2063ba9fd
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2017-05-23 16:42:31 +0300

    4.Matching Brackets
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/01. Stacks-And-Queues/Lab/Matching-Brackets/MatchingBrackets.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class MatchingBrackets
+{
+    static void Main()
+    {
+        var input = Console.ReadLine();
+        var stack = new Stack<int>();
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (input[i] == '(')
+            {
+                stack.Push(i);
+            }
+
+            if (input[i] == ')')
+            {
+                var lastScopeIndexOpen = stack.Pop();
+                Console.WriteLine(input.Substring(lastScopeIndexOpen, i - lastScopeIndexOpen + 1));
+            }
+        }
+    }
+}
