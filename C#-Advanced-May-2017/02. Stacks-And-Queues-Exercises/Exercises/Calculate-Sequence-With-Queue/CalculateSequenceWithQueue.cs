69029944af22f844e04a4d623d4c6d105f31efa7
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2017-05-24 21:43:04 +0300

    05. Sequence With Queue
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/02. Stacks-And-Queues-Exercises/Exercises/Calculate-Sequence-With-Queue/CalculateSequenceWithQueue.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class CalculateSequenceWithQueue
+{
+    static void Main()
+    {
+        long n = long.Parse(Console.ReadLine());
+        long membersCount = 50;
+        var queue = new Queue<long>();
+
+        queue.Enqueue(n);
+
+        for (int i = 0; i < membersCount; i++)
+        {
+            var currentS = queue.Dequeue();
+            Console.Write(currentS + " ");
+            queue.Enqueue(currentS + 1);
+            queue.Enqueue(currentS * 2 + 1);
+            queue.Enqueue(currentS + 2);
+        }
+    }
+}
