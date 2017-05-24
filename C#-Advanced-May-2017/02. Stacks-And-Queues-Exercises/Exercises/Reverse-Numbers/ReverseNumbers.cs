f8aa889345627babdea1f180b4988147cadb1645
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2017-05-24 19:23:37 +0300

    01. Reverse Numbers
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/02. Stacks-And-Queues-Exercises/Exercises/Reverse-Numbers/ReverseNumbers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class ReverseNumbers
+{
+    static void Main()
+    {
+        var tokens = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        var stack = new Stack<int>();
+
+        for (int i = 0; i < tokens.Length; i++)
+        {
+            stack.Push(int.Parse(tokens[i]));
+        }
+
+        while (stack.Count > 0)
+        {
+            Console.Write(stack.Pop() + " ");
+        }
+
+        Console.WriteLine();
+    }
+}
