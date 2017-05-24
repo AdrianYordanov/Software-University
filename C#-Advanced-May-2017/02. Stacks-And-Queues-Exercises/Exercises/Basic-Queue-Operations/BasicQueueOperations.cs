508b78388fab4cdaf72ca6daddb7bb6aae2eb3f8
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2017-05-24 20:39:15 +0300

    04. Basic Queue Operations
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/02. Stacks-And-Queues-Exercises/Exercises/Basic-Queue-Operations/BasicQueueOperations.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class BasicQueueOperations
+{
+    static void Main()
+    {
+        var tokens = Console.ReadLine().Split(' ');
+        var numbers = Console.ReadLine().Split(' ');
+        var queue = new Queue<int>();
+        var n = int.Parse(tokens[0]);
+        var s = int.Parse(tokens[1]);
+        var x = int.Parse(tokens[2]);
+
+        for (int enqueueElementCounter = 0; enqueueElementCounter < n; enqueueElementCounter++)
+        {
+            queue.Enqueue(int.Parse(numbers[enqueueElementCounter]));
+        }
+
+        for (int dequeueElementsCounter = 0; dequeueElementsCounter < s; dequeueElementsCounter++)
+        {
+            queue.Dequeue();
+        }
+
+        var queueCount = queue.Count;
+        var isFoundX = false;
+        var smallestNumber = int.MaxValue;
+
+
+        while (queue.Count > 0)
+        {
+            var enqueueNumber = queue.Dequeue();
+
+            if (enqueueNumber == x)
+            {
+                isFoundX = true;
+            }
+            else if (smallestNumber > enqueueNumber)
+            {
+                smallestNumber = enqueueNumber;
+            }
+        }
+
+        if (queueCount == 0)
+        {
+            Console.WriteLine(0);
+        }
+        else
+        {
+            if (isFoundX)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(smallestNumber);
+            }
+        }
+    }
+}
