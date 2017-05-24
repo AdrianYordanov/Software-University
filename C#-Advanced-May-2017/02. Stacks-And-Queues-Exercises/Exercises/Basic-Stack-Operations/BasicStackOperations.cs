b8c56a5885b80dcf320863b81c05c26903a197a5
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2017-05-24 19:48:33 +0300

    02. Basic Stack Operations
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/02. Stacks-And-Queues-Exercises/Exercises/Basic-Stack-Operations/BasicStackOperations.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+class BasicStackOperations
+{
+    static void Main()
+    {
+        var tokens = Console.ReadLine().Split(' ');
+        var numbers = Console.ReadLine().Split(' ');
+        var stack = new Stack<int>();
+        var n = int.Parse(tokens[0]);
+        var s = int.Parse(tokens[1]);
+        var x = int.Parse(tokens[2]);
+
+        for (int pushElementCounter = 0; pushElementCounter < n; pushElementCounter++)
+        {
+            stack.Push(int.Parse(numbers[pushElementCounter]));
+        }
+
+        for (int popElementsCounter = 0; popElementsCounter < s; popElementsCounter++)
+        {
+            stack.Pop();
+        }
+
+        var stackCount = stack.Count;
+        var isFoundX = false;
+        var smallestNumber = int.MaxValue;
+
+
+        while (stack.Count > 0)
+        {
+            var popNumber = stack.Pop();
+
+            if (popNumber == x)
+            {
+                isFoundX = true;
+            }
+            else if (smallestNumber > popNumber)
+            {
+                smallestNumber = popNumber;
+            }
+        }
+
+        if (stackCount == 0)
+        {
+            Console.WriteLine(0);
+        }
+        else
+        {
+            if (isFoundX)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(smallestNumber);
+            }
+        }
+    }
+}
