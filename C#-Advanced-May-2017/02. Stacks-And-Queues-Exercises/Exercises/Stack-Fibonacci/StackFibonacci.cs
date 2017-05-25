f50625859d21f7f4e6a8b786355a11846a012c33
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2017-05-25 21:27:34 +0300

    09. Stack Fibonacci
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/02. Stacks-And-Queues-Exercises/Exercises/Stack-Fibonacci/StackFibonacci.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class StackFibonacci
+{
+    static void Main()
+    {
+        var n = ulong.Parse(Console.ReadLine());
+        var stack = new Stack<ulong>(new ulong[] { 0, 1 });
+
+        for (ulong i = 2; i <= n; i++)
+        {
+            var last = stack.Pop();
+            var beforeLast = stack.Pop();
+            var current = last + beforeLast;
+            stack.Push(beforeLast);
+            stack.Push(last);
+            stack.Push(current);
+        }
+
+        Console.WriteLine(stack.Peek());
+    }
+}
