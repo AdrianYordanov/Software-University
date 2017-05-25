2fe2e4432d6e5820eb70cdeacffda3ed9f25949e
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2017-05-25 14:34:54 +0300

    01. Parking Lot
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/03. Sets-And-Dictionaries/Lab/Parking-Lot/ParkingLot.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+class ParkingLot
+{
+    static void Main()
+    {
+        var input = Console.ReadLine();
+        var parking = new SortedSet<string>();
+
+        while (input != "END")
+        {
+            var tokens = Regex.Split(input, "[, ]+", RegexOptions.IgnorePatternWhitespace);
+            var operation = tokens[0];
+            var carNumber = tokens[1];
+
+            switch (operation)
+            {
+                case "IN": parking.Add(carNumber); break;
+                case "OUT": parking.Remove(carNumber); break;
+            }
+
+            input = Console.ReadLine();
+        }
+
+        if (parking.Count != 0)
+        {
+            foreach (var car in parking)
+            {
+                Console.WriteLine(car);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Parking Lot is Empty");
+        }
+    }
+}
