5008a7a025c142216ab2644bf2eb83a28e877936
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2017-05-25 23:09:11 +0300

    10. Simple Text Editor
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/02. Stacks-And-Queues-Exercises/Exercises/Simple-Text-Editor/SimpleTextEditor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class SimpleTextEditor
+{
+    static void Main()
+    {
+        var n = int.Parse(Console.ReadLine());
+        var stringHistory = new Stack<string>();
+        stringHistory.Push(string.Empty);
+
+        for (int i = 0; i < n; i++)
+        {
+            var tokens = Console.ReadLine().Split(' ');
+            var operation = int.Parse(tokens[0]);
+
+            switch (operation)
+            {
+                case 1:
+                    {
+                        var textAppend = tokens[1];
+                        var update = stringHistory.Peek() + textAppend;
+                        stringHistory.Push(update);
+                        break;
+                    }
+                case 2:
+                    {
+                        var count = int.Parse(tokens[1]);
+                        var update = stringHistory.Peek();
+                        update = update.Remove(update.Length - count);
+                        stringHistory.Push(update);
+                        break;
+                    }
+                case 3:
+                    {
+                        var index = int.Parse(tokens[1]);
+                        var lastUpdate = stringHistory.Peek();
+                        Console.WriteLine(lastUpdate[index - 1]);
+                        break;
+                    }
+                case 4:
+                    {
+                        stringHistory.Pop();
+                        break;
+                    }
+            }
+        }
+    }
+}
