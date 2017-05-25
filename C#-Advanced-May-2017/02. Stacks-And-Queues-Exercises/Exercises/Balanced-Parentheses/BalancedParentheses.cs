301591d44c7c7ed5a60519f8faeeedfa95772759
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2017-05-25 20:22:54 +0300

    07. Balanced Parenthesis
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/02. Stacks-And-Queues-Exercises/Exercises/Balanced-Parentheses/BalancedParentheses.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class BalancedParentheses
+{
+    static void Main()
+    {
+        var input = Console.ReadLine();
+        var stack = new Stack<char>();
+        var openBrackets = new char[] { '{', '[', '(' };
+        var isBalanced = true;
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            var currentBracket = input[i];
+
+            if (openBrackets.Contains(currentBracket))
+            {
+                stack.Push(currentBracket);
+            }
+            else
+            {
+                if (stack.Count == 0)
+                {
+                    isBalanced = false;
+                    break;
+                }
+
+                var lastOpenBracket = stack.Pop();
+
+                switch (currentBracket)
+                {
+                    case '}': if (lastOpenBracket != '{') { isBalanced = false; } break;
+                    case ']': if (lastOpenBracket != '[') { isBalanced = false; } break;
+                    case ')': if (lastOpenBracket != '(') { isBalanced = false; } break;
+                }
+
+                if (!isBalanced)
+                {
+                    break;
+                }
+            }
+        }
+
+        Console.WriteLine(isBalanced ? "YES" : "NO");
+    }
+}
