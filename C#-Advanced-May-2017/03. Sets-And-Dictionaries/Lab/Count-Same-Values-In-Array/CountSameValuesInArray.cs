c9925adedad0a89ff4d75e3c1587ebfbb0b46fde
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2017-05-25 15:43:24 +0300

    03. Count Same Values In Array
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/03. Sets-And-Dictionaries/Lab/Count-Same-Values-In-Array/CountSameValuesInArray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class CountSameValuesInArray
+{
+    static void Main()
+    {
+        var numbers = Console.ReadLine()
+            .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(double.Parse)
+            .ToArray();
+        var dictionary = new SortedDictionary<double, int>();
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            var currentNumber = numbers[i];
+
+            if (!dictionary.ContainsKey(currentNumber))
+            {
+                dictionary.Add(currentNumber, 1);
+            }
+            else
+            {
+                dictionary[currentNumber]++;
+            }
+        }
+
+        foreach (var key in dictionary.Keys)
+        {
+            Console.WriteLine($"{key} - {dictionary[key]} times");
+        }
+    }
+}
