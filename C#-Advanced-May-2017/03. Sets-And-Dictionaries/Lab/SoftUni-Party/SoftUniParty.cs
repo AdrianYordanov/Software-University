2e31ea87e6668633c0f4f9ea2790fe35a7b3e8ac
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2017-05-25 15:09:11 +0300

    02. SoftUni Party
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/03. Sets-And-Dictionaries/Lab/SoftUni-Party/SoftUniParty.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class SoftUniParty
+{
+    static void Main()
+    {
+        var input = string.Empty;
+        var guestes = new SortedSet<string>();
+
+        while ((input = Console.ReadLine()) != "PARTY")
+        {
+            guestes.Add(input);
+        }
+
+
+        while ((input = Console.ReadLine()) != "END")
+        {
+            if (guestes.Contains(input))
+            {
+                // The person is going.
+                guestes.Remove(input);
+            }
+        }
+
+        Console.WriteLine(guestes.Count);
+
+        foreach (var personNumber in guestes)
+        {
+            Console.WriteLine(personNumber);
+        }
+    }
+}
