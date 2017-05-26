96ebfa499786192bc3e6744dae1eefc7896dbda6
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2017-05-26 15:29:53 +0300

    05. Phonebook
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/03. Sets-And-Dictionaries/Exercises/Phonebook/Phonebook.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Phonebook
+{
+    static void Main()
+    {
+        var phonebook = new Dictionary<string, string>();
+        var input = string.Empty;
+
+        while ((input = Console.ReadLine()) != "search")
+        {
+            var tokens = input.Split('-');
+            var name = tokens[0];
+            var number = tokens[1];
+
+            if (phonebook.ContainsKey(name))
+            {
+                phonebook[name] = number;
+            }
+            else
+            {
+                phonebook.Add(name, number);
+            }
+        }
+
+        while ((input = Console.ReadLine()) != "stop")
+        {
+            if (phonebook.ContainsKey(input))
+            {
+                Console.WriteLine($"{input} -> {phonebook[input]}");
+            }
+            else
+            {
+                Console.WriteLine($"Contact {input} does not exist.");
+            }
+        }
+    }
+}
