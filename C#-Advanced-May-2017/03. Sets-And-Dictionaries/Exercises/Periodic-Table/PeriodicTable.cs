74c10dbf31bcca1375d1f6c6572ab98e616a636e
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2017-05-26 15:11:11 +0300

    03. Periodic Table
    Judge - 100/100.
    Previous problem - removed namespaces which we don't need.

---
+++ C#-Advanced-May-2017/03. Sets-And-Dictionaries/Exercises/Periodic-Table/PeriodicTable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class PeriodicTable
+{
+    static void Main()
+    {
+        var n = int.Parse(Console.ReadLine());
+        var sortedSet = new SortedSet<string>();
+
+        for (int i = 0; i < n; i++)
+        {
+            var tokens = Console.ReadLine().Split(' ');
+
+            foreach (var token in tokens)
+            {
+                sortedSet.Add(token);
+            }
+        }
+
+        foreach (var str in sortedSet)
+        {
+            Console.Write(str + " ");
+        }
+    }
+}
