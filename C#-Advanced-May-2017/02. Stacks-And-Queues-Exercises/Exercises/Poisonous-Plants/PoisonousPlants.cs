1124ae6c56b8fad1cea3aa42c38d72e6584b2969
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2017-05-26 14:32:28 +0300

    11. Poisonous Plants
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/02. Stacks-And-Queues-Exercises/Exercises/Poisonous-Plants/PoisonousPlants.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class PoisonousPlants
+{
+    static void Main()
+    {
+        var n = int.Parse(Console.ReadLine());
+        var plants = Console.ReadLine()
+            .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+        var days = new int[n];
+        var proximityStack = new Stack<int>();
+        proximityStack.Push(0);
+
+        for (int i = 0; i < plants.Length; i++)
+        {
+            int maxDays = 0;
+
+            while (proximityStack.Count > 0 && plants[proximityStack.Peek()] >= plants[i])
+            {
+                maxDays = Math.Max(maxDays, days[proximityStack.Pop()]);
+            }
+
+            if (proximityStack.Count > 0)
+            {
+                days[i] = maxDays + 1;
+            }
+
+            proximityStack.Push(i);
+        }
+
+        Console.WriteLine(days.Max());
+    }
+}
