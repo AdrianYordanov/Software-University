7c894eeec3cac2cd0c601d60f06dd4df2a78a09b
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2017-05-26 15:06:05 +0300

    02. Sets Of Elements
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/03. Sets-And-Dictionaries/Exercises/Sets-Of-Elements/SetsOfElements.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class SetsOfElements
+{
+    static void Main()
+    {
+        var tokens = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse)
+            .ToArray();
+        var firstSet = new HashSet<int>();
+        var secondSet = new HashSet<int>();
+
+        for (int i = 0; i < tokens[0]; i++)
+        {
+            var number = int.Parse(Console.ReadLine());
+            firstSet.Add(number);
+        }
+
+        for (int i = 0; i < tokens[1]; i++)
+        {
+            var number = int.Parse(Console.ReadLine());
+            secondSet.Add(number);
+        }
+
+        foreach (var item in firstSet)
+        {
+            if (secondSet.Contains(item))
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
