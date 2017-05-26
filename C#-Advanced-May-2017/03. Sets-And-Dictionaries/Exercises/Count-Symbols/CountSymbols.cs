845cd2a84d7f24a453c4c34fc932b3d4ff4af663
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2017-05-26 15:17:35 +0300

    04. Count Symbols
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/03. Sets-And-Dictionaries/Exercises/Count-Symbols/CountSymbols.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class CountSymbols
+{
+    static void Main()
+    {
+        var input = Console.ReadLine();
+        var container = new SortedDictionary<char, int>();
+
+        foreach (var character in input)
+        {
+            if (!container.ContainsKey(character))
+            {
+                container.Add(character, 1);
+            }
+            else
+            {
+                container[character]++;
+            }
+        }
+
+        foreach (var character in container.Keys)
+        {
+            Console.WriteLine($"{character}: {container[character]} time/s");
+        }
+    }
+}
