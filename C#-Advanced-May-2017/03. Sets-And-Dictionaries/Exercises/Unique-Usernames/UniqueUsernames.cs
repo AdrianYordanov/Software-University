e30a8d36f53455ab47b624a1395a4b5d80414b51
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2017-05-26 14:55:48 +0300

    01. Unique Usernames
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/03. Sets-And-Dictionaries/Exercises/Unique-Usernames/UniqueUsernames.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class UniqueUsernames
+{
+    static void Main()
+    {
+        var n = int.Parse(Console.ReadLine());
+        var users = new HashSet<string>();
+
+        for (int i = 0; i < n; i++)
+        {
+            var username = Console.ReadLine();
+            users.Add(username);
+        }
+
+        foreach (var username in users)
+        {
+            Console.WriteLine(username);
+        }
+    }
+}
