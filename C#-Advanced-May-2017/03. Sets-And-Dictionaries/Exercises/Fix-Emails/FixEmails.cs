24097fb4476a013a1dfc6d3db6c1834f509f6ec4
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2017-05-26 16:05:35 +0300

    07. Fix Emails
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/03. Sets-And-Dictionaries/Exercises/Fix-Emails/FixEmails.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class FixEmails
+{
+    static void Main()
+    {
+        var input = string.Empty;
+        var dictionary = new Dictionary<string, string>();
+
+        while ((input = Console.ReadLine()) != "stop")
+        {
+            var email = Console.ReadLine();
+            var domain = email.Substring(email.Length - 2);
+
+            if (domain.ToLower() == "us" || domain.ToLower() == "uk")
+            {
+                continue;
+            }
+
+            if (dictionary.ContainsKey(input))
+            {
+                dictionary[input] = email;
+            }
+            else
+            {
+                dictionary.Add(input, email);
+            }
+        }
+
+        foreach (var name in dictionary.Keys)
+        {
+            Console.WriteLine($"{name} –> {dictionary[name]}");
+        }
+    }
+}
