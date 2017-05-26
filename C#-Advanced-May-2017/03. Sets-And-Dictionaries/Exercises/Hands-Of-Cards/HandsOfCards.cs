8073ce01963277680f7171e70dedac710269d853
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2017-05-26 17:16:23 +0300

    08. Hands of Cards
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/03. Sets-And-Dictionaries/Exercises/Hands-Of-Cards/HandsOfCards.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class HandsOfCards
+{
+    private static string[] cards;
+    private static string[] multiplier;
+
+    static void Main()
+    {
+        var input = string.Empty;
+        var people = new Dictionary<string, HashSet<string>>();
+        cards = new string[] { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+        multiplier = new string[] { "C", "D", "H", "S" };
+
+        while ((input = Console.ReadLine()) != "JOKER")
+        {
+            var tokens = input.Split(':');
+            var player = tokens[0];
+            var playerCards = tokens[1].Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (people.ContainsKey(player))
+            {
+                people[player].UnionWith(playerCards);
+            }
+            else
+            {
+                people.Add(player, new HashSet<string>(playerCards));
+            }
+        }
+
+        foreach (var player in people.Keys)
+        {
+            var playerScore = CalculateScore(people[player]);
+            Console.WriteLine($"{player}: {playerScore}");
+        }
+    }
+
+    private static int CalculateScore(HashSet<string> playerCards)
+    {
+        var cardsScore = 0;
+
+        foreach (var card in playerCards)
+        {
+            var tempScore = (Array.IndexOf(cards, card.Remove(card.Length - 1)) + 2) *
+                (Array.IndexOf(multiplier, card[card.Length - 1].ToString()) + 1);
+            cardsScore += tempScore;
+        }
+
+        return cardsScore;
+    }
+}
