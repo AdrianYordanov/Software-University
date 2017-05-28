6d1fdf96bfeb89b3c44f469a55a7353c5d71f45e
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2017-05-28 17:19:47 +0300

    09. User Logs
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/04. Sets-And-Dictionaries-Exercises/Exercises/User-Logs/UserLogs.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class UserLogs
+{
+    static void Main()
+    {
+        var input = string.Empty;
+        var users = new SortedDictionary<string, Dictionary<string, int>>();
+
+        while ((input = Console.ReadLine()) != "end")
+        {
+            var tokens = input.Split(' ');
+            var ip = tokens[0].Split('=')[1];
+            var user = tokens[2].Split('=')[1];
+
+            if (users.ContainsKey(user))
+            {
+                if (users[user].ContainsKey(ip))
+                {
+                    users[user][ip]++;
+                }
+                else
+                {
+                    users[user].Add(ip, 1);
+                }
+            }
+            else
+            {
+                var userIps = new Dictionary<string, int>();
+                userIps.Add(ip, 1);
+                users.Add(user, userIps);
+            }
+        }
+
+        foreach (var name in users.Keys)
+        {
+            Console.WriteLine(name + ":");
+            var ips = users[name];
+
+            Console.WriteLine(string.Join(", ", ips.Select(x => x.Key + " => " + x.Value).ToArray()) + ".");
+        }
+    }
+}
