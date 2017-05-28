b2f8fab2268c5231dd65cd00d5fdf52cf2d7dbdb
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2017-05-28 20:32:37 +0300

    12. Legendary Farming
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/04. Sets-And-Dictionaries-Exercises/Exercises/Legendary-Farming/LegendaryFarming.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class LegendaryFarming
+{
+    static void Main()
+    {
+        var items = new Dictionary<string, string>();
+        var remainingMaterials = new Dictionary<string, int>();
+        var otherMaterials = new Dictionary<string, int>();
+        var winner = string.Empty;
+
+        items.Add("shards", "Shadowmourne");
+        items.Add("fragments", "Valanyr");
+        items.Add("motes", "Dragonwrath");
+
+        remainingMaterials.Add("shards", 0);
+        remainingMaterials.Add("fragments", 0);
+        remainingMaterials.Add("motes", 0);
+
+        while (winner == string.Empty)
+        {
+            var tokens = Console.ReadLine()
+                .ToLower()
+                .Split(' ');
+
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                var quantity = int.Parse(tokens[i]);
+                var material = tokens[++i];
+
+                switch (material)
+                {
+                    case "shards":
+                    case "fragments":
+                    case "motes":
+                        {
+                            remainingMaterials[material] += quantity;
+
+                            if (winner == string.Empty && remainingMaterials[material] >= 250)
+                            {
+                                winner = material;
+                                remainingMaterials[material] -= 250;
+                            }
+
+                            break;
+                        }
+                    default:
+                        {
+                            if (otherMaterials.ContainsKey(material))
+                            {
+                                otherMaterials[material] += quantity;
+                            }
+                            else
+                            {
+                                otherMaterials.Add(material, quantity);
+                            }
+
+                            break;
+                        }
+                }
+
+                if (winner != string.Empty)
+                {
+                    break;
+                }
+            }
+        }
+
+        Console.WriteLine($"{items[winner]} obtained!");
+
+        foreach (var material in remainingMaterials.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+        {
+            Console.WriteLine($"{material.Key}: {material.Value}");
+        }
+
+        foreach (var material in otherMaterials.OrderBy(x => x.Key))
+        {
+            Console.WriteLine($"{material.Key}: {material.Value}");
+        }
+    }
+}
