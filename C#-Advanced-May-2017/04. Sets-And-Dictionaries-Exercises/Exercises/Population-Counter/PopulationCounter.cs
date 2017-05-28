6820e8bbfc707fa385407fdaf6b6af0e8aad8db6
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2017-05-28 18:37:59 +0300

    10. Population Counter
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/04. Sets-And-Dictionaries-Exercises/Exercises/Population-Counter/PopulationCounter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class PopulationCounter
+{
+    static void Main()
+    {
+        var input = string.Empty;
+        var countries = new Dictionary<string, Dictionary<string, long>>();
+
+        while ((input = Console.ReadLine()) != "report")
+        {
+            var tokens = input.Split('|');
+            var city = tokens[0];
+            var country = tokens[1];
+            var cityPopulation = long.Parse(tokens[2]);
+
+            if (countries.ContainsKey(country))
+            {
+                countries[country].Add(city, cityPopulation);
+            }
+            else
+            {
+                var cities = new Dictionary<string, long>();
+                cities.Add(city, cityPopulation);
+                countries.Add(country, cities);
+            }
+        }
+
+        var orderedCountries = countries
+            .OrderByDescending(pair => pair.Value.Values.Sum())
+            .ToDictionary(x => x.Key, x => x.Value);
+
+        foreach (var pair in orderedCountries)
+        {
+            var country = pair.Key;
+            var cities = pair.Value;
+            var orderedCities = cities
+                .OrderByDescending(x => x.Value)
+                .ToDictionary(x => x.Key, x => x.Value);
+
+
+            Console.WriteLine($"{country} (total population: {orderedCities.Values.Sum()})");
+
+            foreach (var nestedPair in orderedCities)
+            {
+                var city = nestedPair.Key;
+                var cityPopulation = nestedPair.Value;
+                Console.WriteLine($"=>{city}: {cityPopulation}");
+            }
+        }
+    }
+}
