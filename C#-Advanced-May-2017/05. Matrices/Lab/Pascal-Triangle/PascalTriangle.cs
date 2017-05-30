81809ef89fd8e407b7c12791980e3b19bf70a685
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-05-30 22:19:11 +0300

    04. Pascal Triangle
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/05. Matrices/Lab/Pascal-Triangle/PascalTriangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class PascalTriangle
+{
+    static void Main()
+    {
+        var n = int.Parse(Console.ReadLine());
+        var triangle = new long[n][];
+
+        for (int i = 0; i < triangle.Length; i++)
+        {
+            triangle[i] = new long[i + 1];
+            triangle[i][0] = triangle[i][i] = 1;
+
+            for (int j = 1; j < triangle[i].Length - 1; j++)
+            {
+                triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+            }
+        }
+
+        for (int i = 0; i < triangle.Length; i++)
+        {
+            Console.WriteLine(string.Join(" ", triangle[i]));
+        }
+    }
+}
