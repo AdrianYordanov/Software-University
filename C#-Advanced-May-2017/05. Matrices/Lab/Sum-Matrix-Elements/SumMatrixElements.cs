c7a1c0ce56861df78940778b3b94b1a3417b08bf
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-05-30 19:21:44 +0300

    01. Sum Of All Elements Of Matrix
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/05. Matrices/Lab/Sum-Matrix-Elements/SumMatrixElements.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class SumMatrixElements
+{
+    static void Main()
+    {
+        var parameters = Console.ReadLine()
+            .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+        var sum = 0;
+        var rowsCount = int.Parse(parameters[0]);
+        var colsCount = int.Parse(parameters[1]);
+        var matrix = new int[rowsCount, colsCount];
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            var tokens = Console.ReadLine()
+                .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                matrix[i, j] = int.Parse(tokens[j]);
+            }
+        }
+
+        foreach (var number in matrix)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine(rowsCount);
+        Console.WriteLine(colsCount);
+        Console.WriteLine(sum);
+    }
+}
