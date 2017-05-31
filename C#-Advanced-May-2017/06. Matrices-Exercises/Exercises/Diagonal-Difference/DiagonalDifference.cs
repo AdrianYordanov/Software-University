67d41573cb3d8814dfe82ec95d9e3fe03f8dc40f
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-05-31 13:42:36 +0300

    02. Diagonal Difference
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/06. Matrices-Exercises/Exercises/Diagonal-Difference/DiagonalDifference.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class DiagonalDifference
+{
+    static void Main()
+    {
+        var n = int.Parse(Console.ReadLine());
+        var matrix = new int[n, n];
+        var primaryDiagonal = 0;
+        var secondaryDiagonal = 0;
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            var firstLineTokens = Console.ReadLine()
+                .Split(' ');
+
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                matrix[i, j] = int.Parse(firstLineTokens[j]);
+            }
+        }
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            primaryDiagonal += matrix[i, i];
+            secondaryDiagonal += matrix[i, (matrix.GetLength(0) - i) - 1];
+        }
+
+        Console.WriteLine(Math.Abs(primaryDiagonal - secondaryDiagonal));
+    }
+}
