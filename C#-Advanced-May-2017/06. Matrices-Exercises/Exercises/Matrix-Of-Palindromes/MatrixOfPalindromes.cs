11dc67359c2ca086bf773b431acb6a547ec46c0b
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-05-31 13:27:32 +0300

    01. Matrix of Palindromes
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/06. Matrices-Exercises/Exercises/Matrix-Of-Palindromes/MatrixOfPalindromes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+class MatrixOfPalindromes
+{
+    static void Main()
+    {
+        var tokens = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse)
+            .ToArray();
+        var matrix = new string[tokens[0], tokens[1]];
+        char[] alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+
+        for (int rowLetter = 0; rowLetter < matrix.GetLength(0); rowLetter++)
+        {
+            for (int colLetter = rowLetter, colIndex = 0; colIndex < matrix.GetLength(1); colLetter++, colIndex++)
+            {
+                matrix[rowLetter, colIndex] = $"{alphabet[rowLetter]}{alphabet[colLetter]}{alphabet[rowLetter]}";
+            }
+        }
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
