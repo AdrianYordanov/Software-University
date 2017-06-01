d1920ebfc604bc9e27d54eea8292f1af82b6e584
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-01 17:55:33 +0300

    01. Odd Lines

---
+++ C#-Advanced-May-2017/08. Streams-Exercise/Exercises/Odd-Lines/OddLines.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+class OddLines
+{
+    static void Main()
+    {
+        var reader = new StreamReader("../../text.txt");
+
+        using (reader)
+        {
+            var lineIndex = 0;
+            var line = string.Empty;
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (lineIndex % 2 != 0)
+                {
+                    Console.WriteLine(line);
+                }
+
+                ++lineIndex;
+            }
+        }
+    }
+}
