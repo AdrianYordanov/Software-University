1d965feab50e858501062ddf6dcb71c4670eac02
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-01 15:58:41 +0300

    03. Writing Text To File

---
+++ C#-Advanced-May-2017/07. Streams/Lab/Writing-To-File/WritingToFile.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Text;
+
+class WritingToFile
+{
+    static void Main()
+    {
+        string text = "Кирилица";
+        var fileStream = new FileStream("../../log.txt", FileMode.Create);
+
+        try
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+            fileStream.Write(bytes, 2, bytes.Length - 2);
+        }
+        finally
+        {
+            fileStream.Close();
+        }
+    }
+}
