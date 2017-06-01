6477c9f0124fbf9cf7be9cef5eb41f818a00b8a9
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-01 18:57:33 +0300

    03. Word Count

---
+++ C#-Advanced-May-2017/08. Streams-Exercise/Exercises/Word-Count/WordCount.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+class WordCount
+{
+    static void Main()
+    {
+        var words = new Dictionary<string, int>();
+
+        using (var wordReader = new StreamReader("../../words.txt"))
+        {
+            var line = string.Empty;
+
+            while ((line = wordReader.ReadLine()) != null)
+            {
+                var wordTokens = Regex
+                        .Split(line.ToLower(), @"\W+");
+
+
+                foreach (var word in wordTokens)
+                {
+                    words[word] = 0;
+                }
+            }
+
+            using (var textReader = new StreamReader("../../text.txt"))
+            {
+                line = string.Empty;
+
+                while ((line = textReader.ReadLine()) != null)
+                {
+                    var wordTokens = Regex
+                        .Split(line.ToLower(), @"\W+");
+
+
+
+                    foreach (var word in wordTokens)
+                    {
+                        if (words.ContainsKey(word))
+                        {
+                            words[word]++;
+                        }
+                    }
+                }
+            }
+
+            using (var writer = new StreamWriter("../../result.txt"))
+            {
+                var orderedWords = words.OrderByDescending(x => x.Value);
+
+                foreach (var pair in orderedWords)
+                {
+                    writer.WriteLine($"{pair.Key} - {pair.Value}");
+                }
+            }
+        }
+    }
+}
