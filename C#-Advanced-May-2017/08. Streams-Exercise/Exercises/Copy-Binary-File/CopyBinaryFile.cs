b326d168fb9d7cd2cdfbe3d5e250d716a7996546
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-01 20:04:53 +0300

    04. Copy Binary File

---
+++ C#-Advanced-May-2017/08. Streams-Exercise/Exercises/Copy-Binary-File/CopyBinaryFile.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+
+class CopyBinaryFile
+{
+    static void Main()
+    {
+        using (var file = new FileStream("../../logo.png", FileMode.Open))
+        {
+            using (var createFile = new FileStream("../../result.png", FileMode.Create))
+            {
+                var buffer = new byte[4096];
+                var countBytes = file.Read(buffer, 0, buffer.Length);
+
+                while (countBytes != 0)
+                {
+                    createFile.Write(buffer, 0, countBytes);
+                    countBytes = file.Read(buffer, 0, buffer.Length);
+                }
+            }
+        }
+    }
+}
