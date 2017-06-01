e6c8e695cfa2961fd96b2e5706bd3c7ebe2c84da
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-01 18:09:10 +0300

    02. Line Numbers

---
+++ C#-Advanced-May-2017/08. Streams-Exercise/Exercises/Line-Numbers/LineNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+
+class LineNumbers
+{
+    static void Main()
+    {
+        using (var reader = new StreamReader("../../text.txt"))
+        {
+            using (var writer = new StreamWriter("../../result.txt"))
+            {
+                var counter = 1;
+                var line = string.Empty;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    writer.WriteLine($"{counter}. {line}");
+                    ++counter;
+                }
+            }
+        }
+    }
+}
