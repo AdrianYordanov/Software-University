91883dcff65d868b4b116288c786da0529511765
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-06 19:08:56 +0300

    05. Concatenate Strings
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/09. Manual-String-Processing/Lab/Concatenate-Strings/ConcatenateStrings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+class ConcatenateStrings
+{
+    static void Main()
+    {
+        var n = int.Parse(Console.ReadLine());
+        var sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++)
+        {
+            var input = Console.ReadLine();
+            sb.Append(string.Concat(input, " "));
+        }
+
+        Console.WriteLine(sb);
+    }
+}
