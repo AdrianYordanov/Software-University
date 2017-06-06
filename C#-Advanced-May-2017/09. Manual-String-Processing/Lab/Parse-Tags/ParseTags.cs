ab59c17cbbf53c8d7fceec514295508d6fe70d6a
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-06 17:34:45 +0300

    03. Parse Tags
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/09. Manual-String-Processing/Lab/Parse-Tags/ParseTags.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class ParseTags
+{
+    static void Main()
+    {
+        var openCase = "<upcase>";
+        var closeCase = "</upcase>";
+        var input = Console.ReadLine();
+        var openIndex = input.IndexOf(openCase);
+
+        while (openIndex != -1)
+        {
+            var closeIndex = input.IndexOf(closeCase);
+            if (closeIndex == -1)
+                break;
+
+            var changeCase = input.Substring(openIndex, closeIndex - openIndex + closeCase.Length);
+            var upperCase = changeCase
+                .Replace(openCase, "")
+                .Replace(closeCase, "")
+                .ToUpper();
+
+            input = input.Replace(changeCase, upperCase);
+            openIndex = input.IndexOf(openCase);
+        }
+
+        Console.WriteLine(input);
+    }
+}
