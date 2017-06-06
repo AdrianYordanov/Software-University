490213dc241c382d7b62cd1241a0b83f4ea08427
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-06 16:12:45 +0300

    02. Parse URLs
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/09. Manual-String-Processing/Lab/Parse-Url/ParseUrl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class ParseUrl
+{
+    static void Main()
+    {
+        var url = Console.ReadLine();
+        var mainTokens = url.Split(new[] { "://" }, StringSplitOptions.RemoveEmptyEntries);
+
+        if (mainTokens.Length != 2 || mainTokens[1].IndexOf("/") < 0)
+        {
+            Console.WriteLine("Invalid URL");
+        }
+        else
+        {
+            var resourceIndex = mainTokens[1].IndexOf("/");
+            var protocol = mainTokens[0];
+            var server = mainTokens[1].Substring(0, resourceIndex);
+            var resource = mainTokens[1].Substring(resourceIndex + 1);
+
+            Console.WriteLine($"Protocol = {protocol}");
+            Console.WriteLine($"Server = {server}");
+            Console.WriteLine($"Resources = {resource}");
+        }
+    }
+}
