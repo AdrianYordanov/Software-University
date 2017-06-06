2a20764b9e24e1ffae7d82fcdacc66cd4efc51df
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-06 18:46:30 +0300

    04. Special Words
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/09. Manual-String-Processing/Lab/Special-Words/SpecialWords.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class SpecialWords
+{
+    static void Main()
+    {
+        var specialWords = Console.ReadLine().Split(' ');
+        var text = Console.ReadLine().Split(new[] { '(', ')', '[', ']', '<', '>', ',', '-', '!', '?', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        var words = new Dictionary<string, int>();
+
+        foreach (var currentSpecialWord in specialWords)
+        {
+            var counter = 0;
+            var foundIndex = 0;
+            var indexOfWord = Array.IndexOf(text, currentSpecialWord, foundIndex);
+
+            while (indexOfWord != -1)
+            {
+                ++counter;
+                foundIndex = indexOfWord;
+                indexOfWord = Array.IndexOf(text, currentSpecialWord, foundIndex + 1);
+            }
+
+            words.Add(currentSpecialWord, counter);
+        }
+
+        foreach (var pair in words)
+        {
+            Console.WriteLine($"{pair.Key} - {pair.Value}");
+        }
+    }
+}
