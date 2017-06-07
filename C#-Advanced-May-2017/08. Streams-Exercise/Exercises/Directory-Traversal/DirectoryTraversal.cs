e87af21224308f336a69d6a6bc029f7017f83067
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-07 18:40:51 +0300

    07. Directory Traversal

---
+++ C#-Advanced-May-2017/08. Streams-Exercise/Exercises/Directory-Traversal/DirectoryTraversal.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class DirectoryTraversal
+{
+    static void Main()
+    {
+        var folderPath = Console.ReadLine();
+        // folderPath = "../../obj/Debug/";
+        var foundFiles = Directory.GetFiles(folderPath);
+        var extensions = new Dictionary<string, Dictionary<string, double>>();
+
+        foreach (var file in foundFiles)
+        {
+            var fileName = Path.GetFileName(file);
+            var extension = fileName.Substring(fileName.IndexOf('.'));
+
+            if (extensions.ContainsKey(extension))
+            {
+                extensions[extension][fileName] = new FileInfo(file).Length / (double)1024;
+            }
+            else
+            {
+                var dictionary = new Dictionary<string, double>();
+                dictionary.Add(fileName, new FileInfo(file).Length / (double)1024);
+                extensions.Add(extension, dictionary);
+            }
+        }
+
+        var orderedExtensions = extensions
+            .OrderByDescending(pair => pair.Value.Count)
+            .ThenBy(pair => pair.Key);
+
+        using (var result = new StreamWriter(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/report.txt"))
+        {
+            foreach (var pair in orderedExtensions)
+            {
+                result.WriteLine(pair.Key);
+
+                foreach (var nestedPair in pair.Value.OrderBy(x => x.Value))
+                {
+                    result.WriteLine($"--{nestedPair.Key} - {nestedPair.Value}kb");
+                }
+            }
+        }
+    }
+}
