112b3237124f67896118aac94d5ea53f79ad536e
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-10 00:50:28 +0300

    11. Parking System
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/06. Matrices-Exercises/Exercises/Parking-System/ParkingSystem.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class ParkingSystem
+{
+    static void Main()
+    {
+        var mainTokens = Console.ReadLine().Split(' ');
+        var rowsCount = int.Parse(mainTokens[0]);
+        var colsCount = int.Parse(mainTokens[1]);
+        var parking = new Dictionary<int, HashSet<int>>();
+        var input = string.Empty;
+
+        while ((input = Console.ReadLine()) != "stop")
+        {
+            var spotCol = 0;
+            var tokens = input.Split(' ');
+            var enterRow = int.Parse(tokens[0]);
+            var initialRow = int.Parse(tokens[1]);
+            var initialCol = int.Parse(tokens[2]);
+
+
+            for (int colToLeft = initialCol, colToRight = initialCol;
+                colToLeft > 0 || colToRight < colsCount;
+                colToLeft--, colToRight++)
+            {
+                if (colToLeft > 0 && IsFreeSpot(parking, initialRow, colToLeft))
+                {
+                    spotCol = colToLeft;
+                    break;
+                }
+                else if (colToRight < colsCount && IsFreeSpot(parking, initialRow, colToRight))
+                {
+                    spotCol = colToRight;
+                    break;
+                }
+            }
+
+            if (spotCol != 0)
+            {
+                parking[initialRow].Add(spotCol);
+                Console.WriteLine(Math.Abs(enterRow - initialRow) + spotCol + 1);
+            }
+            else
+            {
+                Console.WriteLine($"Row {initialRow} full");
+            }
+        }
+    }
+
+    private static bool IsFreeSpot(Dictionary<int, HashSet<int>> parking, int row, int col)
+    {
+        if (parking.ContainsKey(row))
+        {
+            if (parking[row].Contains(col))
+            {
+                return false;
+            }
+        }
+        else
+        {
+            parking.Add(row, new HashSet<int>());
+        }
+
+        return true;
+    }
+}
