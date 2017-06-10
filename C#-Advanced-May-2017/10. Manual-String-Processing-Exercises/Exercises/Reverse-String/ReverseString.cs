e9101849eb5d06440d5f30e3e5f715094a558138
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-10 11:19:14 +0300

    01. Reverse String
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/10. Manual-String-Processing-Exercises/Exercises/Reverse-String/ReverseString.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class ReverseString
+{
+    static void Main()
+    {
+        var input = Console.ReadLine().ToCharArray();
+        Array.Reverse(input);
+        var result = new string(input);
+        Console.WriteLine(result);
+    }
+}
