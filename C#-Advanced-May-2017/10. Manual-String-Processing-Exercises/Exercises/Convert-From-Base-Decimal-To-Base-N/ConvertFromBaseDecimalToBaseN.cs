7a9adc9323ca16a5202f22545c40a517bf377257
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-10 18:00:10 +0300

    04. Convert from base-10 to base-N
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/10. Manual-String-Processing-Exercises/Exercises/Convert-From-Base-Decimal-To-Base-N/ConvertFromBaseDecimalToBaseN.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+
+class ConvertFromBaseDecimalToBaseN
+{
+    static void Main()
+    {
+        var tokens = Console.ReadLine().Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+        var nBase = int.Parse(tokens[0]);
+        var decimalNumber = BigInteger.Parse(tokens[1]);
+        var result = string.Empty;
+
+        while (decimalNumber > 0)
+        {
+            var remainder = decimalNumber % nBase;
+            decimalNumber = decimalNumber / nBase;
+            result = remainder + result;
+        }
+
+        Console.WriteLine(result == string.Empty ? "0" : result);
+    }
+}
