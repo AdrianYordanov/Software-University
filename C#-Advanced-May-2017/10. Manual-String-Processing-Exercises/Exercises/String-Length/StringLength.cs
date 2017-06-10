e67863b2ee2437e00e76c85585b98519a86d31cd
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-10 11:29:41 +0300

    02. String Length
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/10. Manual-String-Processing-Exercises/Exercises/String-Length/StringLength.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class StringLength
+{
+    static void Main()
+    {
+        var input = Console.ReadLine();
+        var length = input.Length <= 20 ? input.Length : 20;
+        input = input.Substring(0, length);
+        Console.WriteLine(input.PadRight(20, '*'));
+    }
+}
