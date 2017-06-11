3bfbd45070e1ff90bd52b1f20e204464f90e20c5
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-11 21:01:05 +0300

    16. Extract Hyperlinks
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/10. Manual-String-Processing-Exercises/Exercises/Extract-Hyperlinks/ExtractHyperlinks.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+class ExtractHyperlinks
+{
+    static void Main()
+    {
+        string line;
+        StringBuilder html = new StringBuilder();
+        while ((line = Console.ReadLine()) != "END")
+            html.Append(line);
+        string htmlStr = html.ToString();
+
+        Regex aPattern = new Regex(@"<\s*a\s+[^>]+?>", RegexOptions.IgnoreCase);
+
+        Regex hrefVal = new Regex(@"href\s*=\s*([\u0022'])(.+?)\1");
+        foreach (Match anchor in aPattern.Matches(htmlStr))
+        {
+            string anchorStr = anchor.Value;
+            Match m = hrefVal.Match(anchor.Value);
+            if (m.Success)
+                Console.WriteLine(m.Groups[2].Value);
+        }
+    }
+}
