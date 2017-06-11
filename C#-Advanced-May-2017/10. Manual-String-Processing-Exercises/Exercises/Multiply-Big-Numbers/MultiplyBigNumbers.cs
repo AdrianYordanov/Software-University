aecab0aa928ddc8011d887bed543e8ac4b4376e0
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-11 12:44:30 +0300

    08. Multiply big number
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/10. Manual-String-Processing-Exercises/Exercises/Multiply-Big-Numbers/MultiplyBigNumbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class MultiplyBigNumbers
+{
+    static void Main()
+    {
+        var firstNumber = Console.ReadLine().TrimStart(new Char[] { '0' });
+        var secondNumber = int.Parse(Console.ReadLine());
+        var result = string.Empty;
+        var remainder = 0;
+
+        for (int firstNumberIndex = firstNumber.Length - 1; firstNumberIndex >= 0 || remainder > 0; firstNumberIndex--)
+        {
+            var firstNumberDigit = firstNumberIndex >= 0 ? int.Parse(firstNumber[firstNumberIndex].ToString()) : 0;
+            var addDigit = (firstNumberDigit * secondNumber) + remainder;
+
+            if (addDigit > 9)
+            {
+                remainder = addDigit / 10;
+                addDigit = addDigit % 10;
+            }
+            else
+            {
+                remainder = 0;
+            }
+
+            result = addDigit + result;
+        }
+
+        result = result.TrimStart(new char[] { '0' });
+        result = result.Length == 0 ? "0" : result;
+        Console.WriteLine(result);
+    }
+}
