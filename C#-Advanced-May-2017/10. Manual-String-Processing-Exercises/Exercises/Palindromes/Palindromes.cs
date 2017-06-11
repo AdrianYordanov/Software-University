4dcfe2e05827fc9cf208236621fd54e1c298bc34
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-11 13:29:04 +0300

    11. Palindromes
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/10. Manual-String-Processing-Exercises/Exercises/Palindromes/Palindromes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Palindromes
+{
+    static void Main()
+    {
+        var words = Console.ReadLine().Split(new char[] { ' ', ',', '.', '?', '!' }, StringSplitOptions.RemoveEmptyEntries);
+        var palindromes = new SortedSet<string>();
+
+        foreach (var word in words)
+        {
+            var toArray = word.ToCharArray();
+            Array.Reverse(toArray);
+            var reversed = new string(toArray);
+
+            if (word.Equals(reversed))
+            {
+                palindromes.Add(word);
+            }
+        }
+
+        Console.WriteLine($"[{string.Join(", ", palindromes)}]");
+    }
+}
