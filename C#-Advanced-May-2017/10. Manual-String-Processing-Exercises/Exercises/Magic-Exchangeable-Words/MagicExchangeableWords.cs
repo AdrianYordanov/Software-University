4079555af0ba1a6a89f82d1b232845973ef3ac93
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-11 14:58:49 +0300

    13. Magic exchangeable words
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/10. Manual-String-Processing-Exercises/Exercises/Magic-Exchangeable-Words/MagicExchangeableWords.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MagicExchangeableWords
+{
+    static void Main()
+    {
+        var input = Console.ReadLine().Split(' ');
+        var firstUnique = new HashSet<char>();
+        var secondUnique = new HashSet<char>();
+
+        foreach (char character in input[0])
+        {
+            firstUnique.Add(character);
+        }
+
+        foreach (char character in input[1])
+        {
+            secondUnique.Add(character);
+        }
+
+        if (firstUnique.Count == secondUnique.Count)
+        {
+
+            Console.WriteLine("true");
+        }
+        else
+        {
+            Console.WriteLine("false");
+        }
+    }
+}
