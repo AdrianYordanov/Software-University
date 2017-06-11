b9378386357a0f17dac39db501d37f758c2f9a74
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-11 10:50:14 +0300

    06. Count Substring Occurrences
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/10. Manual-String-Processing-Exercises/Exercises/Count-Substring-Occurrences/CountSubstringOccurrences.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class CountSubstringOccurrences
+{
+    static void Main()
+    {
+        var text = Console.ReadLine().ToLower();
+        var searchString = Console.ReadLine().ToLower();
+
+        var foundIndex = -1;
+        var stringCounter = 0;
+
+        while ((foundIndex = text.IndexOf(searchString, foundIndex + 1)) != -1)
+        {
+            ++stringCounter;
+        }
+
+        Console.WriteLine(stringCounter);
+    }
+}
