37b1249999c24001271ece1ecdb13c6d71df418a
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-11 13:00:39 +0300

    09. Text Filter
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/10. Manual-String-Processing-Exercises/Exercises/Text-Filter/TextFilter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class TextFilter
+{
+    static void Main()
+    {
+        var bannedWords = Console.ReadLine().Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+        var text = Console.ReadLine();
+
+        foreach (var word in bannedWords)
+        {
+            text = text.Replace(word, new string('*', word.Length));
+        }
+
+        Console.WriteLine(text);
+    }
+}
