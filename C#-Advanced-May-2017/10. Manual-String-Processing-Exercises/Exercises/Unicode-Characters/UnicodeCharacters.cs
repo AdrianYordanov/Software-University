d07be1830493ce5f19d111b7d1ce5cd3150f6051
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-11 13:11:38 +0300

    10. Unicode Characters
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/10. Manual-String-Processing-Exercises/Exercises/Unicode-Characters/UnicodeCharacters.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+class UnicodeCharacters
+{
+    static void Main()
+    {
+        var line = Console.ReadLine();
+        var uniocdeArray = line.Select(x => string.Format("\\u{0:x4}", (int)x));
+        Console.WriteLine(string.Join(string.Empty, uniocdeArray));
+    }
+}
