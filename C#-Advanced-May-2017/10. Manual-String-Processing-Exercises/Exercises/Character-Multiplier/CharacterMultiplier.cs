7980d52e459f1c65960f1b63943f4541d7789274
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-11 13:49:58 +0300

    12. Character Multiplier
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/10. Manual-String-Processing-Exercises/Exercises/Character-Multiplier/CharacterMultiplier.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class CharacterMultiplier
+{
+    static void Main()
+    {
+        var tokens = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        var firstString = tokens[0];
+        var secondString = tokens[1];
+        var result = 0;
+
+        for (int i = 0; i < firstString.Length || i < secondString.Length; i++)
+        {
+            if (i < firstString.Length && i < secondString.Length)
+            {
+                result += MultiplyCharacters(firstString[i], secondString[i]);
+            }
+            else if (i < firstString.Length)
+            {
+                result += firstString[i];
+            }
+            else
+            {
+                result += secondString[i];
+            }
+        }
+
+        Console.WriteLine(result);
+    }
+
+    static int MultiplyCharacters(char first, char second)
+    {
+        return first * second;
+    }
+}
