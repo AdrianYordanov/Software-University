b57579a563c9fac89ffc368ba81f0c9ae86f338d
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-11 12:01:16 +0300

    07. Sum big numbers
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/10. Manual-String-Processing-Exercises/Exercises/Sum-Big-Numbers/SumBigNumbers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class SumBigNumbers
+{
+    static void Main()
+    {
+        var firstNumber = Console.ReadLine().TrimStart(new Char[] { '0' });
+        var secondNumber = Console.ReadLine().TrimStart(new Char[] { '0' });
+        var result = string.Empty;
+        var remainder = 0;
+
+        for (int firstNumberIndex = firstNumber.Length - 1, secondNumberIndex = secondNumber.Length - 1;
+            firstNumberIndex >= 0 || secondNumberIndex >= 0 || remainder > 0;
+            firstNumberIndex--, secondNumberIndex--)
+        {
+            var firstNumberDigit = firstNumberIndex >= 0 ? int.Parse(firstNumber[firstNumberIndex].ToString()) : 0;
+            var secondNumberDigit = secondNumberIndex >= 0 ? int.Parse(secondNumber[secondNumberIndex].ToString()) : 0;
+            var addDigit = firstNumberDigit + secondNumberDigit + remainder;
+
+            if (addDigit > 9)
+            {
+                remainder = addDigit / 10;
+                addDigit = addDigit % 10;
+            }
+            else
+            {
+                remainder = 0;
+            }
+
+            result = addDigit + result;
+        }
+
+        Console.WriteLine(result);
+    }
+}
