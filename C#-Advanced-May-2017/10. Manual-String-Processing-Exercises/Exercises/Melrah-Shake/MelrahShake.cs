f83aba74b55f23f61fdb90aa0a2429252fa08709
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-12 14:18:45 +0300

    15. Melrah Shake
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/10. Manual-String-Processing-Exercises/Exercises/Melrah-Shake/MelrahShake.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class MelrahShake
+{
+    static void Main()
+    {
+        var text = Console.ReadLine();
+        var pattern = Console.ReadLine();
+
+        while (pattern.Length > 0)
+        {
+            var leftMost = text.IndexOf(pattern);
+
+            if (leftMost != -1)
+            {
+                if (leftMost + pattern.Length > text.Length - 1)
+                {
+                    break;
+                }
+
+                var rightMost = text.LastIndexOf(pattern, leftMost + pattern.Length);
+
+                if (rightMost != -1)
+                {
+                    text = text.Remove(leftMost, pattern.Length);
+                    rightMost = text.LastIndexOf(pattern);
+                    text = text.Remove(rightMost, pattern.Length);
+                    pattern = pattern.Remove(pattern.Length / 2, 1);
+                    Console.WriteLine("Shaked it.");
+                }
+                else
+                {
+                    break;
+                }
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        Console.WriteLine("No shake.");
+        Console.WriteLine(text);
+    }
+}
