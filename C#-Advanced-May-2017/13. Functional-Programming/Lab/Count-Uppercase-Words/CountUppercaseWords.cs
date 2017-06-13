3fb4dbe88c20020bb42b5145d50b56ecfda5d39b
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-13 15:33:44 +0300

    03. Count Uppercase Words
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/13. Functional-Programming/Lab/Count-Uppercase-Words/CountUppercaseWords.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+class CountUppercaseWords
+{
+    static void Main()
+    {
+        Func<string, bool> isUpperCase = x => char.IsUpper(x[0]);
+        Console.ReadLine()
+            .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+            .Where(isUpperCase)
+            .ToList()
+            .ForEach(x => Console.WriteLine(x));
+    }
+}
