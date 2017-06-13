24e68e12c2226838977d2cf433575fb6226abc68
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-14 00:17:13 +0300

    Remove unneeded namespaces.

--- C#-Advanced-May-2017/11. Regular-Expressions/Lab/Valid-Time/ValidTime.cs
+++ C#-Advanced-May-2017/11. Regular-Expressions/Lab/Valid-Time/ValidTime.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 class ValidTime
 {
@@ -21,6 +17,6 @@ class ValidTime
     private static bool IsValidHour(string hourToString)
     {
         var hour = int.Parse(hourToString);
-        return hour < 12;
+        return hour <= 12;
     }
 }
