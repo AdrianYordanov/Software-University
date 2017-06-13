f4de3aab5add336997d363e673d068f6183cabe7
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-13 19:19:48 +0300

    02. Vowel Count
    Judge - 100/100.

--- C#-Advanced-May-2017/11. Regular-Expressions/Lab/Vowel-Count/VowelCount.cs
+++ C#-Advanced-May-2017/11. Regular-Expressions/Lab/Vowel-Count/VowelCount.cs
@@ -6,7 +6,7 @@ class VowelCount
     static void Main()
     {
         var text = Console.ReadLine();
-        var regex = new Regex("[aeiouy]", RegexOptions.IgnoreCase);
+        var regex = new Regex("[AEIOUYaeiouy]");
         var count = regex.Matches(text).Count;
         Console.WriteLine($"Vowels: {count}");
     }
