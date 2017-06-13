4fc6e0aa55226dc8b18b48e909a02f654f1a4ca9
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-13 16:06:30 +0300

    04. Add VAT
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/13. Functional-Programming/Lab/Add-VAT/AddVAT.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+class AddVAT
+{
+    static void Main()
+    {
+        Console.ReadLine()
+            .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(x => double.Parse(x) * 1.2)
+            .ToList()
+            .ForEach(x => Console.WriteLine($"{x:F2}"));
+    }
+}
