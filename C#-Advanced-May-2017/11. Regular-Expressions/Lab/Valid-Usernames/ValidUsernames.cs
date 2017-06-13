d8b93e8a768fddac3d49cb19f0184981c145776b
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-14 00:00:17 +0300

    06. Valid Usernames
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/11. Regular-Expressions/Lab/Valid-Usernames/ValidUsernames.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class ValidUsernames
+{
+    static void Main()
+    {
+        var regex = new Regex("^[a-zA-Z0-9_-]{3,16}$");
+        var input = string.Empty;
+
+        while ((input = Console.ReadLine()) != "END")
+        {
+            Console.WriteLine(regex.IsMatch(input) ? "valid" : "invalid");
+        }
+    }
+}
