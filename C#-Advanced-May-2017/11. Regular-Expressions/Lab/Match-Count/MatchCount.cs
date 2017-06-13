e0efe50b68dc9584a57db969094c9ce98ca48544
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-13 18:48:36 +0300

    01. Match Count
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/11. Regular-Expressions/Lab/Match-Count/MatchCount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+
+class MatchCount
+{
+    static void Main()
+    {
+        var pattern = Console.ReadLine();
+        var text = Console.ReadLine();
+        var regex = new Regex(pattern);
+        var match = regex.Match(text);
+        var counter = 0;
+
+        while (match.Success)
+        {
+            ++counter;
+            match = match.NextMatch();
+        }
+
+        Console.WriteLine(counter);
+    }
+}
