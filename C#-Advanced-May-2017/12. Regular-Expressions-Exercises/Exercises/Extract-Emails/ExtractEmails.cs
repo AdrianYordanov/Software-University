f562afae1b3bb0cd6bafaf2e61460823a8fdf763
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-13 23:12:36 +0300

    05. Extract Email
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/12. Regular-Expressions-Exercises/Exercises/Extract-Emails/ExtractEmails.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class ExtractEmails
+{
+    static void Main()
+    {
+        var text = Console.ReadLine();
+        string pattern = @"(?<=[\s])([a-zA-Z0-9]+([\.\-_]?[a-zA-Z0-9]+)*)@([a-zA-Z]+(\-?[a-zA-Z]+)*)(\.([a-zA-Z]+(\-?[a-zA-Z]+)*))+";
+        Regex regex = new Regex(pattern);
+        MatchCollection matches = regex.Matches(text);
+
+        foreach (Match match in matches)
+        {
+            Console.WriteLine(match.Value);
+        }
+    }
+}
