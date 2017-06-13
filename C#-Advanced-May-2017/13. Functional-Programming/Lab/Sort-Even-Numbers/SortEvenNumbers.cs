84e9d3271a7d2a5468aebded46839d1cc6779774
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-13 14:45:23 +0300

    01. Sort Even Numbers
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/13. Functional-Programming/Lab/Sort-Even-Numbers/SortEvenNumbers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+class SortEvenNumbers
+{
+    static void Main()
+    {
+        var sorted = Console.ReadLine()
+           .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+           .Select(int.Parse)
+           .Where(number => number % 2 == 0)
+           .OrderBy(x => x);
+        Console.WriteLine(string.Join(", ", sorted));
+    }
+}
