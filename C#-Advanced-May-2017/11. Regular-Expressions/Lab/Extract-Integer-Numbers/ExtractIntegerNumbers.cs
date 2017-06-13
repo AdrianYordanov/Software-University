b91b8c3f63690ec55d7ce4927422eada925ac342
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-13 23:44:08 +0300

    04. Extract Integer Numbers
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/11. Regular-Expressions/Lab/Extract-Integer-Numbers/ExtractIntegerNumbers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class ExtractIntegerNumbers
+{
+    static void Main()
+    {
+        var text = Console.ReadLine();
+        var regex = new Regex(@"\d+");
+        var numbers = regex.Matches(text);
+
+        foreach (Match matchedNumber in numbers)
+        {
+            Console.WriteLine(matchedNumber.Value);
+        }
+    }
+}
