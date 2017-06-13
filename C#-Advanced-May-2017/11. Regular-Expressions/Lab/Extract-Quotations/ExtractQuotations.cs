994cb7a1dfa9a778e3c1f7ba1bf960eac0e80a90
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-14 00:26:11 +0300

    08. Extract Quotations
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/11. Regular-Expressions/Lab/Extract-Quotations/ExtractQuotations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class ExtractQuotations
+{
+    static void Main()
+    {
+        var text = Console.ReadLine();
+        var regex = new Regex("('|\")(.*?)\\1");
+        var matches = regex.Matches(text);
+
+        foreach (Match match in matches)
+        {
+            Console.WriteLine(match.Groups[2]);
+        }
+    }
+}
