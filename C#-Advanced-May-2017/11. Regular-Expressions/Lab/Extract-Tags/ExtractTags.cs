792c4ef6d2b8ed61755f3453a41ee5659abf8cf0
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-13 23:52:17 +0300

    05. Extract Tags
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/11. Regular-Expressions/Lab/Extract-Tags/ExtractTags.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+class ExtractTags
+{
+    static void Main()
+    {
+        var tags = new List<string>();
+        var input = string.Empty;
+        var regex = new Regex("<.+?>");
+
+        while ((input = Console.ReadLine()) != "END")
+        {
+            var matches = regex.Matches(input);
+
+            foreach (Match matchedTag in matches)
+            {
+                tags.Add(matchedTag.Value);
+            }
+        }
+
+        foreach (var tag in tags)
+        {
+            Console.WriteLine(tag);
+        }
+    }
+}
