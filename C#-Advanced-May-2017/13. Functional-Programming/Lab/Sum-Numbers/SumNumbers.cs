7fdde5fafcb5d8d56f7d0e5f6f9c4ea6e810d677
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-13 15:04:29 +0300

    Remove unneed namespaces.

--- C#-Advanced-May-2017/13. Functional-Programming/Lab/Sum-Numbers/SumNumbers.cs
+++ C#-Advanced-May-2017/13. Functional-Programming/Lab/Sum-Numbers/SumNumbers.cs
@@ -1,16 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 class SumNumbers
 {
     static void Main()
     {
         var input = Console.ReadLine();
-        Func<string, int> count = x => x.Split(new char[] { ' ', ',' }).ToList().Count;
-        Func<string, int> sum = x => x.Split(new char[] { ' ', ',' }).Select(int.Parse).Sum();
+        Func<string, int> count = x => x.Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries).ToList().Count;
+        Func<string, int> sum = x => x.Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).Sum();
         Console.WriteLine(count(input));
         Console.WriteLine(sum(input));
     }
