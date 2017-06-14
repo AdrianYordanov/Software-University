e98ae286d195eb142d612474cab494024ca3acf3
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-15 01:19:05 +0300

    08. Extract Hyperlinks
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/12. Regular-Expressions-Exercises/Exercises/Extract-Hyperlinks/ExtractHyperlinks.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+class ExtractHyperlinks
+{
+    static void Main()
+    {
+        var input = string.Empty;
+        var html = new StringBuilder();
+        var regex = new Regex(@"<a\s+[^>]*href\s*=\s*((?<quotes>\""|')(?<res1>.+?)\k<quotes>|(?<res2>[^\""'][^\s>]+)).*?>");
+
+        while ((input = Console.ReadLine()) != "END")
+        {
+            html.Append(input);
+        }
+
+        var tags = regex.Matches(html.ToString());
+
+        foreach (Match matchedTag in tags)
+        {
+            var firstResult = matchedTag.Groups["res1"];
+            var secondResult = matchedTag.Groups["res2"];
+            Console.WriteLine(firstResult.Length == 0 ? secondResult : firstResult);
+        }
+    }
+}
