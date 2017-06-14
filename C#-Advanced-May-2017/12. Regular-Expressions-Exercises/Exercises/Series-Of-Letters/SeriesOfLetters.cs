a8de8ca400d998e472f6eed5d45e7b6401238dc4
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-14 11:12:38 +0300

    03. Series Of Letters
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/12. Regular-Expressions-Exercises/Exercises/Series-Of-Letters/SeriesOfLetters.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class SeriesOfLetters
+{
+    static void Main()
+    {
+        var text = Console.ReadLine();
+        var regex = new Regex(@"(.)\1+");
+        var result = regex.Replace(text, "$1");
+        Console.WriteLine(result);
+    }
+}
