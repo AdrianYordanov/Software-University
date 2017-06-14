5d4adb1f40a3ccca406c5ba304ad1542f0240ff8
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-14 10:43:06 +0300

    01. Match Full Name
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/12. Regular-Expressions-Exercises/Exercises/Match-Full-Name/MatchFullName.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+class MatchFullName
+{
+    static void Main()
+    {
+        var regex = new Regex(@"\b[A-Z][a-z]+ [A-Z][a-z]+\b");
+        var names = new List<string>();
+        var input = string.Empty;
+
+        while ((input = Console.ReadLine()) != "end")
+        {
+            var matches = regex.Matches(input);
+
+            foreach (Match matchedName in matches)
+            {
+                names.Add(matchedName.Value);
+            }
+        }
+
+        foreach (var name in names)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
