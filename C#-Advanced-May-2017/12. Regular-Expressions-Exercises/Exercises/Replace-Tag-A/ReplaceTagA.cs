58ef85796939ab5ab9c2f651e57d9e0123549975
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-14 11:29:30 +0300

    04. Replace a tag
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/12. Regular-Expressions-Exercises/Exercises/Replace-Tag-A/ReplaceTagA.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+class ReplaceTagA
+{
+    static void Main()
+    {
+        var html = new StringBuilder();
+        var regex = new Regex(@"<a(\s+.*)>(.*)<\/a>");
+        var input = string.Empty;
+
+        while ((input = Console.ReadLine()) != "end")
+        {
+            html.AppendLine(input);
+        }
+
+        var result = regex.Replace(html.ToString(), "[URL$1]$2[/URL]");
+        Console.WriteLine(result);
+    }
+}
