9d0c1b1541947ec4c65319123dead8c80020f7b8
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-14 12:21:27 +0300

    06. Sentence Extractor
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/12. Regular-Expressions-Exercises/Exercises/Sentence-Extractor/SentenceExtractor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+class SentenceExtractor
+{
+    static void Main()
+    {
+        var word = Console.ReadLine();
+        var text = Console.ReadLine();
+        var result = new List<string>();
+        var sentenceRegex = new Regex($@"[^.?!]*(?<=[.?\s!]){word}(?=[\s.?!])[^.?!]*[.?!]");
+        var sentences = sentenceRegex.Matches(text);
+
+        foreach (Match sentence in sentences)
+        {
+            result.Add(sentence.Value);
+        }
+
+        foreach (var sentence in result)
+        {
+            Console.WriteLine(sentence);
+        }
+    }
+}
