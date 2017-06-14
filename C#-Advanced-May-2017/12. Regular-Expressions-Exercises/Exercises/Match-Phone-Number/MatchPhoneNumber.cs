83338bb78f4ec2614ec8cc4d9f6f788b78a00821
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-14 11:05:34 +0300

    02. Match Phone Number
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/12. Regular-Expressions-Exercises/Exercises/Match-Phone-Number/MatchPhoneNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+class MatchPhoneNumber
+{
+    static void Main()
+    {
+        var regex = new Regex(@"(?<=^| )\+359( |\-)2\1\d{3}\1\d{4}\b");
+        var numbers = new List<string>();
+        var input = string.Empty;
+
+        while ((input = Console.ReadLine()) != "end")
+        {
+            var matches = regex.Matches(input);
+
+            foreach (Match matchedName in matches)
+            {
+                numbers.Add(matchedName.Value);
+            }
+        }
+
+        foreach (var number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
