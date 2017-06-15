2b6694be81069424efff529399625c26415d35c0
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-15 14:59:33 +0300

    02. Upper Strings
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/15. LINQ/Lab/Upper-Strings/UpperStrings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+class UpperStrings
+{
+    static void Main()
+    {
+        var result = Console.ReadLine()
+            .Split(' ')
+            .Select(character => character.ToUpper());
+        Console.WriteLine(string.Join(" ", result));
+    }
+}
