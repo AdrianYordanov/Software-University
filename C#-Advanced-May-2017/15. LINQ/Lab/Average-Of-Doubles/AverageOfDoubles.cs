d5d19783548cce4d8d95ac0200d5d3b95b8ccaa0
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-15 15:25:49 +0300

    04. Average of Doubles
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/15. LINQ/Lab/Average-Of-Doubles/AverageOfDoubles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+class AverageOfDoubles
+{
+    static void Main()
+    {
+        var result = Console.ReadLine()
+            .Split(' ')
+            .Select(double.Parse)
+            .Average();
+        Console.WriteLine($"{result:F2}");
+    }
+}
