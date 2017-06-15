6444b0209b3ebc04a3b34e7e318c71270c040fb3
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-15 15:58:29 +0300

    05. Min Even Number
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/15. LINQ/Lab/Min-Even-Number/MinEvenNumber.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+class MinEvenNumber
+{
+    static void Main()
+    {
+        var evenNumbers = Console.ReadLine()
+            .Split(' ')
+            .Select(double.Parse)
+            .Where(number => number % 2 == 0)
+            .ToList();
+        Console.WriteLine(evenNumbers.Count == 0 ? "No match" : $"{evenNumbers.Min():F2}");
+    }
+}
