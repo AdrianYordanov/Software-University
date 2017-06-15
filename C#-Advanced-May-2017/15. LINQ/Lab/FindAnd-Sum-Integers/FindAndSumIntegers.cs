e7781a592707132de630169bd989e3d76f7ae113
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-15 16:23:31 +0300

    06. Find and Sum Integers
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/15. LINQ/Lab/FindAnd-Sum-Integers/FindAndSumIntegers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+class FindAndSumIntegers
+{
+    static void Main()
+    {
+        long temp = 0;
+        var numbers = Console.ReadLine()
+            .Split(' ')
+            .Where(x => long.TryParse(x, out temp))
+            .Select(long.Parse)
+            .ToList();
+        Console.WriteLine(numbers.Count == 0 ? "No match" : numbers.Sum().ToString());
+    }
+}
