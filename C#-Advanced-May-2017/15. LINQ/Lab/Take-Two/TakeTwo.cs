a10714c69c6bead4beff78cf74d87b0f3424de63
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-15 14:48:44 +0300

    01. Take Two
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/15. LINQ/Lab/Take-Two/TakeTwo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+class TakeTwo
+{
+    static void Main()
+    {
+        var result = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse)
+            .Where(number => number >= 10 && number <= 20)
+            .Take(2)
+            .ToList();
+        Console.WriteLine(string.Join(" ", result));
+    }
+}
