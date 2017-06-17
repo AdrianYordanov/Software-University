7d591e7bd25b35fe3fd566f35db52720d294ee9c
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-17 20:07:04 +0300

    09. Query Mess
    Judge  - 100/100.

---
+++ C#-Advanced-May-2017/12. Regular-Expressions-Exercises/Exercises/Query-Mess/QueryMess.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+class QueryMess
+{
+    static void Main()
+    {
+        var line = string.Empty;
+        var result = new StringBuilder();
+        var regex = new Regex(@"(?<=&|^)(.*?)=(.*?)(?=&|$)");
+
+        while ((line = Console.ReadLine()) != "END")
+        {
+            var linkTokens = line.Split('?');
+
+            if (linkTokens.Length > 1)
+            {
+                line = linkTokens[1];
+            }
+
+            var pairs = new Dictionary<string, List<string>>();
+            var matchedPairs = regex.Matches(line);
+
+            foreach (Match currentPair in matchedPairs)
+            {
+                var key = currentPair.Groups[1].ToString();
+                var value = currentPair.Groups[2].ToString();
+                key = Regex.Replace(key, @"(%20|\+)", " ");
+                key = Regex.Replace(key, @"[ \s]+", " ").Trim();
+                value = Regex.Replace(value, @"(%20|\+)", " ");
+                value = Regex.Replace(value, @"[ \s]+", " ").Trim();
+
+                if (!pairs.ContainsKey(key))
+                {
+                    pairs.Add(key, new List<string>());
+                }
+
+                pairs[key].Add(value);
+            }
+
+            foreach (var pair in pairs)
+            {
+                result.Append($"{pair.Key}=[{string.Join(", ", pair.Value)}]");
+            }
+
+            result.AppendLine();
+        }
+
+        Console.Write(result);
+    }
+}
