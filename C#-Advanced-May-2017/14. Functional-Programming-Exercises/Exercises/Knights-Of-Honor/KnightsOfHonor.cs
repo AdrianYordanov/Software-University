67052818ecfec9120e38c599f5c30902d0efa736
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-19 22:21:17 +0300

    02. Knights of Honor
    Judge -  100/100.

---
+++ C#-Advanced-May-2017/14. Functional-Programming-Exercises/Exercises/Knights-Of-Honor/KnightsOfHonor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+class KnightsOfHonor
+{
+    static void Main()
+    {
+        Action<string> print = (inputString) =>
+        {
+            var result = inputString.Split(' ')
+            .Select(x => x = $"Sir {x}");
+            Console.WriteLine(string.Join(Environment.NewLine, result));
+        };
+
+        print(Console.ReadLine());
+    }
+}
