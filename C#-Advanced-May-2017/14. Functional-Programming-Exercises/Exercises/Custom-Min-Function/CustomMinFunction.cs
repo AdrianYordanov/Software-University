885a4073ccb59dc0ad2eda26f4eebdad54b66d39
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-19 22:31:10 +0300

    03. Custom Min Function
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/14. Functional-Programming-Exercises/Exercises/Custom-Min-Function/CustomMinFunction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+class CustomMinFunction
+{
+    static void Main()
+    {
+        Func<int[], int> customMinFunc = (numArray) =>
+        {
+            var min = int.MaxValue;
+
+            for (int i = 0; i < numArray.Length; i++)
+            {
+                if (min > numArray[i])
+                {
+                    min = numArray[i];
+                }
+            }
+
+            return min;
+        };
+
+        var input = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse)
+            .ToArray();
+        Console.WriteLine(customMinFunc(input));
+    }
+}
