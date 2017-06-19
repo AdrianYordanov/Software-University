c0b761a08a661763dec3a9ee89fa8afee7a4b6ef
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-19 22:57:36 +0300

    04. Find Evens or Odds
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/14. Functional-Programming-Exercises/Exercises/Find-Evens-Or-Odds/FindEvensOrOdds.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class FindEvensOrOdds
+{
+    static void Main()
+    {
+        var input = Console.ReadLine().Split(' ');
+        var left = int.Parse(input[0]);
+        var right = int.Parse(input[1]);
+        var parameter = Console.ReadLine();
+
+        Console.WriteLine(string.Join(" ", CreateFunction(parameter)(left, right)));
+    }
+
+    private static Func<int, int, int[]> CreateFunction(string paramater)
+    {
+        return (left, right) =>
+        {
+            var list = new List<int>();
+
+            for (int counter = left; counter <= right; counter++)
+            {
+                list.Add(counter);
+            }
+
+            switch (paramater)
+            {
+                case "odd": return list.Where(number => number % 2 != 0).ToArray();
+                case "even": return list.Where(number => number % 2 == 0).ToArray();
+                default: throw new Exception("Wrong paramater");
+            }
+        };
+    }
+}
