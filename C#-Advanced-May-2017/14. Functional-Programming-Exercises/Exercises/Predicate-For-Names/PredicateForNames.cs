63b337c7092ba2680d79ba58aaa623dd58ca4ca9
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-20 13:22:32 +0300

    07. Predicate For Names
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/14. Functional-Programming-Exercises/Exercises/Predicate-For-Names/PredicateForNames.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+class PredicateForNames
+{
+    static void Main()
+    {
+        var length = int.Parse(Console.ReadLine());
+        var names = Console.ReadLine()
+            .Split(' ')
+            .ToList();
+
+        Predicate<string> checkName = (name) =>
+        {
+            return name.Length <= length;
+        };
+
+        var result = names.Where(name => checkName(name));
+        Console.WriteLine(string.Join(Environment.NewLine, result));
+    }
+}
