40ad8dcfcdb2143e89692f2cb7f3c9da68daee3c
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-20 15:23:07 +0300

    08. Custom Comparator
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/14. Functional-Programming-Exercises/Exercises/Custom-Comparator/CustomComparator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class CustomComparator
+{
+    static void Main()
+    {
+        var numbers = Console.ReadLine().Split(' ')
+            .Select(int.Parse)
+            .ToArray();
+        var evenOddComparator = new EvenOddComparer();
+        Array.Sort(numbers, evenOddComparator);
+        Console.WriteLine(string.Join(" ", numbers));
+    }
+
+    private class EvenOddComparer : IComparer<int>
+    {
+        public int Compare(int a, int b)
+        {
+            if (IsEven(a) && !IsEven(b))
+            {
+                return -1;
+            }
+            else if (!IsEven(a) && IsEven(b))
+            {
+                return 1;
+            }
+
+            if (a >= b)
+            {
+                return 1;
+            }
+
+            return -1;
+        }
+
+        private bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
+    }
+}
