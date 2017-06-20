eea363f7143a78c7a40a4582304ad17db33858e1
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-20 12:55:22 +0300

    05. Applied Arithmetics
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/14. Functional-Programming-Exercises/Exercises/Applied-Arithmetics/AppliedArithmetics.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class AppliedArithmetics
+{
+    static void Main()
+    {
+        var numbers = Console.ReadLine().Split(' ')
+            .Select(int.Parse)
+            .ToList();
+        var line = string.Empty;
+
+        while ((line = Console.ReadLine()) != "end")
+        {
+            switch (line)
+            {
+                case "add": Add(numbers); break;
+                case "subtract": Subtract(numbers); break;
+                case "multiply": Multiply(numbers); break;
+                case "print": Print(numbers); break;
+            }
+        }
+    }
+
+    private static void Add(List<int> numbers)
+    {
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            numbers[i]++;
+        }
+    }
+
+    private static void Subtract(List<int> numbers)
+    {
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            numbers[i]--;
+        }
+    }
+
+    private static void Multiply(List<int> numbers)
+    {
+
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            numbers[i] *= 2;
+        }
+    }
+
+    private static void Print(List<int> numbers)
+    {
+        Console.WriteLine(string.Join(" ", numbers));
+    }
+}
