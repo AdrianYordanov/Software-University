3a50f569ebccee43645c79f381a7ff4b1014d5f1
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-20 12:38:59 +0300

    Find even or odds.
    New solution with Predicate.

--- C#-Advanced-May-2017/14. Functional-Programming-Exercises/Exercises/Find-Evens-Or-Odds/FindEvensOrOdds.cs
+++ C#-Advanced-May-2017/14. Functional-Programming-Exercises/Exercises/Find-Evens-Or-Odds/FindEvensOrOdds.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 class FindEvensOrOdds
 {
@@ -10,27 +9,33 @@ class FindEvensOrOdds
         var left = int.Parse(input[0]);
         var right = int.Parse(input[1]);
         var parameter = Console.ReadLine();
+        var numbers = new List<int>();
+        var checkFunc = CreateFunction(parameter);
 
-        Console.WriteLine(string.Join(" ", CreateFunction(parameter)(left, right)));
+        for (int i = left; i <= right; i++)
+        {
+            if (checkFunc(i))
+            {
+                numbers.Add(i);
+            }
+        }
+
+        Console.WriteLine(string.Join(" ", numbers));
     }
 
-    private static Func<int, int, int[]> CreateFunction(string paramater)
+    private static Predicate<int> CreateFunction(string paramater)
     {
-        return (left, right) =>
+        if (paramater == "even")
         {
-            var list = new List<int>();
-
-            for (int counter = left; counter <= right; counter++)
-            {
-                list.Add(counter);
-            }
-
-            switch (paramater)
-            {
-                case "odd": return list.Where(number => number % 2 != 0).ToArray();
-                case "even": return list.Where(number => number % 2 == 0).ToArray();
-                default: throw new Exception("Wrong paramater");
-            }
-        };
+            return number => number % 2 == 0;
+        }
+        else if (paramater == "odd")
+        {
+            return number => number % 2 != 0;
+        }
+        else
+        {
+            throw new Exception("Invalid parameter.");
+        }
     }
 }
