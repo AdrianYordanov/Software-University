93ca98d8ea035422606b523079e44844da6879e5
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-20 16:02:08 +0300

    09. List Of Predicates
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/14. Functional-Programming-Exercises/Exercises/List-Of-Predicates/ListOfPredicates.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+class ListOfPredicates
+{
+    static void Main()
+    {
+        var n = int.Parse(Console.ReadLine());
+        var sequence = Console.ReadLine().Split(' ');
+        var predicates = sequence
+            .Select(number => (Predicate<int>)(x => x % int.Parse(number) == 0))
+            .ToArray();
+
+        for (int candidateNumber = 1; candidateNumber <= n; candidateNumber++)
+        {
+            bool isDivisible = true;
+
+            foreach (var predicate in predicates)
+            {
+                if (!predicate(candidateNumber))
+                {
+                    isDivisible = false;
+                    break;
+                }
+            }
+
+            if (isDivisible)
+            {
+                Console.Write($"{candidateNumber} ");
+            }
+        }
+    }
+}
