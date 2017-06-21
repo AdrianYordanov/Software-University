42c8830e0584e341b326f92c0fee1c344173b77e
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-21 18:38:00 +0300

    13. TriFunction
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/14. Functional-Programming-Exercises/Exercises/TriFunction/TriFunction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class TriFunction
+{
+    static void Main()
+    {
+        Func<string, int, bool> isCorrectName = (name, checkLength) =>
+        {
+            var sum = 0;
+
+            foreach (var character in name)
+            {
+                sum += character;
+            }
+
+            return sum >= checkLength;
+        };
+
+        Func<Func<string, int, bool>, string, int, string> getName = (condtionFunc, namesAsString, checkLength) =>
+        {
+            return namesAsString.Split(' ')
+            .FirstOrDefault(name => condtionFunc(name, checkLength));
+        };
+
+        var length = int.Parse(Console.ReadLine());
+        Console.WriteLine(getName(isCorrectName, Console.ReadLine(), length));
+
+    }
+}
