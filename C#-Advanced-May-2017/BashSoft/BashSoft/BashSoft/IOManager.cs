93086ca2d296c4ba2b4b881c589ecc62359edc69
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-23 12:39:23 +0300

    BashSoft
    First part done: Stacks-And-Queues

---
+++ C#-Advanced-May-2017/BashSoft/BashSoft/BashSoft/IOManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BashSoft
+{
+    public static class IOManager
+    {
+        public static void TraverseDirectory(string path)
+        {
+            OutputWriter.WriteEmptyLine();
+            var initialIdentation = path.Split('\\').Length;
+            var subFolders = new Queue<string>();
+            subFolders.Enqueue(path);
+
+            while (subFolders.Count != 0)
+            {
+                var currentPath = subFolders.Dequeue();
+                int identation = currentPath.Split('\\').Length - initialIdentation;
+                OutputWriter.WriteMessageOnNewLine(string.Format("{0}{1}", new string('-', identation), currentPath));
+
+                foreach (var directoryPath in Directory.GetDirectories(currentPath))
+                {
+                    subFolders.Enqueue(directoryPath);
+                }
+            }
+        }
+    }
+}
