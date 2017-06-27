2d6865669e3a843c038739e384a4d4dc0283b0da
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-27 17:25:37 +0300

    02.BankAccountMethods
    Judge - 100/100.

---
+++ C#-OOP-Basics-June-2017/01. Defining-Classes/Lab/Bank-Account-Methods/BankAccount.cs
@@ -0,0 +1,43 @@
+﻿class BankAccount
+{
+    private int id;
+    private double balance;
+
+    public int ID
+    {
+        get
+        {
+            return this.id;
+        }
+        set
+        {
+            this.id = value;
+        }
+    }
+    public double Balance
+    {
+        get
+        {
+            return this.balance;
+        }
+        set
+        {
+            this.balance = value;
+        }
+    }
+
+    public void Deposit(double amount)
+    {
+        this.Balance += amount;
+    }
+
+    public void Withdraw(double amount)
+    {
+        this.Balance -= amount;
+    }
+
+    public override string ToString()
+    {
+        return $"Account {this.ID}, balance {this.Balance}";
+    }
+}
---
+++ C#-OOP-Basics-June-2017/01. Defining-Classes/Lab/Bank-Account-Methods/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class StartUp
+{
+    static void Main()
+    {
+        var account = new BankAccount();
+        account.ID = 1;
+        account.Deposit(15);
+        account.Withdraw(5);
+        Console.WriteLine(account);
+    }
+}
