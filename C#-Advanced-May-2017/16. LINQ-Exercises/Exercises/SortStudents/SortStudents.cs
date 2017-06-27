6376eb3eb82e3ee5cdce07e36bdb2cf0b2784e58
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-27 14:41:46 +0300

    04. Sort Students
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/16. LINQ-Exercises/Exercises/SortStudents/SortStudents.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class SortStudents
+{
+    static void Main()
+    {
+        var input = string.Empty;
+        var students = new List<string[]>();
+
+        while ((input = Console.ReadLine()) != "END")
+        {
+            students.Add(input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+        }
+
+        var result = students.
+            OrderBy(s => s[1]).ThenByDescending(s => s[0]);
+
+        foreach (var student in result)
+        {
+            Console.WriteLine($"{student[0]} {student[1]}");
+        }
+    }
+}
