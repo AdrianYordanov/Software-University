e87852e0ab1732982fe84de45ee7f6e5085623f1
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-27 14:48:17 +0300

    06. Filter Students by Phone
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/16. LINQ-Exercises/Exercises/Filter-Students-By-Phone/FilterStudentsByPhone.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class FilterStudentsByPhone
+{
+    static void Main()
+    {
+        var input = Console.ReadLine();
+        var students = new List<string[]>();
+
+        while ((input = Console.ReadLine()) != "END")
+        {
+            students.Add(input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+        }
+
+        var result = students
+            .Where(s => s[2].StartsWith("02") || s[2].StartsWith("+3592"));
+
+        foreach (var student in result)
+        {
+            Console.WriteLine($"{student[0]} {student[1]}");
+        }
+    }
+}
