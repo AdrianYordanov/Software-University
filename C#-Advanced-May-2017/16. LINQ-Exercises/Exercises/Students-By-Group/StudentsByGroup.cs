f990936f577c75dd2d1898223e2a23855467cbc9
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-27 13:20:53 +0300

    01. Students by Group
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/16. LINQ-Exercises/Exercises/Students-By-Group/StudentsByGroup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class StudentsByGroup
+{
+    static void Main()
+    {
+        var listOfStudents = new List<string[]>();
+        var input = string.Empty;
+
+        while ((input = Console.ReadLine()) != "END")
+        {
+            listOfStudents.Add(input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+        }
+
+        var output = listOfStudents
+            .Where(s => s[2] == "2")
+            .OrderBy(s => s[0])
+            .Select(s => s[0] + " " + s[1]);
+
+        Console.WriteLine(string.Join(Environment.NewLine, output));
+    }
+}
