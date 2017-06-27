07d8e40a008eeb0a553b7bb24de6f8d72221ba87
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-27 14:44:41 +0300

    05. Filter Students by Email Domain

---
+++ C#-Advanced-May-2017/16. LINQ-Exercises/Exercises/Filter-Students-By-Email-Domain/FilterStudentsByEmailDomain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class FilterStudentsByEmailDomain
+{
+    static void Main()
+    {
+        var input = string.Empty;
+        var students = new List<string[]>();
+
+        while ((input = Console.ReadLine()) != "END")
+        {
+            students.Add(input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+        }
+
+        var result = students
+            .Where(s => s[2]
+            .Contains("@gmail.com"));
+
+        foreach (var student in result)
+        {
+            Console.WriteLine($"{student[0]} {student[1]}");
+        }
+    }
+}
