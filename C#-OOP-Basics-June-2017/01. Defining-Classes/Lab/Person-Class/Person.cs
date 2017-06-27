3ed904401ac6382cea248baa21770159e6f37ce1
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-27 18:37:15 +0300

    04. Person Class
    Judge - 100/100.

---
+++ C#-OOP-Basics-June-2017/01. Defining-Classes/Lab/Person-Class/Person.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+class Person
+{
+    private string name;
+    private int age;
+    private List<BankAccount> accounts;
+
+    public Person(string name, int age, List<BankAccount> accounts)
+    {
+        this.Name = name;
+        this.Age = age;
+        this.accounts = accounts;
+    }
+
+    public Person(string name, int age)
+        : this(name, age, new List<BankAccount>())
+    { }
+
+    public string Name
+    {
+        get
+        {
+            return this.name;
+        }
+        set
+        {
+            this.name = value;
+        }
+    }
+
+    public int Age
+    {
+        get
+        {
+            return this.age;
+        }
+        set
+        {
+            this.age = value;
+        }
+    }
+
+    public double GetBalance()
+    {
+        return this.accounts
+            .Sum(account => account.Balance);
+    }
+}
