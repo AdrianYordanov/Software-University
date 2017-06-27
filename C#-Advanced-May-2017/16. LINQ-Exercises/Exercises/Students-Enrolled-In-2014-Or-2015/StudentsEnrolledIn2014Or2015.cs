bbb584be3ee5072c84f844edb84a48a3164b0983
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-27 15:12:46 +0300

    09. Students Enrolled in 2014 or 2015
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/16. LINQ-Exercises/Exercises/Students-Enrolled-In-2014-Or-2015/StudentsEnrolledIn2014Or2015.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class StudentsEnrolledIn2014Or2015
+{
+    static void Main()
+    {
+        var input = string.Empty;
+        var students = new List<string[]>();
+
+        while ((input = Console.ReadLine()) != "END")
+        {
+            students.Add(input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+        }
+
+        var result = students
+            .Where(s => s[0].EndsWith("14") || s[0].EndsWith("15"));
+
+        foreach (var student in result)
+        {
+            Console.WriteLine(string.Join(" ", student.Skip(1)));
+        }
+    }
+}
