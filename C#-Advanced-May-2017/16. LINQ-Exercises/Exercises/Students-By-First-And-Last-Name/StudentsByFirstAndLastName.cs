84df2ecc2a375cb95415184d783fe3f7d8e3e3b0
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-27 14:34:21 +0300

    02. Students by First and Last Name
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/16. LINQ-Exercises/Exercises/Students-By-First-And-Last-Name/StudentsByFirstAndLastName.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class StudentsByFirstAndLastName
+{
+    static void Main()
+    {
+        var input = string.Empty;
+        var studentNames = new List<string[]>();
+
+        while ((input = Console.ReadLine()) != "END")
+        {
+            studentNames.Add(input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+        }
+
+        var result = studentNames
+            .Where(s => s[0].CompareTo(s[1]) < 0);
+
+        foreach (var student in result)
+        {
+            Console.WriteLine($"{student[0]} {student[1]}");
+        }
+    }
+}
