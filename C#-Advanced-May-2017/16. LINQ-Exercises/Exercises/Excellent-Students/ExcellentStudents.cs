e012bc8dff71f501de4d8ffd4b7a212b6f6a2ef0
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-27 14:51:21 +0300

    07. Excellent Students
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/16. LINQ-Exercises/Exercises/Excellent-Students/ExcellentStudents.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class ExcellentStudents
+{
+    static void Main()
+    {
+        var input = Console.ReadLine();
+        var students = new List<string[]>();
+
+        while ((input = Console.ReadLine()) != "END")
+        {
+            students.Add(input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+        }
+
+        var result = students
+            .Where(s => s
+            .Skip(2)
+            .Any(n => n == "6"));
+
+        foreach (var student in result)
+        {
+            Console.WriteLine($"{student[0]} {student[1]}");
+        }
+    }
+}
