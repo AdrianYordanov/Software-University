01ed7b84faf8971dc2c79b23dad88b6b0bfdcb75
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-27 15:29:02 +0300

    12. Little John
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/16. LINQ-Exercises/Exercises/Little-John/LittleJohn.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+class LittleJohn
+{
+    static void Main()
+    {
+        var largeArrow = ">>>----->>";
+        var mediumArrow = ">>----->";
+        var smallArrow = ">----->";
+        var input = string.Empty;
+
+        for (int i = 0; i < 4; i++)
+        {
+            input += " " + Console.ReadLine();
+        }
+
+        var countLarge = GetCountOfString(input, largeArrow);
+        input = input.Replace(largeArrow, " ");
+
+        var countMedium = GetCountOfString(input, mediumArrow);
+        input = input.Replace(mediumArrow, " ");
+
+        var countSmall = GetCountOfString(input, smallArrow);
+        input = input.Replace(smallArrow, " ");
+
+        var numberDec = countSmall.ToString() + countMedium.ToString() + countLarge.ToString();
+
+        var numberBin = Convert.ToString(Convert.ToInt64(numberDec, 10), 2);
+
+        numberBin = numberBin + ReverseString(numberBin);
+        var result = Convert.ToString(Convert.ToInt64(numberBin, 2), 10);
+
+        Console.WriteLine(result);
+    }
+
+    public static int GetCountOfString(string stringToCheckIn, string stringToLookFor)
+    {
+        var count = 0;
+        var index = stringToCheckIn.IndexOf(stringToLookFor);
+
+        while (index >= 0)
+        {
+            count++;
+            index = stringToCheckIn.IndexOf(stringToLookFor, index + stringToLookFor.Length);
+        }
+
+        return count;
+    }
+
+    public static string ReverseString(string inputString)
+    {
+        var outputString = "";
+
+        for (int i = 0; i < inputString.Length; i++)
+        {
+            outputString = inputString[i] + outputString;
+        }
+
+        return outputString;
+    }
+}
