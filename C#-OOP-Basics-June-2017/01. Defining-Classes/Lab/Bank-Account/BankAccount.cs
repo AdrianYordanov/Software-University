ca8889cbdb1ac53f6b4cab4712eab66249adbedf
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-27 17:35:57 +0300

    01. Bank Account
    Judge - 100/100.

---
+++ C#-OOP-Basics-June-2017/01. Defining-Classes/Lab/Bank-Account/BankAccount.cs
@@ -0,0 +1,29 @@
+﻿class BankAccount
+{
+    private int id;
+    private double balance;
+
+    public int ID
+    {
+        get
+        {
+            return this.id;
+        }
+        set
+        {
+            this.id = value;
+        }
+    }
+
+    public double Balance
+    {
+        get
+        {
+            return this.balance;
+        }
+        set
+        {
+            this.balance = value;
+        }
+    }
+}
---
+++ C#-OOP-Basics-June-2017/01. Defining-Classes/Lab/Bank-Account/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class StartUp
+{
+    static void Main()
+    {
+        var account = new BankAccount();
+        account.ID = 1;
+        account.Balance = 15;
+        Console.WriteLine($"Account {account.ID}, balance {account.Balance}");
+    }
+}
