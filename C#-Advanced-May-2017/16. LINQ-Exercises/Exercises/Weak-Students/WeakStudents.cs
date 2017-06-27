6a0bfc4eaaf3bdbba9f570df5a1c4a3c29e9d316
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-27 15:08:20 +0300

    08. WeakStudents
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/16. LINQ-Exercises/Exercises/Weak-Students/WeakStudents.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class WeakStudents
+{
+    static void Main()
+    {
+        var input = string.Empty;
+        List<string[]> students = new List<string[]>();
+
+        while ((input = Console.ReadLine()) != "END")
+        {
+            students.Add(input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+        }
+
+        var result = students
+            .Where(s => s
+            .Skip(2)
+            .Count(m => int.Parse(m) <= 3) >= 2);
+
+        foreach (var student in result)
+        {
+            Console.WriteLine($"{student[0]} {student[1]}");
+        }
+    }
+}
