7973da0e1fe6ce51e28058ac1266cd9a44dbc0a7
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-27 14:38:21 +0300

    03. Students by Age
    Judge - 100/100.

---
+++ C#-Advanced-May-2017/16. LINQ-Exercises/Exercises/Students-By-Age/StudentsByAge.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class StudentsByAge
+{
+    static void Main()
+    {
+        string input = string.Empty;
+        var studentsAndAges = new List<string[]>();
+
+        while ((input = Console.ReadLine()) != "END")
+        {
+            studentsAndAges.Add(input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+        }
+
+        var result = studentsAndAges
+             .Where(s => int.Parse(s[2]) >= 18 && int.Parse(s[2]) <= 24);
+
+        foreach (var student in result)
+        {
+            Console.WriteLine($"{student[0]} {student[1]} {student[2]}");
+        }
+    }
+}
