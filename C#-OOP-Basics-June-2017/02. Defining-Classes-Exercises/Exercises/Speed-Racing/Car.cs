a58004d136bb50eb82562edd7b7e1eec4924abe0
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-29 00:21:47 +0300

    07. Speed Racing
    Judge - 100/100.

---
+++ C#-OOP-Basics-June-2017/02. Defining-Classes-Exercises/Exercises/Speed-Racing/Car.cs
@@ -0,0 +1,34 @@
+﻿class Car
+{
+    private string model;
+    private double fuelAmount;
+    private double cosumptionPerKilometer;
+    private int distanceTravelled;
+
+    public Car(string model, double fuelAmount, double consumptionPerKilometer)
+    {
+        this.model = model;
+        this.fuelAmount = fuelAmount;
+        this.cosumptionPerKilometer = consumptionPerKilometer;
+        this.distanceTravelled = 0;
+    }
+
+    public bool CanTravelDistance(int distance)
+    {
+        var consumptionForTheTraveling = distance * this.cosumptionPerKilometer;
+
+        if (consumptionForTheTraveling <= this.fuelAmount)
+        {
+            this.fuelAmount -= consumptionForTheTraveling;
+            this.distanceTravelled += distance;
+            return true;
+        }
+
+        return false;
+    }
+
+    public override string ToString()
+    {
+        return $"{this.model} {this.fuelAmount:F2} {this.distanceTravelled}";
+    }
+}
---
+++ C#-OOP-Basics-June-2017/02. Defining-Classes-Exercises/Exercises/Speed-Racing/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+class StartUp
+{
+    static void Main()
+    {
+        var cars = new Dictionary<string, Car>();
+        var n = int.Parse(Console.ReadLine());
+        var input = string.Empty;
+
+        for (int i = 0; i < n; i++)
+        {
+            input = Console.ReadLine();
+            var tokens = input.Split(' ');
+            var model = tokens[0];
+            var amount = double.Parse(tokens[1]);
+            var consumption = double.Parse(tokens[2]);
+            cars.Add(model, new Car(model, amount, consumption));
+        }
+
+        while ((input=Console.ReadLine())!="End")
+        {
+            var tokens = input.Split(' ');
+            var model = tokens[1];
+            var distanceForTraveling = int.Parse(tokens[2]);
+            var currentCar = cars[model];
+            
+            if(!currentCar.CanTravelDistance(distanceForTraveling))
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+
+        Console.WriteLine(string.Join(Environment.NewLine, cars.Values));
+    }
+}
