683fea9e6c1b9c0ac6775bd29d874c0beb58ed4b
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-28 18:43:19 +0300

    02. Creating Constructors
    Judge - 100/100.

---
+++ C#-OOP-Basics-June-2017/02. Defining-Classes-Exercises/Exercises/Creating-Constructors/Person.cs
@@ -0,0 +1,19 @@
+﻿class Person
+{
+    public string name;
+    public int age;
+
+    public Person(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person(int age)
+        : this("No name", age)
+    { }
+
+    public Person()
+        : this("No name", 1)
+    { }
+}
