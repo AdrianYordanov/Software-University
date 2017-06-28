52375c8d2a407e6f6f688dcd58dfc9c38c22c273
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-28 19:00:30 +0300

    03. Oldest Family Member
    Judge - 100/100.

---
+++ C#-OOP-Basics-June-2017/02. Defining-Classes-Exercises/Exercises/Oldest-Family-Member/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class StartUp
+{
+    static void Main()
+    {
+        var n = int.Parse(Console.ReadLine());
+        var family = new Family();
+
+        for (int i = 0; i < n; i++)
+        {
+            var tokens = Console.ReadLine().Split(' ');
+            family.AddMember(new Person(tokens[0], int.Parse(tokens[1])));
+        }
+
+        Console.WriteLine(family.GetOldestMember());
+
+        var oldestMemberMethod = typeof(Family).GetMethod("GetOldestMember");
+        var addMemberMethod = typeof(Family).GetMethod("AddMember");
+
+        if (oldestMemberMethod == null || addMemberMethod == null)
+        {
+            throw new Exception();
+        }
+    }
+}
