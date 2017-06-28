3b42d1ddb4a6370a3adf5440e8f031fb4f4cf9c0
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-28 17:07:46 +0300

    10. DNA Helix
    Judge - 100/100.

---
+++ JS-Fundamentals-September-2016/06. Functions-Exercises/Exercises/DNA-Helix/DNA-Helix.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function dnaHelix(input) {
+    let helix = ['**AT**', '*C--G*', 'T----T', '*A--G*', '**GG**',
+        '*A--T*', 'C----G', '*T--T*', '**AG**', '*G--G*',
+        'A----T', '*C--G*', '**TT**', '*A--G*', 'G----G',
+        '*A--T*', '**CG**', '*T--T*', 'A----G', '*G--G*'];
+
+    for (let i = 0; i < input; i++) {
+        console.log(helix[i % helix.length]);
+    }
+}
+
+dnaHelix([22]);
