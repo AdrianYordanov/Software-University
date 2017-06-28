5a1963f9cef6babb1c6398ec6ba2f0d8d37d5d1d
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-28 21:30:24 +0300

    06. Company Roster
    Judge - 100/100.

---
+++ C#-OOP-Basics-June-2017/02. Defining-Classes-Exercises/Exercises/Company-Roster/Employee.cs
@@ -0,0 +1,36 @@
+﻿class Employee
+{
+    public string name;
+    public decimal salary;
+    public string position;
+    public string department;
+    public string email;
+    public int age;
+
+    public Employee(string name, decimal salary, string position, string department, string email, int age)
+    {
+        this.name = name;
+        this.salary = salary;
+        this.position = position;
+        this.department = department;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Employee(string name, decimal salary, string position, string department, string email)
+        : this(name, salary, position, department, email, -1)
+    { }
+
+    public Employee(string name, decimal salary, string position, string department, int age)
+        : this(name, salary, position, department, "n/a", age)
+    { }
+
+    public Employee(string name, decimal salary, string position, string department)
+       : this(name, salary, position, department, "n/a", -1)
+    { }
+
+    public override string ToString()
+    {
+        return $"{this.name} {this.salary:F2} {this.email} {this.age}";
+    }
+}
---
+++ C#-OOP-Basics-June-2017/02. Defining-Classes-Exercises/Exercises/Company-Roster/StartUp.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class StartUp
+{
+    static void Main()
+    {
+        var n = int.Parse(Console.ReadLine());
+        var employees = new List<Employee>();
+
+        for (int i = 0; i < n; i++)
+        {
+            var tokens = Console.ReadLine().Split(' ');
+            var name = tokens[0];
+            var salary = decimal.Parse(tokens[1]);
+            var position = tokens[2];
+            var department = tokens[3];
+
+            if (tokens.Length == 4)
+            {
+                employees.Add(new Employee(name, salary, position, department));
+            }
+            else if (tokens.Length == 5)
+            {
+                var tempForAge = 0;
+
+                if (int.TryParse(tokens[4], out tempForAge))
+                {
+                    employees.Add(new Employee(name, salary, position, department, tempForAge));
+                }
+                else
+                {
+                    employees.Add(new Employee(name, salary, position, department, tokens[4]));
+                }
+            }
+            else if (tokens.Length == 6)
+            {
+                employees.Add(new Employee(name, salary, position, department, tokens[4], int.Parse(tokens[5])));
+            }
+        }
+
+        var bestDepartment = employees
+            .GroupBy(employee => employee.department, employee => employee.salary)
+            .OrderByDescending(departmentGroup => departmentGroup.Sum() / departmentGroup.Count())
+            .First()
+            .Key;
+        var fromDepartment = employees
+            .Where(employee => employee.department == bestDepartment)
+            .OrderByDescending(employee => employee.salary);
+        Console.WriteLine($"Highest Average Salary: {bestDepartment}");
+        Console.WriteLine(string.Join(Environment.NewLine, fromDepartment));
+    }
+}
