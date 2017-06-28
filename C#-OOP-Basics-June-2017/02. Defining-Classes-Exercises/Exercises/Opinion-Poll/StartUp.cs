f249141b16569c6165d2a0b7c2c62fc3b57553d4
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-28 19:10:08 +0300

    04. Opinion Poll
    Judge - 100/100.

---
+++ C#-OOP-Basics-June-2017/02. Defining-Classes-Exercises/Exercises/Opinion-Poll/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class StartUp
+{
+    static void Main()
+    {
+        var n = int.Parse(Console.ReadLine());
+        var people = new List<Person>();
+
+        for (int i = 0; i < n; i++)
+        {
+            var tokens = Console.ReadLine().Split(' ');
+            people.Add(new Person(tokens[0], int.Parse(tokens[1])));
+        }
+
+        var filteredPeople = people
+            .Where(person => person.Age > 30)
+            .OrderBy(person => person.Name);
+        Console.WriteLine(string.Join(Environment.NewLine, filteredPeople));
+    }
+}
