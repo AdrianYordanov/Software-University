937dd053f0205168ff6b172ea564b142a9e9b426
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-28 18:34:10 +0300

    01. Define a Class Person
    Judge - 100/100.

---
+++ C#-OOP-Basics-June-2017/02. Defining-Classes-Exercises/Exercises/Define-A-Class-Person/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+
+class StartUp
+{
+    static void Main()
+    {
+        var pesho = new Person() { name = "Pesho", age = 20 };
+        var gosho = new Person() { name = "Gosho", age = 18 };
+        var stamat = new Person() { name = "Stamat", age = 43 };
+
+        var personType = typeof(Person);
+        var fields = personType.GetFields(BindingFlags.Public | BindingFlags.Instance);
+        Console.WriteLine(fields.Length);
+    }
+}
