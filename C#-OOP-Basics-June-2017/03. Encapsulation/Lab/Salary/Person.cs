59eb4394f0909ca922a46876841d2871667bcba5
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-29 15:32:29 +0300

    02. Salary
    Judge - 100/100.

---
+++ C#-OOP-Basics-June-2017/03. Encapsulation/Lab/Salary/Person.cs
@@ -0,0 +1,64 @@
+﻿class Person
+{
+    private string firstName;
+    private string lastName;
+    private int age;
+    private double salary;
+
+    public Person(string firstName, string lastName, int age, double salary)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public string FirstName
+    {
+        get
+        {
+            return this.firstName;
+        }
+    }
+
+    public string LastName
+    {
+        get
+        {
+            return this.lastName;
+        }
+    }
+
+    public int Age
+    {
+        get
+        {
+            return this.age;
+        }
+    }
+
+    public double Salary
+    {
+        get
+        {
+            return this.salary;
+        }
+    }
+
+    public void IncreaseSalary(double percent)
+    {
+        if (this.age > 30)
+        {
+            this.salary += this.salary * percent / 100;
+        }
+        else
+        {
+            this.salary += this.salary * percent / 200;
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"{this.FirstName} {this.LastName} get {this.Salary:F2} leva";
+    }
+}
