aa7b4ddf6a43357110ddfc54b0797846f69289bd
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-29 16:37:07 +0300

    04. Team
    Judge - 100/100.

---
+++ C#-OOP-Basics-June-2017/03. Encapsulation/Lab/Team/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class StartUp
+{
+    static void Main()
+    {
+        var name = Console.ReadLine();
+        var lines = int.Parse(Console.ReadLine());
+        var team = new Team(name);
+
+        for (int i = 0; i < lines; i++)
+        {
+            var cmdArgs = Console.ReadLine().Split();
+            var person = new Person(cmdArgs[0],
+                                    cmdArgs[1],
+                                    int.Parse(cmdArgs[2]),
+                                    double.Parse(cmdArgs[3]));
+            team.AddPlayer(person);
+        }
+    }
+}
