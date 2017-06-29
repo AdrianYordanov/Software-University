a33a84274ac2491c0fd315dfb96017464d961a07
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-29 19:15:56 +0300

    11. Pokemon Trainer
    Judge - 100/100.

---
+++ C#-OOP-Basics-June-2017/02. Defining-Classes-Exercises/Exercises/Pokemon-Trainer/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class StartUp
+{
+    static void Main()
+    {
+        var input = string.Empty;
+        var trainers = new Dictionary<string, Trainer>();
+
+        while ((input = Console.ReadLine()) != "Tournament")
+        {
+            var tokens = input.Split(' ');
+            var trainerName = tokens[0];
+            var pokemonName = tokens[1];
+            var pokemonElement = tokens[2];
+            var pokemonHealth = int.Parse(tokens[3]);
+            var createdPokemon = new Pokemon(pokemonName, pokemonElement, pokemonHealth);
+
+            if (!trainers.ContainsKey(trainerName))
+            {
+                trainers.Add(trainerName, new Trainer(trainerName));
+            }
+
+            trainers[trainerName].AddPokemon(createdPokemon);
+        }
+
+        while ((input = Console.ReadLine()) != "End")
+        {
+            foreach (var trainer in trainers.Values)
+            {
+                trainer.TryToAddBadge(input);
+            }
+        }
+
+        var result = trainers.Values
+            .OrderByDescending(trainer => trainer.Badges);
+
+        Console.WriteLine(string.Join(Environment.NewLine, result));
+    }
+}
---
+++ C#-OOP-Basics-June-2017/02. Defining-Classes-Exercises/Exercises/Pokemon-Trainer/Trainer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+class Trainer
+{
+    private string name;
+    private int badges;
+    private List<Pokemon> pokemons;
+
+    public Trainer(string name)
+    {
+        this.name = name;
+        this.badges = 0;
+        this.pokemons = new List<Pokemon>();
+    }
+
+    public int Badges
+    {
+        get
+        {
+            return this.badges;
+        }
+    }
+
+    public void AddPokemon(Pokemon pokemon)
+    {
+        this.pokemons.Add(pokemon);
+    }
+
+    public void TryToAddBadge(string element)
+    {
+        var countWithElement = this.pokemons
+            .Where(pokemon => pokemon.Element == element)
+            .Count();
+
+        if (countWithElement > 0)
+        {
+            this.badges++;
+        }
+        else
+        {
+            this.pokemons.ForEach(pokemon => pokemon.Health -= 10);
+            this.pokemons = this.pokemons
+                .Where(pokemon => pokemon.Health > 0)
+                .ToList();
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"{this.name} {this.Badges} {this.pokemons.Count}";
+    }
+}
