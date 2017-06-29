0f0b4c147116b3eaf695385c1e2a1a6e9fac2df2
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-29 15:47:03 +0300

    03. Validation
    Judge - 100/100.

---
+++ C#-OOP-Basics-June-2017/03. Encapsulation/Lab/Validation/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class StartUp
+{
+    static void Main()
+    {
+        var lines = int.Parse(Console.ReadLine());
+        var persons = new List<Person>();
+        for (int i = 0; i < lines; i++)
+        {
+            var cmdArgs = Console.ReadLine().Split();
+            try
+            {
+                var person = new Person(cmdArgs[0],
+                                        cmdArgs[1],
+                                        int.Parse(cmdArgs[2]),
+                                        double.Parse(cmdArgs[3]));
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        persons.ForEach(p => Console.WriteLine(p.ToString()));
+    }
+}
