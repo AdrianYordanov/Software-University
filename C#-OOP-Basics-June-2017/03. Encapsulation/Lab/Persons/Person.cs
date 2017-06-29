aff21d628ed5f5dc7e307a596f1b7a2c141cd724
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-06-29 15:11:01 +0300

    01. Persons
    Judge - 100/100.

---
+++ C#-OOP-Basics-June-2017/03. Encapsulation/Lab/Persons/Person.cs
@@ -0,0 +1,46 @@
+﻿class Person
+{
+    private string firstName;
+    private string lastName;
+    private int age;
+
+    public Person(string firstName, string lastName, int age)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.Age = age;
+    }
+
+    public string FirstName
+    {
+        get
+        {
+            return this.firstName;
+        }
+    }
+
+    public string LastName
+    {
+        get
+        {
+            return this.lastName;
+        }
+    }
+
+    public int Age
+    {
+        get
+        {
+            return this.age;
+        }
+        set
+        {
+            this.age = value;
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"{this.FirstName} {this.LastName} is a {this.Age} years old";
+    }
+}
