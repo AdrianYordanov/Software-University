f17d375844e073b6c50c19418c675e675be613f4
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-07-05 17:06:06 +0300

    01. Class Box
    Judge - 100/100.

---
+++ C#-OOP-Basics-June-2017/04. Encapsulation-Exercises/Exercises/Class-Box/Box.cs
@@ -0,0 +1,28 @@
+﻿class Box
+{
+    private double length;
+    private double width;
+    private double height;
+
+    public Box(double length, double width, double height)
+    {
+        this.length = length;
+        this.width = width;
+        this.height = height;
+    }
+
+    public double Volume()
+    {
+        return this.length * this.width * this.height;
+    }
+
+    public double LateralSurfaceArea()
+    {
+        return 2 * this.length * this.height + 2 * this.width * this.height;
+    }
+
+    public double SurfaceArea()
+    {
+        return 2 * this.length * this.width + 2 * this.length * this.height + 2 * this.width * this.height;
+    }
+}
