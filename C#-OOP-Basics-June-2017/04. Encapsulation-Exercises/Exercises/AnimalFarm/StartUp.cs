a5116423fb6ac3760b29d023e4339dc1c8e4fff0
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-07-05 17:30:57 +0300

    03. Animal Farm
    Judge - 100/100.

---
+++ C#-OOP-Basics-June-2017/04. Encapsulation-Exercises/Exercises/AnimalFarm/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        var chickenType = typeof(Chicken);
+        var fields = chickenType.GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+        var methods = chickenType.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance);
+        Debug.Assert(fields.Where(f => f.IsPrivate).Count() == 2);
+        Debug.Assert(methods.Where(m => m.IsPrivate).Count() == 1);
+
+        var name = Console.ReadLine();
+        var age = int.Parse(Console.ReadLine());
+
+        try
+        {
+            Chicken chicken = new Chicken(name, age);
+            Console.WriteLine(
+                "Chicken {0} (age {1}) can produce {2} eggs per day.",
+                chicken.Name,
+                chicken.Age,
+                chicken.GetProductPerDay());
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
