02385738a0baa1216b8170cba7032a68b057242d
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-07-05 18:43:53 +0300

    04. Shopping Spree
    Judge - 100/100.

---
+++ C#-OOP-Basics-June-2017/04. Encapsulation-Exercises/Exercises/Shopping-Spree/Person.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Person
+{
+    private string name;
+    private decimal money;
+    private List<Product> bag;
+
+    public Person(string name, decimal money)
+    {
+        this.Name = name;
+        this.Money = money;
+        this.bag = new List<Product>();
+    }
+
+    public string Name
+    {
+        get
+        {
+            return this.name;
+        }
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Name cannot be empty");
+            }
+
+            this.name = value;
+        }
+    }
+
+    public decimal Money
+    {
+        get
+        {
+            return this.money;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Money cannot be negative");
+            }
+
+            this.money = value;
+        }
+    }
+
+    public string TryMakeOrder(Product product)
+    {
+        if (this.Money < product.Cost)
+        {
+            return $"{this.Name} can't afford {product.Name}";
+        }
+
+        this.Money -= product.Cost;
+        this.bag.Add(product);
+        return $"{this.Name} bought {product.Name}";
+    }
+
+    public override string ToString()
+    {
+        if (this.bag.Count == 0)
+        {
+            return $"{this.Name} - Nothing bought";
+        }
+
+        return $"{this.Name} - {string.Join(", ", this.bag.Select(p => p.Name))}";
+    }
+}
---
+++ C#-OOP-Basics-June-2017/04. Encapsulation-Exercises/Exercises/Shopping-Spree/StartUp.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class StartUp
+{
+    static void Main()
+    {
+        var people = new List<Person>();
+        var prodcuts = new List<Product>();
+
+        try
+        {
+            var peopleTokens = Console.ReadLine().Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+            var productsTokens = Console.ReadLine().Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var token in peopleTokens)
+            {
+                var name = token.Split('=')[0];
+                var money = decimal.Parse(token.Split('=')[1]);
+                people.Add(new Person(name, money));
+            }
+
+            foreach (var token in productsTokens)
+            {
+                var name = token.Split('=')[0];
+                var cost = decimal.Parse(token.Split('=')[1]);
+                prodcuts.Add(new Product(name, cost));
+            }
+
+            var input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                var commandTokens = input.Split(' ');
+                var person = people.FirstOrDefault(p => p.Name == commandTokens[0]);
+                var product = prodcuts.FirstOrDefault(p => p.Name == commandTokens[1]);
+                Console.WriteLine(person.TryMakeOrder(product));
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, people));
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
