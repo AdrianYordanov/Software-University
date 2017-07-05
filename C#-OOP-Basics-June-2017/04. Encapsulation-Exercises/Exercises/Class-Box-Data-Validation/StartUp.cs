60e2ee4719fdc0a524c4905d49db8e32e12473b6
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-07-05 17:17:49 +0300

    02. Class Box Data Validation
    Judge - 100/100.

---
+++ C#-OOP-Basics-June-2017/04. Encapsulation-Exercises/Exercises/Class-Box-Data-Validation/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+
+class StartUp
+{
+    static void Main()
+    {
+        var boxType = typeof(Box);
+        var fields = boxType.GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+        Console.WriteLine(fields.Length);
+
+        var length = double.Parse(Console.ReadLine());
+        var width = double.Parse(Console.ReadLine());
+        var height = double.Parse(Console.ReadLine());
+
+        try
+        {
+            var box = new Box(length, width, height);
+            Console.WriteLine($"Surface Area - {box.SurfaceArea():f2}");
+            Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea():f2}");
+            Console.WriteLine($"Volume - {box.Volume():f2}");
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
