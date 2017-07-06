cc4ad2fb4771ab580b57b0f3340eed3293dd9c3f
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-07-06 12:36:38 +0300

    04. Random List
    Judge - 100/100.

---
+++ C#-OOP-Basics-June-2017/05. Inheritance/Lab/Random-List/RandomList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+
+class RandomList : ArrayList
+{
+    public string RandomString()
+    {
+        var rand = new Random();
+        var index = rand.Next(this.Count);
+        var returnValue = this[index];
+        this.RemoveAt(index);
+        return returnValue.ToString();
+    }
+}
---
+++ C#-OOP-Basics-June-2017/05. Inheritance/Lab/Random-List/StartUp.cs
@@ -0,0 +1,13 @@
+﻿class StartUp
+{
+    static void Main()
+    {
+        var randomList = new RandomList();
+        randomList.Add("1");
+        randomList.Add("2");
+        randomList.Add("3");
+        randomList.Add("4");
+        randomList.Add("5");
+        System.Console.WriteLine(randomList.RandomString());
+    }
+}
