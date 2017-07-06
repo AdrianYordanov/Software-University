2a21e2184f48245a1a4e10491e8053b01f03249b
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-07-06 19:19:57 +0300

    01. Person
    Judge - 100/100.

---
+++ C#-OOP-Basics-June-2017/06. Inheritance-Exercises/Exercises/Person/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class StartUp
+{
+    static void Main(string[] args)
+    {
+        string name = Console.ReadLine();
+        int age = int.Parse(Console.ReadLine());
+
+        try
+        {
+            Child child = new Child(name, age);
+            Console.WriteLine(child);
+        }
+        catch (ArgumentException ae)
+        {
+            Console.WriteLine(ae.Message);
+        }
+    }
+}
