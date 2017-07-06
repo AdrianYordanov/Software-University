6b61455f632928861d39edfc6aff06c4f02a10a1
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-07-06 21:50:05 +0300

    02. Book Shop
    Judge - 100/100.

---
+++ C#-OOP-Basics-June-2017/06. Inheritance-Exercises/Exercises/Book-Shop/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class StartUp
+{
+    static void Main()
+    {
+        try
+        {
+            var author = Console.ReadLine();
+            var title = Console.ReadLine();
+            var price = decimal.Parse(Console.ReadLine());
+
+            var book = new Book(author, title, price);
+            var goldenEditionBook = new GoldenEditionBook(author, title, price);
+
+            Console.WriteLine(book);
+            Console.WriteLine(goldenEditionBook);
+        }
+        catch (ArgumentException ae)
+        {
+            Console.WriteLine(ae.Message);
+        }
+    }
+}
