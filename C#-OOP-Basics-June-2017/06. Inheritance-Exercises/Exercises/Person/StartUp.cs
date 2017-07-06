1de69c5ddc47525472154659085c82b1db03b598
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-07-06 20:01:53 +0300

    Change the keyword defining.
    We should use var.

--- C#-OOP-Basics-June-2017/06. Inheritance-Exercises/Exercises/Person/StartUp.cs
+++ C#-OOP-Basics-June-2017/06. Inheritance-Exercises/Exercises/Person/StartUp.cs
@@ -4,12 +4,12 @@ class StartUp
 {
     static void Main(string[] args)
     {
-        string name = Console.ReadLine();
-        int age = int.Parse(Console.ReadLine());
+        var name = Console.ReadLine();
+        var age = int.Parse(Console.ReadLine());
 
         try
         {
-            Child child = new Child(name, age);
+            var child = new Child(name, age);
             Console.WriteLine(child);
         }
         catch (ArgumentException ae)
