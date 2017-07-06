14b8e434de4a00f3396716ee4cd1639ee7a1afd0
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-07-06 15:34:46 +0300

    03. Shapes
    Judge - 100/100.

---
+++ C#-OOP-Basics-June-2017/07. Polymorphism/Lab/Shapes/Shape.cs
@@ -0,0 +1,11 @@
+﻿abstract class Shape
+{
+    abstract public double CalculatePerimeter();
+
+    abstract public double CalculateArea();
+
+    virtual public string Draw()
+    {
+        return "Drawing ";
+    }
+}
---
+++ C#-OOP-Basics-June-2017/07. Polymorphism/Lab/Shapes/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class StartUp
+{
+    static void Main()
+    {
+        var shapes = new List<Shape>();
+        shapes.Add(new Rectangle(3, 4));
+        shapes.Add(new Circle(120.34));
+
+        foreach (var shape in shapes)
+        {
+            Console.WriteLine($"{shape.Draw()}, area: {shape.CalculateArea():f2} perimeter: {shape.CalculatePerimeter():f2}");
+        }
+    }
+}
