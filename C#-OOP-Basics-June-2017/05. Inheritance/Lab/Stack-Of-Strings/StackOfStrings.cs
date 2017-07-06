0e5bc7c2ecee8be4da73888b46e0f72c53c6ef69
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-07-06 12:47:35 +0300

    05. Stack of Strings
    Judge - 100/100.

---
+++ C#-OOP-Basics-June-2017/05. Inheritance/Lab/Stack-Of-Strings/StackOfStrings.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+class StackOfStrings
+{
+    private List<string> data;
+
+    public StackOfStrings()
+    {
+        this.data = new List<string>();
+    }
+
+    public void Push(string element)    {
+        this.data.Add(element);
+    }
+
+    public string Pop()
+    {
+        var element = this.data.Last();
+        this.data.Remove(element);
+        return element;
+    }    public string Peek()
+    {
+        return this.data.Last();
+    }    public bool IsEmpty()
+    {
+        return this.data.Count == 0;
+    }}
---
+++ C#-OOP-Basics-June-2017/05. Inheritance/Lab/Stack-Of-Strings/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class StartUp
+{
+    static void Main()
+    {
+        var stack = new StackOfStrings();
+        stack.Push("1");
+        stack.Push("2");
+        Console.WriteLine(stack.IsEmpty());
+        Console.WriteLine(stack.Peek());
+        Console.WriteLine(stack.Pop());
+        Console.WriteLine(stack.Peek());
+        stack.Pop();
+        Console.WriteLine(stack.IsEmpty());
+    }
+}
