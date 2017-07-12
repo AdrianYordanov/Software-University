17b10df9b8fe7031c1af1277ede2aed39162b348
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-07-12 15:25:29 +0300

    04. Rotate Array
    Judge - 100/100.

---
+++ JS-Fundamentals-September-2016/09. Arrays-and-Matrices-Exercises/Exercises/Rotate-Array/Rotate-Array.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function rotateArray(input) {
+    let rotations = Number(input.pop());
+    rotations %= input.length;
+
+    for(let i=0; i<rotations; i++) {
+        input.unshift(input.pop());
+    }
+
+    console.log(input.join(' '));
+}
+
+rotateArray([1, 2, 3, 4, 2]);
