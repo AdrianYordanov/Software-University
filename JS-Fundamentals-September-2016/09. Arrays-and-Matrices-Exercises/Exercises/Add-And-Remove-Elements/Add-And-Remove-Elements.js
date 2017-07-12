fc732a708a46d1a967fd6283a617d5c426f65c04
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-07-12 15:21:31 +0300

    03. Add and Remove Elements
    Judge - 100/100.

---
+++ JS-Fundamentals-September-2016/09. Arrays-and-Matrices-Exercises/Exercises/Add-And-Remove-Elements/Add-And-Remove-Elements.js
@@ -0,0 +1,22 @@
+function addAndRemoveElements(input) {
+    let result = [];
+    let number = 1;
+
+    for(let command of input) {
+        if(command == "add") {
+            result.push(number);
+        } else {
+            result.pop();
+        }
+
+        number++;
+    }
+
+    if(result.length == 0) {
+        console.log("Empty");
+    } else {
+        result.forEach(element => console.log(element));
+    }
+}
+
+addAndRemoveElements(["add", "add", "add", "add"]);
