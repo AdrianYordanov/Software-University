fb25c99c0c07aef8b900fcce375cb3392c8394ce
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-07-12 16:07:02 +0300

    07. Magic Matrices
    Judge - 100/100.

---
+++ JS-Fundamentals-September-2016/09. Arrays-and-Matrices-Exercises/Exercises/Magic-Matrices/Magic-Matrices.js
@@ -0,0 +1,28 @@
+"use strict";
+
+function magicMatrices(input) {
+    let matrix = input.map(row => row.split(" ").map(Number));
+    let sum = matrix[0].reduce((a,b) => a+b);
+    let isMagic = true;
+
+    for(let i=1; i<matrix.length; i++) {
+        if(sum != matrix[i].reduce((a,b) => a+b)) {
+            isMagic = false;
+        }
+    }
+
+    for(let col=0; col<matrix[0].length; col++) {
+        let sumCol = 0;
+        for(let row=0; row<matrix.length; row++) {
+            sumCol += matrix[row][col];
+        }
+
+        if(sumCol != sum) {
+            isMagic = false;
+        }
+    }
+
+    console.log(isMagic);
+}
+
+magicMatrices(['4 5 6', '6 5 4', '5 5 5']);
