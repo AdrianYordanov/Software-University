985c8a0a09c65fce9cbb6fc4740d16b393b1b209
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-07-19 20:05:25 +0300

    Ferrari exercise coment.
    Add coment to tell the resharper that we don't use this variables.
    They are only to check do we use the interface and the class.

--- C#-OOP-Advanced-July-2017/02. Interfaces-And-Abstraction-Exercises/Exercises/Ferrari/StartUp.cs
+++ C#-OOP-Advanced-July-2017/02. Interfaces-And-Abstraction-Exercises/Exercises/Ferrari/StartUp.cs
@@ -4,8 +4,10 @@ public class StartUp
 {
     public static void Main()
     {
+        // ReSharper disable once UnusedVariable
         var ferrariName = typeof(Ferrari).Name;
-        var iCarInterfaceName = typeof(ICar).Name;
+        // ReSharper disable once UnusedVariable
+        var carInterfaceName = typeof(ICar).Name;
 
         var isCreated = typeof(ICar).IsInterface;
         if (!isCreated)
