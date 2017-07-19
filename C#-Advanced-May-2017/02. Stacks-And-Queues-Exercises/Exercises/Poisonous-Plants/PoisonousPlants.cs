db99a18d881114418d9ac0c0eed2b5d42514e682
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-07-19 20:53:20 +0300

    Refactor PoisonousPlants

--- C#-Advanced-May-2017/02. Stacks-And-Queues-Exercises/Exercises/Poisonous-Plants/PoisonousPlants.cs
+++ C#-Advanced-May-2017/02. Stacks-And-Queues-Exercises/Exercises/Poisonous-Plants/PoisonousPlants.cs
@@ -2,24 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 
-
-class PoisonousPlants
+public class PoisonousPlants
 {
-    static void Main()
+    public static void Main()
     {
         var n = int.Parse(Console.ReadLine());
         var plants = Console.ReadLine()
-            .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+            .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
             .Select(int.Parse)
             .ToArray();
         var days = new int[n];
         var proximityStack = new Stack<int>();
         proximityStack.Push(0);
-
-        for (int i = 0; i < plants.Length; i++)
+        for (var i = 0; i < plants.Length; i++)
         {
-            int maxDays = 0;
-
+            var maxDays = 0;
             while (proximityStack.Count > 0 && plants[proximityStack.Peek()] >= plants[i])
             {
                 maxDays = Math.Max(maxDays, days[proximityStack.Pop()]);
