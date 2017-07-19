d9608c072370792966057adfc9d424be13bef478
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-07-19 20:37:42 +0300

    Refactor CalculateSequenceWithQueue

--- C#-Advanced-May-2017/02. Stacks-And-Queues-Exercises/Exercises/Calculate-Sequence-With-Queue/CalculateSequenceWithQueue.cs
+++ C#-Advanced-May-2017/02. Stacks-And-Queues-Exercises/Exercises/Calculate-Sequence-With-Queue/CalculateSequenceWithQueue.cs
@@ -1,22 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 
-class CalculateSequenceWithQueue
+public class CalculateSequenceWithQueue
 {
-    static void Main()
+    public static void Main()
     {
-        long n = long.Parse(Console.ReadLine());
-        long membersCount = 50;
+        var n = long.Parse(Console.ReadLine());
+        var membersCount = 50L;
         var queue = new Queue<long>();
 
         queue.Enqueue(n);
 
-        for (int i = 0; i < membersCount; i++)
+        for (var i = 0; i < membersCount; i++)
         {
             var currentS = queue.Dequeue();
             Console.Write(currentS + " ");
             queue.Enqueue(currentS + 1);
-            queue.Enqueue(currentS * 2 + 1);
+            queue.Enqueue((currentS * 2) + 1);
             queue.Enqueue(currentS + 2);
         }
     }
