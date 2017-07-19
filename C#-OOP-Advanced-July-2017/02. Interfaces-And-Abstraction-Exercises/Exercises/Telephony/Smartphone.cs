ef15c3ced3322fb4c566a78736894f1455ef3878
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-07-19 19:56:14 +0300

    04. Telephony
    Judge - 100/100.

---
+++ C#-OOP-Advanced-July-2017/02. Interfaces-And-Abstraction-Exercises/Exercises/Telephony/Smartphone.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Smartphone : ICallable, IBrowsable
+{
+    private readonly IList<string> numbers;
+    private readonly IList<string> sites;
+
+    public Smartphone()
+    {
+        this.numbers = new List<string>();
+        this.sites = new List<string>();
+    }
+
+    public void AddPhone(string phone)
+    {
+        this.numbers.Add(!phone.All(char.IsDigit) ? "Invalid number!" : $"Calling... {phone}");
+    }
+
+    public void AddSite(string site)
+    {
+        this.sites.Add(site.Any(char.IsDigit) ? "Invalid URL!" : $"Browsing: {site}!");
+    }
+
+    public string Call()
+    {
+        return string.Join(Environment.NewLine, this.numbers);
+    }
+
+    public string Browse()
+    {
+        return string.Join(Environment.NewLine, this.sites);
+    }
+}
---
+++ C#-OOP-Advanced-July-2017/02. Interfaces-And-Abstraction-Exercises/Exercises/Telephony/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        var smartPhone = new Smartphone();
+        var numbers = Console.ReadLine().Split(' ');
+        var sites = Console.ReadLine().Split(' ');
+
+        foreach (var number in numbers)
+        {
+            smartPhone.AddPhone(number);
+        }
+
+        foreach (var site in sites)
+        {
+            smartPhone.AddSite(site);
+        }
+
+        PrintPhones(smartPhone);
+        PrintSites(smartPhone);
+    }
+
+    private static void PrintPhones(ICallable phones)
+    {
+        Console.WriteLine(phones.Call());
+    }
+
+    private static void PrintSites(IBrowsable phones)
+    {
+        Console.WriteLine(phones.Browse());
+    }
+}
