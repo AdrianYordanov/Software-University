e2f96080084ea376cc859edae4d5228078b17b4e
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-07-19 20:01:49 +0300

    Ferari exercise
    Add test with reflection to check are we use interface.

--- C#-OOP-Advanced-July-2017/02. Interfaces-And-Abstraction-Exercises/Exercises/Ferrari/StartUp.cs
+++ C#-OOP-Advanced-July-2017/02. Interfaces-And-Abstraction-Exercises/Exercises/Ferrari/StartUp.cs
@@ -4,6 +4,15 @@ public class StartUp
 {
     public static void Main()
     {
+        var ferrariName = typeof(Ferrari).Name;
+        var iCarInterfaceName = typeof(ICar).Name;
+
+        var isCreated = typeof(ICar).IsInterface;
+        if (!isCreated)
+        {
+            throw new Exception("No interface ICar was created");
+        }
+
         var driver = Console.ReadLine();
         Console.WriteLine(new Ferrari(driver));
     }
