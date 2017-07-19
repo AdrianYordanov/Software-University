7095c5e99093b6b46d3d3279f90300e92cc515db
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-07-19 21:11:38 +0300

    Refactor AcademyGraduation

--- C#-Advanced-May-2017/03. Sets-And-Dictionaries/Lab/Academy-Graduation/AcademyGraduation.cs
+++ C#-Advanced-May-2017/03. Sets-And-Dictionaries/Lab/Academy-Graduation/AcademyGraduation.cs
@@ -2,18 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 
-class AcademyGraduation
+public class AcademyGraduation
 {
-    static void Main()
+    public static void Main()
     {
         var studentsCount = int.Parse(Console.ReadLine());
         var students = new SortedDictionary<string, double[]>();
-
-        for (int i = 0; i < studentsCount; i++)
+        for (var i = 0; i < studentsCount; i++)
         {
             var name = Console.ReadLine();
             var grades = Console.ReadLine()
-                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
                 .Select(double.Parse)
                 .ToArray();
             students.Add(name, grades);
