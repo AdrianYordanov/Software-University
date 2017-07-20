ee60b1a6201265445c25542ca9e4eb83536ee216
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-07-20 10:37:13 +0300

    Refactor Phonebook

--- C#-Advanced-May-2017/04. Sets-And-Dictionaries-Exercises/Exercises/Population-Counter/PopulationCounter.cs
+++ C#-Advanced-May-2017/04. Sets-And-Dictionaries-Exercises/Exercises/Population-Counter/PopulationCounter.cs
@@ -2,28 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 
-class PopulationCounter
+public class PopulationCounter
 {
-    static void Main()
+    public static void Main()
     {
         var input = string.Empty;
         var countries = new Dictionary<string, Dictionary<string, long>>();
-
         while ((input = Console.ReadLine()) != "report")
         {
             var tokens = input.Split('|');
             var city = tokens[0];
             var country = tokens[1];
             var cityPopulation = long.Parse(tokens[2]);
-
             if (countries.ContainsKey(country))
             {
                 countries[country].Add(city, cityPopulation);
             }
             else
             {
-                var cities = new Dictionary<string, long>();
-                cities.Add(city, cityPopulation);
+                var cities = new Dictionary<string, long> { { city, cityPopulation } };
                 countries.Add(country, cities);
             }
         }
@@ -31,7 +28,6 @@ class PopulationCounter
         var orderedCountries = countries
             .OrderByDescending(pair => pair.Value.Values.Sum())
             .ToDictionary(x => x.Key, x => x.Value);
-
         foreach (var pair in orderedCountries)
         {
             var country = pair.Key;
@@ -39,10 +35,7 @@ class PopulationCounter
             var orderedCities = cities
                 .OrderByDescending(x => x.Value)
                 .ToDictionary(x => x.Key, x => x.Value);
-
-
             Console.WriteLine($"{country} (total population: {orderedCities.Values.Sum()})");
-
             foreach (var nestedPair in orderedCities)
             {
                 var city = nestedPair.Key;
