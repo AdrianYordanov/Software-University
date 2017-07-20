cdca03a11782a91fa223ff38395462e8c8667eb1
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-07-20 10:26:35 +0300

    Refactor HandsOfCards

--- C#-Advanced-May-2017/04. Sets-And-Dictionaries-Exercises/Exercises/Hands-Of-Cards/HandsOfCards.cs
+++ C#-Advanced-May-2017/04. Sets-And-Dictionaries-Exercises/Exercises/Hands-Of-Cards/HandsOfCards.cs
@@ -1,24 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 
-class HandsOfCards
+public class HandsOfCards
 {
     private static string[] cards;
     private static string[] multiplier;
 
-    static void Main()
+    public static void Main()
     {
         var input = string.Empty;
         var people = new Dictionary<string, HashSet<string>>();
-        cards = new string[] { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
-        multiplier = new string[] { "C", "D", "H", "S" };
-
+        cards = new[] { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+        multiplier = new[] { "C", "D", "H", "S" };
         while ((input = Console.ReadLine()) != "JOKER")
         {
             var tokens = input.Split(':');
             var player = tokens[0];
-            var playerCards = tokens[1].Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
-
+            var playerCards = tokens[1].Split(new[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
             if (people.ContainsKey(player))
             {
                 people[player].UnionWith(playerCards);
@@ -36,14 +34,12 @@ class HandsOfCards
         }
     }
 
-    private static int CalculateScore(HashSet<string> playerCards)
+    private static int CalculateScore(IEnumerable<string> playerCards)
     {
         var cardsScore = 0;
-
         foreach (var card in playerCards)
         {
-            var tempScore = (Array.IndexOf(cards, card.Remove(card.Length - 1)) + 2) *
-                (Array.IndexOf(multiplier, card[card.Length - 1].ToString()) + 1);
+            var tempScore = (Array.IndexOf(cards, card.Remove(card.Length - 1)) + 2) * (Array.IndexOf(multiplier, card[card.Length - 1].ToString()) + 1);
             cardsScore += tempScore;
         }
 
