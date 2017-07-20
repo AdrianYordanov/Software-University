83f6655450c11ce11c5378fd72e9e97f95f00e71
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-07-20 16:09:04 +0300

    03. Scale
    Judge - 100/100.

---
+++ C#-OOP-Advanced-July-2017/03. Generics/Lab/Scale/Scale.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Scale<T> where T : IComparable<T>
+{
+    private readonly T first;
+    private readonly T second;
+
+    public Scale(T first, T second)
+    {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T GetHavier()
+    {
+        var result = this.first.CompareTo(this.second);
+        if (result == 0)
+        {
+            return default(T);
+        }
+
+        return result > 0 ? this.first : this.second;
+    }
+}
---
+++ C#-OOP-Advanced-July-2017/03. Generics/Lab/Scale/StartUp.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        var scale = new Scale<int>(2, 3);
+        Console.WriteLine(scale.GetHavier());
+    }
+}
