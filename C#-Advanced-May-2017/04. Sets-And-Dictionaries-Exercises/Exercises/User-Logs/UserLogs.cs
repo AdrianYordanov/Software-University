ffe5323b113d2e73022c68cc7d3dc2aa76e59a37
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-07-20 10:43:19 +0300

    Refactor UserLogs

--- C#-Advanced-May-2017/04. Sets-And-Dictionaries-Exercises/Exercises/User-Logs/UserLogs.cs
+++ C#-Advanced-May-2017/04. Sets-And-Dictionaries-Exercises/Exercises/User-Logs/UserLogs.cs
@@ -2,19 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 
-class UserLogs
+public class UserLogs
 {
-    static void Main()
+    public static void Main()
     {
         var input = string.Empty;
         var users = new SortedDictionary<string, Dictionary<string, int>>();
-
         while ((input = Console.ReadLine()) != "end")
         {
             var tokens = input.Split(' ');
             var ip = tokens[0].Split('=')[1];
             var user = tokens[2].Split('=')[1];
-
             if (users.ContainsKey(user))
             {
                 if (users[user].ContainsKey(ip))
@@ -28,17 +26,15 @@ class UserLogs
             }
             else
             {
-                var userIps = new Dictionary<string, int>();
-                userIps.Add(ip, 1);
+                var userIps = new Dictionary<string, int> { { ip, 1 } };
                 users.Add(user, userIps);
             }
         }
 
         foreach (var name in users.Keys)
         {
-            Console.WriteLine(name + ":");
             var ips = users[name];
-
+            Console.WriteLine(name + ":");
             Console.WriteLine(string.Join(", ", ips.Select(x => x.Key + " => " + x.Value).ToArray()) + ".");
         }
     }
