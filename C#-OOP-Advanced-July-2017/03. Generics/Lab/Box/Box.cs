875b0f4f73d83a4681136b58de63e14bacf39ab3
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-07-20 14:39:46 +0300

    01. Box
    Judge - 100/100.

---
+++ C#-OOP-Advanced-July-2017/03. Generics/Lab/Box/Box.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class Box<T>
+{
+    private readonly List<T> collection;
+
+    public Box()
+    {
+        this.collection = new List<T>();
+    }
+
+    public int Count => this.collection.Count;
+
+    public void Add(T element)
+    {
+        this.collection.Add(element);
+    }
+
+    public T Remove()
+    {
+        var element = this.collection.LastOrDefault();
+        this.collection.RemoveAt(this.collection.Count - 1);
+        return element;
+    }
+}
