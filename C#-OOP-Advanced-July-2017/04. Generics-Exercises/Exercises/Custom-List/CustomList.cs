eed025bd29b7bd515f8c96968aaeb1be6dcb8544
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-08-08 16:05:30 +0300

    07. Custom List
    Judge - 100/100.

---
+++ C#-OOP-Advanced-July-2017/04. Generics-Exercises/Exercises/Custom-List/CustomList.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class CustomList<T> : IEnumerable<T> where T : IComparable<T>
+{
+    private IList<T> data;
+
+    public CustomList()
+    {
+        this.data = new List<T>();
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        return this.data.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.data.GetEnumerator();
+    }
+
+    public void Add(T element)
+    {
+        this.data.Add(element);
+    }
+
+    public T Remove(int index)
+    {
+        var item = this.data[index];
+
+        this.data.Remove(item);
+
+        return item;
+    }
+
+    public bool Contains(T element)
+    {
+        return this.data.Any(e => e.CompareTo(element) == 0);
+    }
+
+    public void Swap(int index1, int index2)
+    {
+        var temp = this.data[index1];
+        this.data[index1] = this.data[index2];
+        this.data[index2] = temp;
+    }
+
+    public int CountGreaterThan(T element)
+    {
+        return this.data.Count(e => e.CompareTo(element) > 0);
+    }
+
+    public T Max()
+    {
+        return this.data.Max();
+    }
+
+    public T Min()
+    {
+        return this.data.Min();
+    }
+
+    public void Sort()
+    {
+        this.data = this.data.OrderBy(i => i).ToList();
+    }
+}
---
+++ C#-OOP-Advanced-July-2017/04. Generics-Exercises/Exercises/Custom-List/StartUp.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        var list = new CustomList<string>();
+        var inputLine = string.Empty;
+        while ((inputLine = Console.ReadLine()) != "END")
+        {
+            var tokens = inputLine.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            var command = tokens[0];
+            tokens.Remove(tokens.First());
+            switch (command)
+            {
+                case "Add":
+                    list.Add(tokens[0]);
+                    break;
+
+                case "Remove":
+                    list.Remove(int.Parse(tokens[0]));
+                    break;
+
+                case "Contains":
+                    Console.WriteLine(list.Contains(tokens[0]));
+                    break;
+
+                case "Swap":
+                    list.Swap(int.Parse(tokens[0]), int.Parse(tokens[1]));
+                    break;
+
+                case "Greater":
+                    Console.WriteLine(list.CountGreaterThan(tokens[0]));
+                    break;
+
+                case "Max":
+                    Console.WriteLine(list.Max());
+                    break;
+
+                case "Min":
+                    Console.WriteLine(list.Min());
+                    break;
+
+                case "Print":
+                    foreach (var item in list)
+                    {
+                        Console.WriteLine(item);
+                    }
+
+                    break;
+
+                case "Sort":
+                    list.Sort();
+                    break;
+            }
+        }
+    }
+}
