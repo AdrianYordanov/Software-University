3ee603452e17879d6d22035240a1e02ac41a99e8
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-08-08 15:56:20 +0300

    06. Generic Count Method Double
    Judge - 100/100.

---
+++ C#-OOP-Advanced-July-2017/04. Generics-Exercises/Exercises/Generic-Count-Method-Double/Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Box : IComparable<Box>
+{
+    public Box(double variable)
+    {
+        this.Variable = variable;
+    }
+
+    public double Variable { get; set; }
+
+    public int CompareTo(Box other)
+    {
+        return this.Variable.CompareTo(other.Variable);
+    }
+
+    public override string ToString()
+    {
+        return $"{this.Variable.GetType().FullName}: {this.Variable}";
+    }
+}
