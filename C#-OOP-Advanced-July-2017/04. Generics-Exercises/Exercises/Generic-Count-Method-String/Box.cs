c6ff8b82a8c3882df3b219e695c7398d4d0ee2be
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-08-08 15:52:30 +0300

    05. Generic Count Method String
    Judge - 100/100.

---
+++ C#-OOP-Advanced-July-2017/04. Generics-Exercises/Exercises/Generic-Count-Method-String/Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Box : IComparable<Box>
+{
+    public Box(string variable)
+    {
+        this.Variable = variable;
+    }
+
+    public string Variable { get; set; }
+
+    public int CompareTo(Box other)
+    {
+        return string.Compare(this.Variable, other.Variable, StringComparison.Ordinal);
+    }
+
+    public override string ToString()
+    {
+        return $"{this.Variable.GetType().FullName}: {this.Variable}";
+    }
+}
---
+++ C#-OOP-Advanced-July-2017/04. Generics-Exercises/Exercises/Generic-Count-Method-String/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        var boxes = new List<Box>();
+        var n = int.Parse(Console.ReadLine());
+        for (var i = 0; i < n; i++)
+        {
+            boxes.Add(new Box(Console.ReadLine()));
+        }
+
+        var compareElement = new Box(Console.ReadLine());
+        var greaterElementsCount = GetGreaterElementsCount(boxes, compareElement);
+        Console.WriteLine(greaterElementsCount);
+    }
+
+    public static int GetGreaterElementsCount<T>(IList<T> collection, T compareElement) where T : Box
+    {
+        var counter = 0;
+        foreach (var element in collection)
+        {
+            if (element.CompareTo(compareElement) > 0)
+            {
+                ++counter;
+            }
+        }
+
+        return counter;
+    }
+}
