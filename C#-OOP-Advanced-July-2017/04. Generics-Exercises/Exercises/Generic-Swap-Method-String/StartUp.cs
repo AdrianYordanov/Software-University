138f203d386ab797ad4a149b0b2fe957084cafd0
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-08-08 14:57:06 +0300

    03. Generic Swap Method String
    Judge - 100/100.

---
+++ C#-OOP-Advanced-July-2017/04. Generics-Exercises/Exercises/Generic-Swap-Method-String/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        var boxes = new List<Box>();
+        var n = int.Parse(Console.ReadLine());
+        for (var i = 0; i < n; i++)
+        {
+            boxes.Add(new Box(Console.ReadLine()));
+        }
+
+        var swapTokens = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse)
+            .ToArray();
+        Swapper.Swap(boxes, swapTokens[0], swapTokens[1]);
+        foreach (var box in boxes)
+        {
+            Console.WriteLine(box);
+        }
+    }
+}
