c34ce666c01c1e683afb2832e31fe0af53369c24
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-08-08 15:25:49 +0300

    04. Generic Swap Method Integer
    Judge - 100/100.

---
+++ C#-OOP-Advanced-July-2017/04. Generics-Exercises/Exercises/Generic-Swap-Method-lnteger/Swapper.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+public static class Swapper
+{
+    public static void Swap<T>(IList<T> collection, int firstIndex, int secondIndex)
+    {
+        var firstIndexElement = collection[firstIndex];
+        collection[firstIndex] = collection[secondIndex];
+        collection[secondIndex] = firstIndexElement;
+    }
+}
