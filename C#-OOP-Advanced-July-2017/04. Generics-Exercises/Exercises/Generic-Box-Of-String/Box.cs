8b889757d499a849c88662a24cffd677634723aa
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-08-08 14:45:46 +0300

    01. Generic Box Of String
    Judge - 100/100.

---
+++ C#-OOP-Advanced-July-2017/04. Generics-Exercises/Exercises/Generic-Box-Of-String/Box.cs
@@ -0,0 +1,14 @@
+﻿public class Box
+{
+    public Box(string variable)
+    {
+        this.Variable = variable;
+    }
+
+    public string Variable { get; set; }
+
+    public override string ToString()
+    {
+        return $"{this.Variable.GetType().FullName}: {this.Variable}";
+    }
+}
