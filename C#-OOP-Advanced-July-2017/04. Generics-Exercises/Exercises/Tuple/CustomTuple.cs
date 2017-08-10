3f8a136aa98fdc4dc70d3380f3f6758a1f0634b9
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-08-10 13:03:14 +0300

    10. Tuple
    Judge - 100/100.

---
+++ C#-OOP-Advanced-July-2017/04. Generics-Exercises/Exercises/Tuple/CustomTuple.cs
@@ -0,0 +1,28 @@
+﻿public class CustomTuple<TFirstItem, TSecondItem>
+{
+    private TFirstItem firstItem;
+    private TSecondItem secondItem;
+
+    public CustomTuple(TFirstItem firstItem, TSecondItem secondItem)
+    {
+        this.firstItem = firstItem;
+        this.secondItem = secondItem;
+    }
+
+    public TFirstItem FirstItem
+    {
+        get => this.firstItem;
+        set => this.firstItem = value;
+    }
+
+    public TSecondItem SecondItem
+    {
+        get => this.secondItem;
+        set => this.secondItem = value;
+    }
+
+    public override string ToString()
+    {
+        return $"{this.FirstItem} -> {this.SecondItem}";
+    }
+}
---
+++ C#-OOP-Advanced-July-2017/04. Generics-Exercises/Exercises/Tuple/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        var tokens = Console.ReadLine().Split(' ');
+        var adressTouple = new CustomTuple<string, string>($"{tokens[0]} {tokens[1]}", tokens[2]);
+        tokens = Console.ReadLine().Split(' ');
+        var beerTuple = new CustomTuple<string, int>(tokens[0], int.Parse(tokens[1]));
+        tokens = Console.ReadLine().Split(' ');
+        var lastTuple = new CustomTuple<int, double>(int.Parse(tokens[0]), double.Parse(tokens[1]));
+        Console.WriteLine(adressTouple);
+        Console.WriteLine(beerTuple);
+        Console.WriteLine(lastTuple);
+    }
+}
