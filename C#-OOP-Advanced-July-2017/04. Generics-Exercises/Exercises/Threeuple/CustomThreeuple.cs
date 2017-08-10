d15db6c08e3267f4d3185a59111560db90eef114
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-08-10 13:17:05 +0300

    11. Threeuple
    Judge - 100/100.

---
+++ C#-OOP-Advanced-July-2017/04. Generics-Exercises/Exercises/Threeuple/CustomThreeuple.cs
@@ -0,0 +1,36 @@
+﻿public class CustomThreeuple<TFirstItem, TSecondItem, TThirdItem>
+{
+    private TFirstItem firstItem;
+    private TSecondItem secondItem;
+    private TThirdItem thirdItem;
+
+    public CustomThreeuple(TFirstItem firstItem, TSecondItem secondItem, TThirdItem thirdItem)
+    {
+        this.FirstItem = firstItem;
+        this.SecondItem = secondItem;
+        this.ThirdItem = thirdItem;
+    }
+
+    public TFirstItem FirstItem
+    {
+        get => this.firstItem;
+        set => this.firstItem = value;
+    }
+
+    public TSecondItem SecondItem
+    {
+        get => this.secondItem;
+        set => this.secondItem = value;
+    }
+
+    public TThirdItem ThirdItem
+    {
+        get => this.thirdItem;
+        set => this.thirdItem = value;
+    }
+
+    public override string ToString()
+    {
+        return $"{this.FirstItem} -> {this.SecondItem} -> {this.ThirdItem}";
+    }
+}
---
+++ C#-OOP-Advanced-July-2017/04. Generics-Exercises/Exercises/Threeuple/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        var tokens = Console.ReadLine().Split(' ');
+        var adressThreeuple = new CustomThreeuple<string, string, string>($"{tokens[0]} {tokens[1]}", tokens[2], tokens[3]);
+        tokens = Console.ReadLine().Split(' ');
+        var isDrunk = tokens[2] == "drunk";
+        var beerThreeuple = new CustomThreeuple<string, int, bool>(tokens[0], int.Parse(tokens[1]), isDrunk);
+        tokens = Console.ReadLine().Split(' ');
+        var bankThreeuple = new CustomThreeuple<string, double, string>(tokens[0], double.Parse(tokens[1]), tokens[2]);
+        Console.WriteLine(adressThreeuple);
+        Console.WriteLine(beerThreeuple);
+        Console.WriteLine(bankThreeuple);
+    }
+}
