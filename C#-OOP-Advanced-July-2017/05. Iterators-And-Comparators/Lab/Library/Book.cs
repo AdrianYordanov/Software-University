fcdc5f2f1aa33e1fa842a242716aff94094052a6
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-08-10 21:13:40 +0300

    01. Library
    Judge - 100/100.

---
+++ C#-OOP-Advanced-July-2017/05. Iterators-And-Comparators/Lab/Library/Book.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+public class Book
+{
+    private string title;
+    private int year;
+    private IReadOnlyList<string> authors;
+
+    public Book(string title, int year, params string[] authors)
+    {
+        this.Title = title;
+        this.Year = year;
+        this.Authors = authors;
+    }
+
+    public string Title
+    {
+        get => this.title;
+        set => this.title = value;
+    }
+
+    public int Year
+    {
+        get => this.year;
+        set => this.year = value;
+    }
+
+    public IReadOnlyList<string> Authors
+    {
+        get => this.authors;
+        set => this.authors = value;
+    }
+}
