dbf5df38fa196e045ad742ca351ea8e53edac48e
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-08-10 21:35:26 +0300

    02. Library Iterator
    Judge - 100/100

---
+++ C#-OOP-Advanced-July-2017/05. Iterators-And-Comparators/Lab/Library-Iterator/Library.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Library : IEnumerable<Book>
+{
+    private readonly IList<Book> books;
+
+    public Library(params Book[] books)
+    {
+        this.books = new List<Book>(books);
+    }
+
+    public IEnumerator<Book> GetEnumerator()
+    {
+        return new LibraryIterator(this.books);
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+
+    private class LibraryIterator : IEnumerator<Book>
+    {
+        private readonly IEnumerable<Book> books;
+        private int index;
+
+        public LibraryIterator(IEnumerable<Book> books)
+        {
+            this.Reset();
+            this.books = books;
+        }
+
+        object IEnumerator.Current => this.Current;
+
+        public Book Current => this.books.ElementAt(this.index);
+
+        public void Dispose()
+        {
+        }
+
+        public void Reset()
+        {
+            this.index = -1;
+        }
+
+        public bool MoveNext()
+        {
+            return (this.index += 1) < this.books.Count();
+        }
+    }
+}
