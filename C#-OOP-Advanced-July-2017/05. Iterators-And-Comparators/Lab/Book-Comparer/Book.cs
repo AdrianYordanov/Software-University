b2d0a9b4263ffaadd7a568574a3e6f45284062d5
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-08-11 00:04:35 +0300

    04. Book Comparer
    Judge - 100/100.

---
+++ C#-OOP-Advanced-July-2017/05. Iterators-And-Comparators/Lab/Book-Comparer/Book.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Book : IComparable<Book>
+{
+    private string title;
+    private int year;
+    private IReadOnlyList<string> authors;
+
+    public Book(string title, int year, params string[] authors)
+    {
+        this.Title = title;
+        this.Year = year;
+        this.Authors = authors;
+    }
+
+    public string Title
+    {
+        get => this.title;
+        set => this.title = value;
+    }
+
+    public int Year
+    {
+        get => this.year;
+        set => this.year = value;
+    }
+
+    public IReadOnlyList<string> Authors
+    {
+        get => this.authors;
+        set => this.authors = value;
+    }
+
+    public int CompareTo(Book other)
+    {
+        var result = this.Year.CompareTo(other.Year);
+        if (result == 0)
+        {
+            result = string.Compare(this.Title, other.Title, StringComparison.Ordinal);
+        }
+
+        return result;
+    }
+
+    public override string ToString()
+    {
+        return $"{this.Title} - {this.Year}";
+    }
+}
---
+++ C#-OOP-Advanced-July-2017/05. Iterators-And-Comparators/Lab/Book-Comparer/BookComparator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class BookComparator : IComparer<Book>
+{
+    public int Compare(Book first, Book second)
+    {
+        var result = string.Compare(first.Title, second.Title, StringComparison.CurrentCulture);
+        if (result == 0)
+        {
+            result = second.Year.CompareTo(first.Year);
+        }
+
+        return result;
+    }
+}
