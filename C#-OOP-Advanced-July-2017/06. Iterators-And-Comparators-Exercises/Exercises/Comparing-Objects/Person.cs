6073d1f5657815a9a851051af8b21877e3d22cae
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-08-11 15:12:38 +0300

    05. Comparing Objects
    Judge - 100/100.

---
+++ C#-OOP-Advanced-July-2017/06. Iterators-And-Comparators-Exercises/Exercises/Comparing-Objects/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+public class Person : IComparable<Person>
+{
+    private string name;
+    private int age;
+    private string town;
+
+    public Person(string name, int age, string town)
+    {
+        this.Name = name;
+        this.Age = age;
+        this.Town = town;
+    }
+
+    public string Name
+    {
+        get => this.name;
+        private set => this.name = value;
+    }
+
+    public int Age
+    {
+        get => this.age;
+        private set => this.age = value;
+    }
+
+    public string Town
+    {
+        get => this.town;
+        private set => this.town = value;
+    }
+
+    public int CompareTo(Person other)
+    {
+        var result = string.Compare(this.Name, other.Name, StringComparison.Ordinal);
+        if (result == 0)
+        {
+            result = this.Age.CompareTo(other.Age);
+            if (result == 0)
+            {
+                result = string.Compare(this.Town, other.Town, StringComparison.Ordinal);
+            }
+        }
+
+        return result;
+    }
+}
---
+++ C#-OOP-Advanced-July-2017/06. Iterators-And-Comparators-Exercises/Exercises/Comparing-Objects/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        var input = string.Empty;
+        var people = new List<Person>();
+        while ((input = Console.ReadLine()) != "END")
+        {
+            var tokens = input.Split(
+                    new[]
+                    {
+                        ' '
+                    },
+                    StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+            people.Add(new Person(tokens[0], int.Parse(tokens[1]), tokens[2]));
+        }
+
+        var n = int.Parse(Console.ReadLine()) - 1;
+        var specialPerson = people[n];
+        if (people.Count(x => x.CompareTo(specialPerson) == 0) > 1)
+        {
+            var equalPeople = people.Count(p => p.CompareTo(specialPerson) == 0);
+            var unequalPeople = people.Count(p => p.CompareTo(specialPerson) != 0);
+            Console.WriteLine($"{equalPeople} {unequalPeople} {people.Count}");
+        }
+        else
+        {
+            Console.WriteLine("No matches");
+        }
+    }
+}
