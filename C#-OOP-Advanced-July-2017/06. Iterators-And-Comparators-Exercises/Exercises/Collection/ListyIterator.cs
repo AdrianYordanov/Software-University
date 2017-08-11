55c1d51e92c63899ffe564b15e8338ce4fa8ad1e
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-08-11 14:09:18 +0300

    02. Collection
    Judge - 100/100.

---
+++ C#-OOP-Advanced-July-2017/06. Iterators-And-Comparators-Exercises/Exercises/Collection/ListyIterator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class ListyIterator<T> : IEnumerable<T>
+{
+    private readonly IList<T> collection;
+    private int index;
+
+    public ListyIterator(IEnumerable<T> items)
+    {
+        this.index = 0;
+        this.collection = new List<T>(items);
+    }
+
+    public IEnumerable<T> Collection => this.collection.AsEnumerable();
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        foreach (var element in this.Collection)
+        {
+            yield return element;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+
+    public bool HasNext()
+    {
+        return this.index + 1 < this.collection.Count;
+    }
+
+    public bool Move()
+    {
+        if (!this.HasNext())
+        {
+            return false;
+        }
+
+        this.index++;
+        return true;
+    }
+
+    public void Print()
+    {
+        if (this.collection.Any())
+        {
+            Console.WriteLine(this.collection[this.index]);
+        }
+        else
+        {
+            throw new InvalidOperationException("Invalid Operation!");
+        }
+    }
+}
---
+++ C#-OOP-Advanced-July-2017/06. Iterators-And-Comparators-Exercises/Exercises/Collection/StartUp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        var input = string.Empty;
+        ListyIterator<string> listly = null;
+        while ((input = Console.ReadLine()) != "END")
+        {
+            var tokens = input.Split(
+                    new[]
+                    {
+                        ' '
+                    },
+                    StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+            var command = tokens[0];
+            try
+            {
+                switch (command)
+                {
+                    case "Create":
+                        listly = new ListyIterator<string>(tokens.Skip(1));
+                        break;
+                    case "Move":
+                        Console.WriteLine(listly.Move());
+                        break;
+                    case "Print":
+                        listly.Print();
+                        break;
+                    case "HasNext":
+                        Console.WriteLine(listly.HasNext());
+                        break;
+                    case "PrintAll":
+                        Console.WriteLine(string.Join(" ", listly));
+                        break;
+                }
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
