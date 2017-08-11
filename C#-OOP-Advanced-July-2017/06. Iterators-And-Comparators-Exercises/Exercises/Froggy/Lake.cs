0a03e8e5e087424b014075a0b248085b483c42d1
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-08-11 14:21:44 +0300

    04. Froggy
    Judge - 100/100.

---
+++ C#-OOP-Advanced-July-2017/06. Iterators-And-Comparators-Exercises/Exercises/Froggy/Lake.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Lake : IEnumerable<int>
+{
+    private readonly IList<int> stones;
+
+    public Lake(IEnumerable<int> stones)
+    {
+        this.stones = new List<int>(stones);
+    }
+
+    public IEnumerator<int> GetEnumerator()
+    {
+        for (var i = 0; i < this.stones.Count; i += 2)
+        {
+            yield return this.stones[i];
+        }
+
+        for (var i = this.stones.Count - 1; i > 0; i--)
+        {
+            if (i % 2 == 0)
+            {
+                continue;
+            }
+
+            yield return this.stones[i];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+}
---
+++ C#-OOP-Advanced-July-2017/06. Iterators-And-Comparators-Exercises/Exercises/Froggy/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        var stones = Console.ReadLine()
+            .Split(
+                new[]
+                {
+                    ' ',
+                    ','
+                },
+                StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse);
+        var lake = new Lake(stones);
+        Console.WriteLine(string.Join(", ", lake));
+    }
+}
