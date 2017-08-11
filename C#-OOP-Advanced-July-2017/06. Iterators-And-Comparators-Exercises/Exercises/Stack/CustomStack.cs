f073c729eead37225be9eb23dc72190a2e9c4954
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-08-11 14:15:55 +0300

    03. Stack
    Judge - 100/100.

---
+++ C#-OOP-Advanced-July-2017/06. Iterators-And-Comparators-Exercises/Exercises/Stack/CustomStack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class CustomStack<T> : IEnumerable<T>
+{
+    private readonly IList<T> collection;
+
+    public CustomStack()
+    {
+        this.collection = new List<T>();
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (var i = this.collection.Count - 1; i >= 0; i--)
+        {
+            yield return this.collection[i];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+
+    public void Push(T item)
+    {
+        this.collection.Add(item);
+    }
+
+    public T Pop()
+    {
+        if (this.collection.Count <= 0)
+        {
+            throw new InvalidOperationException("No elements");
+        }
+
+        var last = this.collection.Last();
+        this.collection.Remove(last);
+        return last;
+    }
+}
---
+++ C#-OOP-Advanced-July-2017/06. Iterators-And-Comparators-Exercises/Exercises/Stack/StartUp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        var input = string.Empty;
+        var myStack = new CustomStack<int>();
+        while ((input = Console.ReadLine()) != "END")
+        {
+            var tokens = input.Split(
+                    new[]
+                    {
+                        ' ',
+                        ','
+                    },
+                    StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+            var command = tokens[0];
+            var numbers = tokens.Skip(1).Select(int.Parse).ToArray();
+            try
+            {
+                switch (command)
+                {
+                    case "Push":
+                        foreach (var num in numbers)
+                        {
+                            myStack.Push(num);
+                        }
+
+                        break;
+                    case "Pop":
+                        myStack.Pop();
+                        break;
+                }
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        if (myStack.Any())
+        {
+            for (var i = 0; i < 2; i++)
+            {
+                Console.WriteLine(string.Join(Environment.NewLine, myStack));
+            }
+        }
+    }
+}
