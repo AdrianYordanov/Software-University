da86c95d7667974b84cccd84e9539f1b07e0ad07
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-08-11 12:33:43 +0300

    01. Listy Iterator
    Judge - 100/100.

---
+++ C#-OOP-Advanced-July-2017/06. Iterators-And-Comparators-Exercises/Exercises/Listy-Iterator/ListyIterator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class ListyIterator<T>
+{
+    private readonly IList<T> collection;
+    private int index;
+
+    public ListyIterator(IEnumerable<T> items)
+    {
+        this.index = 0;
+        this.collection = new List<T>(items);
+    }
+
+    public IEnumerable<T> Collection => this.collection.AsEnumerable();
+
+    public bool HasNext()
+    {
+        return this.index + 1 < this.collection.Count;
+    }
+
+    public bool Move()
+    {
+        if (!this.HasNext())
+        {
+            return false;
+        }
+
+        this.index++;
+        return true;
+    }
+
+    public void Print()
+    {
+        if (this.collection.Any())
+        {
+            Console.WriteLine(this.collection[this.index]);
+        }
+        else
+        {
+            throw new InvalidOperationException("Invalid Operation!");
+        }
+    }
+}
