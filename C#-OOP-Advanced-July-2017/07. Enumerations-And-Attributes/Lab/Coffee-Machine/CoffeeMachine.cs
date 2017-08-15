3ad0416191594b13c684dd4cbf2c650dcccce7fc
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-08-15 20:28:28 +0300

    02. Coffee Machine
    Judge - 100/100.

---
+++ C#-OOP-Advanced-July-2017/07. Enumerations-And-Attributes/Lab/Coffee-Machine/CoffeeMachine.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class CoffeeMachine
+{
+    private readonly List<CoffeeType> coffeesSold;
+    private int insertedCoins;
+
+    public CoffeeMachine()
+    {
+        this.insertedCoins = 0;
+        this.coffeesSold = new List<CoffeeType>();
+    }
+
+    public IEnumerable<CoffeeType> CoffeesSold => this.coffeesSold.AsReadOnly();
+
+    public void BuyCoffee(string size, string type)
+    {
+        var coffeePrice = (int)Enum.Parse(typeof(CoffeePrice), size);
+        if (this.insertedCoins < coffeePrice)
+        {
+            return;
+        }
+
+        this.insertedCoins = 0;
+        this.coffeesSold.Add((CoffeeType)Enum.Parse(typeof(CoffeeType), type));
+    }
+
+    public void InsertCoin(string coin)
+    {
+        this.insertedCoins += (int)Enum.Parse(typeof(Coin), coin);
+    }
+}
---
+++ C#-OOP-Advanced-July-2017/07. Enumerations-And-Attributes/Lab/Coffee-Machine/CoffeePrice.cs
@@ -0,0 +1,7 @@
+﻿public enum CoffeePrice
+{
+    None,
+    Small = Coin.Fifty,
+    Normal = 2 * Coin.Fifty,
+    Double = 4 * Coin.Fifty
+}
