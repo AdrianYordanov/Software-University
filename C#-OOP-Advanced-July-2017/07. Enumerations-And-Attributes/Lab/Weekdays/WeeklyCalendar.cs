f1598b3055ed64f4897424dc426bbfd20d0b9b65
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-08-15 19:56:11 +0300

    01. Weekdays
    Judge - 100/100.

---
+++ C#-OOP-Advanced-July-2017/07. Enumerations-And-Attributes/Lab/Weekdays/WeeklyCalendar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+public class WeeklyCalendar
+{
+    private readonly List<WeeklyEntry> weeklySchedule;
+
+    public WeeklyCalendar()
+    {
+        this.weeklySchedule = new List<WeeklyEntry>();
+    }
+
+    public IEnumerable<WeeklyEntry> WeeklySchedule => this.weeklySchedule.AsReadOnly();
+
+    public void AddEntry(string weekDay, string notes)
+    {
+        this.weeklySchedule.Add(new WeeklyEntry(weekDay, notes));
+    }
+}
---
+++ C#-OOP-Advanced-July-2017/07. Enumerations-And-Attributes/Lab/Weekdays/WeeklyEntry.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+public class WeeklyEntry : IComparable<WeeklyEntry>
+{
+    private readonly WeekDay weekDay;
+    private string notes;
+
+    public WeeklyEntry(string weekDay, string notes)
+    {
+        Enum.TryParse(weekDay, out this.weekDay);
+        this.Notes = notes;
+    }
+
+    public WeekDay WeekDay => this.weekDay;
+
+    public string Notes
+    {
+        get => this.notes;
+        set => this.notes = value;
+    }
+
+    public int CompareTo(WeeklyEntry other)
+    {
+        if (ReferenceEquals(this, other))
+        {
+            return 0;
+        }
+
+        if (ReferenceEquals(null, other))
+        {
+            return 1;
+        }
+
+        var result = this.WeekDay.CompareTo(other.WeekDay);
+        if (result == 0)
+        {
+            result = string.Compare(this.Notes, this.Notes, StringComparison.Ordinal);
+        }
+
+        return result;
+    }
+
+    public override string ToString()
+    {
+        return $"{this.WeekDay} - {this.Notes}";
+    }
+}
