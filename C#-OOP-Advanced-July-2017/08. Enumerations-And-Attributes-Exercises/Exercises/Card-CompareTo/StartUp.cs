99553fab698f58a944ba986670745e7a5b1f11bf
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-08-17 18:47:18 +0300

    05. Card CompareTo
    Judge - 100/100.

---
+++ C#-OOP-Advanced-July-2017/08. Enumerations-And-Attributes-Exercises/Exercises/Card-CompareTo/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        var rank = Console.ReadLine();
+        var suit = Console.ReadLine();
+        var firstCard = new Card(rank, suit);
+        rank = Console.ReadLine();
+        suit = Console.ReadLine();
+        var secondCard = new Card(rank, suit);
+        Console.WriteLine(firstCard.CompareTo(secondCard) > 0 ? firstCard : secondCard);
+    }
+}
---
+++ C#-OOP-Advanced-July-2017/08. Enumerations-And-Attributes-Exercises/Exercises/Card-CompareTo/Suit.cs
@@ -0,0 +1,8 @@
+﻿public enum Suit
+{
+    None = -1,
+    Clubs = 0,
+    Diamonds = 13,
+    Hearts = 26,
+    Spades = 39
+}
