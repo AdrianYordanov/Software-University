038faa227584fee758893a5506df264ee7f8eca3
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-08-17 18:28:10 +0300

    03. Card Power
    Judge - 100/100.

---
+++ C#-OOP-Advanced-July-2017/08. Enumerations-And-Attributes-Exercises/Exercises/Card-Power/Card.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Card
+{
+    private readonly Rank rank;
+    private readonly Suit suit;
+
+    public Card(string rank, string suit)
+    {
+        Enum.TryParse(rank, out this.rank);
+        Enum.TryParse(suit, out this.suit);
+    }
+
+    public int CardPower => (int)this.rank + (int)this.suit;
+}
---
+++ C#-OOP-Advanced-July-2017/08. Enumerations-And-Attributes-Exercises/Exercises/Card-Power/Suit.cs
@@ -0,0 +1,8 @@
+﻿public enum Suit
+{
+    None,
+    Clubs = 0,
+    Diamonds = 13,
+    Hearts = 26,
+    Spades = 39
+}
