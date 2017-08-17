40d2838880555646a1d79c0185919745ec6de4a5
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-08-17 18:03:03 +0300

    01. Card Suit
    Judge - 100/100.

---
+++ C#-OOP-Advanced-July-2017/08. Enumerations-And-Attributes-Exercises/Exercises/Card-Suit/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        Console.WriteLine("Card Suits:");
+        foreach (var suit in Enum.GetNames(typeof(Suit)))
+        {
+            Console.WriteLine($"Ordinal value: {(int)Enum.Parse(typeof(Suit), suit)}; Name value: {suit}");
+        }
+    }
+}
