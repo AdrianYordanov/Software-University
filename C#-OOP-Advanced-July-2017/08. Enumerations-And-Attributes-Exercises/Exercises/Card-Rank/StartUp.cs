78082d7e7c9b1e796f00a1e92d7849b4471d2b50
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-08-17 18:07:52 +0300

    02. Card Rank
    Judge - 100/100.

---
+++ C#-OOP-Advanced-July-2017/08. Enumerations-And-Attributes-Exercises/Exercises/Card-Rank/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        Console.WriteLine("Card Ranks:");
+        foreach (var rank in Enum.GetNames(typeof(Rank)))
+        {
+            Console.WriteLine($"Ordinal value: {(int)Enum.Parse(typeof(Rank), rank)}; Name value: {rank}");
+        }
+    }
+}
