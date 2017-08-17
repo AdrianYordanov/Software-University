4436d420c692d1240b1fb252a0bcc433cb2c4412
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-08-17 18:37:29 +0300

    04. Card ToString
    Judge - 100/100.

---
+++ C#-OOP-Advanced-July-2017/08. Enumerations-And-Attributes-Exercises/Exercises/Card-ToString/Card.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Card
+{
+    private readonly Rank rank;
+    private readonly Suit suit;
+
+    public Card(string rank, string suit)
+    {
+        Enum.TryParse(rank, out this.rank);
+        Enum.TryParse(suit, out this.suit);
+    }
+
+    public int CardPower => (int)this.rank + (int)this.suit;
+
+    public override string ToString()
+    {
+        return $"Card name: {this.rank} of {this.suit}; Card power: {this.CardPower}";
+    }
+}
---
+++ C#-OOP-Advanced-July-2017/08. Enumerations-And-Attributes-Exercises/Exercises/Card-ToString/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        var rank = Console.ReadLine();
+        var suit = Console.ReadLine();
+        var card = new Card(rank, suit);
+        Console.WriteLine(card);
+    }
+}
