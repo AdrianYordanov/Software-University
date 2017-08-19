ebd33095b305be4d41a3161cb2b894f06536c5b4
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-08-19 14:24:45 +0300

    06. Custom Enum Attribute
    Judge - 100/100.

---
+++ C#-OOP-Advanced-July-2017/08. Enumerations-And-Attributes-Exercises/Exercises/Custom-Enum-Attribute/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        var input = Console.ReadLine();
+        var enumType = Type.GetType(input);
+        var attributes = enumType.GetCustomAttributes(false);
+        var firstAttribute = (TypeAttribute)attributes[0];
+        Console.WriteLine($"Type = {firstAttribute.Type}, Description = {firstAttribute.Description}");
+    }
+}
---
+++ C#-OOP-Advanced-July-2017/08. Enumerations-And-Attributes-Exercises/Exercises/Custom-Enum-Attribute/TypeAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Enum)]
+public class TypeAttribute : Attribute
+{
+    private string type;
+    private string category;
+    private string description;
+
+    public TypeAttribute(string type, string category, string description)
+    {
+        this.Type = type;
+        this.Category = category;
+        this.Description = description;
+    }
+
+    public string Type
+    {
+        get => this.type;
+        set => this.type = value;
+    }
+
+    public string Category
+    {
+        get => this.category;
+        set => this.category = value;
+    }
+
+    public string Description
+    {
+        get => this.description;
+        set => this.description = value;
+    }
+}
