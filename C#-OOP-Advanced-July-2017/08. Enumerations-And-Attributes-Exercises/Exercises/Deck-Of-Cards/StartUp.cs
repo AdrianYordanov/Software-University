353bf7d00dbf3b50b841b6257a7a0d0e0ce58d80
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-08-19 15:51:26 +0300

    07. Deck of Cards
    Judge - 100/100.

---
+++ C#-OOP-Advanced-July-2017/08. Enumerations-And-Attributes-Exercises/Exercises/Deck-Of-Cards/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        foreach (var suit in Enum.GetNames(typeof(Suit)))
+        {
+            foreach (var rank in Enum.GetNames(typeof(Rank)))
+            {
+                Console.WriteLine($"{rank} of {suit}");
+            }
+        }
+    }
+}
