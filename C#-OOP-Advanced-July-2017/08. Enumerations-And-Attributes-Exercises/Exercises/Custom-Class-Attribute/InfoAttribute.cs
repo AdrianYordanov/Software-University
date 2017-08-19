0a4e9504e571075b022c124c811dbdacc1906d45
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-08-19 16:37:35 +0300

    10. Create Custom Class Attribute
    Judge - 100/100.

---
+++ C#-OOP-Advanced-July-2017/08. Enumerations-And-Attributes-Exercises/Exercises/Custom-Class-Attribute/InfoAttribute.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+public class InfoAttribute : Attribute
+{
+    private readonly List<string> reviewers;
+    private string author;
+    private int revision;
+    private string description;
+
+    public InfoAttribute(string author, int revision, string description, params string[] reviewers)
+    {
+        this.Author = author;
+        this.Revision = revision;
+        this.Description = description;
+        this.reviewers = new List<string>(reviewers);
+    }
+
+    public string Author
+    {
+        get => this.author;
+        private set => this.author = value;
+    }
+
+    public int Revision
+    {
+        get => this.revision;
+        private set => this.revision = value;
+    }
+
+    public string Description
+    {
+        get => this.description;
+        private set => this.description = value;
+    }
+
+    public IReadOnlyCollection<string> Reviewers => this.reviewers.AsReadOnly();
+
+    public void PrintInfo(string field)
+    {
+        switch (field)
+        {
+            case "Author":
+                Console.WriteLine($"Author: {this.Author}");
+                break;
+            case "Revision":
+                Console.WriteLine($"Revision: {this.Revision}");
+                break;
+            case "Description":
+                Console.WriteLine($"Class description: {this.Description}");
+                break;
+            case "Reviewers":
+                Console.WriteLine($"Reviewers: {string.Join(", ", this.Reviewers)}");
+                break;
+        }
+    }
+}
---
+++ C#-OOP-Advanced-July-2017/08. Enumerations-And-Attributes-Exercises/Exercises/Custom-Class-Attribute/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+[Info("Pesho", 3, "Used for C# OOP Advanced Course - Enumerations and Attributes.", "Pesho", "Svetlio")]
+public class StartUp
+{
+    public static void Main()
+    {
+        var input = string.Empty;
+        var attr = (InfoAttribute)typeof(StartUp).GetCustomAttributes(true)[0];
+        while ((input = Console.ReadLine()) != "END")
+        {
+            attr.PrintInfo(input);
+        }
+    }
+}
