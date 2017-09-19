fd44f442db5727132a4fa5b52c449736d9d09618
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-09-19 14:12:57 +0300

    07. Create Transactions Table and a Trigger

---
+++ Databases-Basics-MS-SQL-Server-September-2017/01. Introduction-to-Databases/Lab/Create-Transactions-Table-And-Trigger/Create-Table-Transactions.sql
@@ -0,0 +1,10 @@
+USE Bank
+
+CREATE TABLE Transactions (
+	Id INT PRIMARY KEY IDENTITY,
+	AccountId INT FOREIGN KEY REFERENCES Accounts(Id),
+	OldBalance DECIMAL(15, 2) NOT NULL,
+	NewBalance DECIMAL(15, 2) NOT NULL,
+	Amount AS NewBalance - OldBalance,
+	[DateTime] DATETIME2
+)
---
+++ Databases-Basics-MS-SQL-Server-September-2017/01. Introduction-to-Databases/Lab/Create-Transactions-Table-And-Trigger/Create-Trigger.sql
@@ -0,0 +1,6 @@
+CREATE TRIGGER tr_Transaction ON Accounts
+AFTER UPDATE
+AS
+	INSERT INTO Transactions (AccountId, OldBalance, NewBalance, [DateTime])
+	SELECT inserted.Id, deleted.Balance, inserted.Balance, GETDATE() FROM inserted
+	JOIN deleted ON inserted.Id = deleted.Id
