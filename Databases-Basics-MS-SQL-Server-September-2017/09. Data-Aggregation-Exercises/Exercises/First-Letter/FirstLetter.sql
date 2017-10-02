22f8edd5d47a55fe6b13c9efd66f268a26856898
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-10-02 19:45:44 +0300

    New solution

--- Databases-Basics-MS-SQL-Server-September-2017/09. Data-Aggregation-Exercises/Exercises/First-Letter/FirstLetter.sql
+++ Databases-Basics-MS-SQL-Server-September-2017/09. Data-Aggregation-Exercises/Exercises/First-Letter/FirstLetter.sql
@@ -3,4 +3,4 @@ USE Gringotts
 SELECT LEFT(FirstName, 1) AS [FirstLetter] FROM WizzardDeposits
 WHERE DepositGroup = 'Troll Chest'
 GROUP BY LEFT(FirstName, 1)
-ORDER BY LEFT(FirstName, 1)
+ORDER BY [FirstLetter]
