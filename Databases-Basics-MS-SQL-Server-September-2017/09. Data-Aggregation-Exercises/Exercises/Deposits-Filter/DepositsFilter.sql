508ade7d603b1e566fd4b2a6cd48e1003f30b5d6
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-10-02 19:05:06 +0300

    07. Deposits Filter
    Judge - 100/100.

---
+++ Databases-Basics-MS-SQL-Server-September-2017/09. Data-Aggregation-Exercises/Exercises/Deposits-Filter/DepositsFilter.sql
@@ -0,0 +1,7 @@
+USE Gringotts
+
+SELECT DepositGroup, SUM(DepositAmount) AS [TotalSum] FROM WizzardDeposits
+WHERE MagicWandCreator = 'Ollivander family'
+GROUP BY DepositGroup
+HAVING SUM(DepositAmount) < 150000
+ORDER BY [TotalSum] DESC
