b8718ab35c259c1db12e802ae2b02f67743caf48
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-10-05 18:23:26 +0300

    01. One-To-One Relationship
    Judge - 100/100.

---
+++ Databases-Basics-MS-SQL-Server-September-2017/11. Table-Relations-Exercises/Exercises/One-To-One-Relationship/OneToOneRelationship.sql
@@ -0,0 +1,25 @@
+USE ExampleDatabase
+
+CREATE TABLE Passports (
+	PassportID INT PRIMARY KEY IDENTITY(101, 1),
+	PassportNumber VARCHAR(50) NOT NULL,
+	CONSTRAINT UQ_PassportNumber UNIQUE (PassportNumber)
+)
+
+CREATE TABLE Persons (
+	PersonID INT PRIMARY KEY IDENTITY,
+	FirstName VARCHAR(50) NOT NULL,
+	SALARY DECIMAL NOT NULL,
+	PassportID INT FOREIGN KEY REFERENCES Passports(PassportID) NOT NULL,
+	CONSTRAINT QU_PassportID UNIQUE(PassportID)
+)
+
+INSERT INTO Passports VALUES
+('N34FG21B'),
+('K65LO4R7'),
+('ZE657QP2')
+
+INSERT INTO Persons VALUES
+('Roberto', 43300.00, 102),
+('Tom', 56100.00, 103),
+('Yana', 60200.00, 101)
