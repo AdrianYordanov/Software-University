66631d5e8dc94cc276164dac56c3f2f41f78acf6
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-10-05 18:30:23 +0300

    02. One-To-Many Relationship
    Judge - 100/100.

---
+++ Databases-Basics-MS-SQL-Server-September-2017/11. Table-Relations-Exercises/Exercises/One-To-Many-Relationship/One-To-Many-Relationship.sql
@@ -0,0 +1,26 @@
+USE ExampleDatabase
+
+CREATE TABLE Manufacturers (
+	ManufacturerID INT PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(50) NOT NULL,
+	EstablishedOn DATE NOT NULL
+)
+
+CREATE TABLE Models (
+	ModelID INT PRIMARY KEY IDENTITY(101, 1),
+	[Name] VARCHAR(50) NOT NULL,
+	ManufacturerID INT FOREIGN KEY REFERENCES Manufacturers(ManufacturerID) NOT NULL,
+)
+
+INSERT INTO Manufacturers VALUES
+('BMW', '07/03/1916'),
+('Tesla', '01/01/2003'),
+('Lada', '01/05/1966')
+
+INSERT INTO Models VALUES
+('X1', 1),
+('i6', 1),
+('Model S', 2),
+('Model X', 2),
+('Model 3', 2),
+('Nova', 3)
