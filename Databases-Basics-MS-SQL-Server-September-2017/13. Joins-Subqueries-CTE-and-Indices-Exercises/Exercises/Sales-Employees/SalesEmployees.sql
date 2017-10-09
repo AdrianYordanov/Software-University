8272cdbcfa4bdc43058d0625723d97c6549f3754
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-10-09 18:40:44 +0300

    03. Sales Employees
    Judge - 100/100.

---
+++ Databases-Basics-MS-SQL-Server-September-2017/13. Joins-Subqueries-CTE-and-Indices-Exercises/Exercises/Sales-Employees/SalesEmployees.sql
@@ -0,0 +1,7 @@
+USE SoftUni
+
+SELECT e.EmployeeID, e.FirstName, e.LastName, d.Name AS [DepartmentName]
+FROM Employees AS e
+INNER JOIN Departments AS d ON d.DepartmentID = e.DepartmentID
+WHERE d.Name = 'Sales'
+ORDER BY e.EmployeeID
