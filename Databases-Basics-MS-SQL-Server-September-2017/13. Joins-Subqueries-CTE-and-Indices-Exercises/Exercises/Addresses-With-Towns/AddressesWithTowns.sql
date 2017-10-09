543d26060bd508f820758af6ed6816112f4bfd6d
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-10-09 18:38:35 +0300

    Add alies on query.

--- Databases-Basics-MS-SQL-Server-September-2017/13. Joins-Subqueries-CTE-and-Indices-Exercises/Exercises/Addresses-With-Towns/AddressesWithTowns.sql
+++ Databases-Basics-MS-SQL-Server-September-2017/13. Joins-Subqueries-CTE-and-Indices-Exercises/Exercises/Addresses-With-Towns/AddressesWithTowns.sql
@@ -1,6 +1,6 @@
 USE SoftUni
 
-SELECT TOP(50) e.FirstName, e.LastName, t.Name, a.AddressText
+SELECT TOP(50) e.FirstName, e.LastName, t.Name AS [Town], a.AddressText
 FROM Employees AS e
 INNER JOIN Addresses AS a ON a.AddressID = e.AddressID
 INNER JOIN Towns AS t ON t.TownID = a.TownID
