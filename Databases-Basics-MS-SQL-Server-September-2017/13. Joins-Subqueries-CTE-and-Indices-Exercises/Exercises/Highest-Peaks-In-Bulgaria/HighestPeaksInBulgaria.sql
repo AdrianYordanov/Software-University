667c3715bf12cddb5acd4da5850c7b174232ab65
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-10-09 20:29:56 +0300

    12. Highest Peaks in Bulgaria
    Judge - 100/100.

---
+++ Databases-Basics-MS-SQL-Server-September-2017/13. Joins-Subqueries-CTE-and-Indices-Exercises/Exercises/Highest-Peaks-In-Bulgaria/HighestPeaksInBulgaria.sql
@@ -0,0 +1,9 @@
+USE [Geography]
+
+SELECT c.CountryCode, m.MountainRange, p.PeakName, p.Elevation
+FROM Countries AS c
+INNER JOIN MountainsCountries AS mc ON mc.CountryCode = c.CountryCode
+INNER JOIN Mountains AS m ON m.Id = mc.MountainId
+INNER JOIN Peaks AS p ON p.MountainId = m.Id
+WHERE c.CountryName = 'Bulgaria' AND p.Elevation > 2835
+ORDER BY p.Elevation DESC
