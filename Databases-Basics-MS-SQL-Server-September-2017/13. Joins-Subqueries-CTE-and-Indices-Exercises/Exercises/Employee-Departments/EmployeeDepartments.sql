3b204bb005b2a590a3f31adef9f0a9d7c1383d72
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-10-09 18:45:14 +0300

    04. Employee Departments
    Judge - 100/100.

---
+++ Databases-Basics-MS-SQL-Server-September-2017/13. Joins-Subqueries-CTE-and-Indices-Exercises/Exercises/Employee-Departments/EmployeeDepartments.sql
@@ -0,0 +1,7 @@
+USE SoftUni
+
+SELECT TOP(5) e.EmployeeID, e.FirstName, e.Salary, d.Name AS [DepartmentName]
+FROM Employees AS e
+INNER JOIN Departments AS d ON d.DepartmentID = e.DepartmentID
+WHERE e.Salary > 15000
+ORDER BY e.DepartmentID
