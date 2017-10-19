aadd834acd50210e29dad89dc672ca8fa86e897c
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-10-19 18:10:33 +0300

    Presentation and lab
    There are some exercises. You should try to do comflict in github with the resources.

---
+++ Practical-Teamwork-September-2017/01. Source-Control-Systems-And-GitHub-Resources/Lab/maria/towns.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+	$('#btnDelete').click(deleteTown);
+});
+
+function deleteTown() {
+	let townName = $('#townName').val();
+	$('#townName').val('');
+	let removed = false;
+	for (let option of $('#towns option')) {
+		if (option.textContent == townName) {
+			removed = true;
+			option.remove();
+		}
+	}
+	if (removed)
+		showMessage(townName + " deleted.");
+	else
+		showMessage(townName + " not found.");
+}
+
+function showMessage(msg) {
+	$('#result').text(msg).css("display", "block");
+	setTimeout(function() {
+		$('#result').hide('blind', {}, 500);		
+	}, 3000);	
+}
---
+++ Practical-Teamwork-September-2017/01. Source-Control-Systems-And-GitHub-Resources/Lab/peter/towns.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+	$('#btnDelete').click(deleteTown);
+	$('#btnAdd').click(addTown);
+});
+
+function deleteTown() {
+	let townName = $('#townName').val();
+	$('#townName').val('');
+	let removed = false;
+	for (let option of $('#towns option')) {
+		if (option.textContent == townName) {
+			removed = true;
+			option.remove();
+		}
+	}
+	if (removed)
+		$('#result').text(townName + " deleted.");
+	else
+		$('#result').text(townName + " not found.");
+}
+
+function addTown() {
+	let townName = $('#townNameForAdd').val();
+	$('#townNameForAdd').val('');
+	$('#towns').append($('<option>').text(townName));
+	$('#result').text(townName + " added.");
+}
