7e70fc55b5577ccac2d842a9d55d81e3ae59d3bb
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-10-25 14:59:55 +0300

    07. Find Variable Names in Sentences

---
+++ JS-Fundamentals-September-2016/10. Strings-and-RegExp-Exercises/Exercises/Find-Variable-Names-In-Sentences/Find-Variable-Names-In-Sentences.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function findVariableNamesInSentences([str]) {
+    let variables =[];
+    let regex = /\b_([a-zA-Z0-9]+)\b/g;
+
+    let match = regex.exec(str);
+
+    while(match) {
+        variables.push(match[1]);
+        match = regex.exec(str);
+    }
+
+    console.log(variables.join(","));
+}
