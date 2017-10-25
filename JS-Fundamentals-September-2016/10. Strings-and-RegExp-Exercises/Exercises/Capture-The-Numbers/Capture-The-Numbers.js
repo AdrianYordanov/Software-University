f55109c2553621bad9701840e261be9af64c52e2
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-10-25 14:58:24 +0300

    06. Capture the Numbers

---
+++ JS-Fundamentals-September-2016/10. Strings-and-RegExp-Exercises/Exercises/Capture-The-Numbers/Capture-The-Numbers.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function captureTheNumbers(input) {
+    let numbers = [];
+    let regex =/\d+/g;
+
+    for(let line of input) {
+        let match = regex.exec(line);
+        while(match) {
+            numbers.push(match[0]);
+            match = regex.exec(line);
+        }
+    }
+
+    console.log(numbers.join(" "));
+}
