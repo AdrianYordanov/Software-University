ea76936491c02c86d2282ce5b184c7cde5d1a916
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-10-27 10:48:33 +0300

    Exercises

---
+++ Databases-Advanced-Entity-Framework-October-2017/02. Defining-Classes-Exercises/Exercises/Company-Roster/StartUp.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class StartUp
+{
+    private static void Main()
+    {
+        var n = int.Parse(Console.ReadLine());
+        var employees = new List<Employee>();
+        for (var i = 0; i < n; i++)
+        {
+            var tokens = Console.ReadLine()
+                .Split(' ');
+            var name = tokens[0];
+            var salary = decimal.Parse(tokens[1]);
+            var position = tokens[2];
+            var department = tokens[3];
+            if (tokens.Length == 4)
+            {
+                employees.Add(new Employee(name, salary, position, department));
+            }
+            else if (tokens.Length == 5)
+            {
+                var tempForAge = 0;
+                if (int.TryParse(tokens[4], out tempForAge))
+                {
+                    employees.Add(new Employee(name, salary, position, department, tempForAge));
+                }
+                else
+                {
+                    employees.Add(new Employee(name, salary, position, department, tokens[4]));
+                }
+            }
+            else if (tokens.Length == 6)
+            {
+                employees.Add(new Employee(name, salary, position, department, tokens[4], int.Parse(tokens[5])));
+            }
+        }
+
+        var bestDepartment = employees.GroupBy(employee => employee.Department, employee => employee.Salary)
+            .OrderByDescending(departmentGroup => departmentGroup.Sum() / departmentGroup.Count())
+            .First()
+            .Key;
+        var fromDepartment = employees.Where(employee => employee.Department == bestDepartment)
+            .OrderByDescending(employee => employee.Salary);
+        Console.WriteLine($"Highest Average Salary: {bestDepartment}");
+        Console.WriteLine(string.Join(Environment.NewLine, fromDepartment));
+    }
+}
