9596bf04546478116d7e776af047760e7381b5d1
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-10-27 09:16:35 +0300

    Lab and all problems in it.

---
+++ Databases-Advanced-Entity-Framework-October-2017/01. Defining-Classes/Lab/Person-Class/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class StartUp
+{
+    static void Main()
+    {
+        var accounts = new List<BankAccount>()
+        {
+            new BankAccount(){ ID = 1, Balance = 15},
+            new BankAccount(){ ID = 1, Balance = 20},
+            new BankAccount(){ ID = 1, Balance = 3},
+            new BankAccount(){ ID = 1, Balance = 5},
+        };
+
+        var person = new Person("Pehso", 18, accounts);
+        Console.WriteLine(person.GetBalance());
+    }
+}
