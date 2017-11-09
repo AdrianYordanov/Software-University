8d6842fbd0d176bcde64fcc45aac3bc93a6bb9fa
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-11-09 19:01:43 +0200

    02. Villain Names

---
+++ Databases-Advanced-Entity-Framework-October-2017/02. DB-Apps-Introduction-Exercises/Exercises/Villain-Names/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data.SqlClient;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        var builder = new SqlConnectionStringBuilder
+        {
+            // Change '.' with the name of your SQL Server, if it doesn't work.
+            ["Data Source"] = @"DESKTOP-FPETI1U\SQLEXPRESS",
+            ["Integrated Security"] = true,
+            ["initial catalog"] = "MinionsDB"
+        };
+        var connection = new SqlConnection(builder.ToString());
+        connection.Open();
+        using (connection)
+        {
+            try
+            {
+                var command = new SqlCommand(
+                    "SELECT v.[Name], COUNT(*) AS [Minions Count] FROM Villains AS v\n" +
+                    "INNER JOIN MinionsVillains AS mv ON mv.VillainId = v.Id\n" +
+                    "GROUP BY v.[Name]\n" +
+                    "HAVING COUNT(*) > 3\n" +
+                    "ORDER BY[Minions Count] DESC\n",
+                    connection);
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine($"{reader["Name"]} - {reader["Minions Count"]}");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
