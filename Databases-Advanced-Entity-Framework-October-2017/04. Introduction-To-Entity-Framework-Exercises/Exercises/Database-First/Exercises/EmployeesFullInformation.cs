a3d4927fcae667904f75eb6768cad771c71b35d9
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-11-28 23:33:21 +0200

    03. Employees Full Information
    Judge - 100/100

---
+++ Databases-Advanced-Entity-Framework-October-2017/04. Introduction-To-Entity-Framework-Exercises/Exercises/Database-First/Exercises/EmployeesFullInformation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using P02_DatabaseFirst.Data;
+
+public class EmployeesFullInformation
+{
+    public void Run()
+    {
+        var db = new SoftUniContext();
+        var employees = db.Employees.Select(
+            e => new
+            {
+                FullName = $"{e.FirstName} {e.LastName} {e.MiddleName}",
+                e.JobTitle,
+                e.Salary
+            });
+        foreach (var employee in employees)
+        {
+            Console.WriteLine($"{employee.FullName} {employee.JobTitle} {employee.Salary:F2}");
+        }
+    }
+}
--- Databases-Advanced-Entity-Framework-October-2017/04. Introduction-To-Entity-Framework-Exercises/Exercises/Database-First/StartUp.cs
+++ Databases-Advanced-Entity-Framework-October-2017/04. Introduction-To-Entity-Framework-Exercises/Exercises/Database-First/StartUp.cs
@@ -2,5 +2,6 @@
 {
     public static void Main()
     {
+        // new EmployeesFullInformation().Run();
     }
 }
