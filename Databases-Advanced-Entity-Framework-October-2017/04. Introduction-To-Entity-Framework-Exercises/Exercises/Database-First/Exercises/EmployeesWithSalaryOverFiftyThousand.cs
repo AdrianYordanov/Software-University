2328b311cf44c23e36a4ce0bb9999b3e1da1becb
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-11-28 23:43:20 +0200

    04. Employees with Salary Over 50 000
    Judge - 100/100.

---
+++ Databases-Advanced-Entity-Framework-October-2017/04. Introduction-To-Entity-Framework-Exercises/Exercises/Database-First/Exercises/EmployeesWithSalaryOverFiftyThousand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using P02_DatabaseFirst.Data;
+
+public class EmployeesWithSalaryOverFiftyThousand
+{
+    public void Run()
+    {
+        var db = new SoftUniContext();
+        var employees = db.Employees.Where(e => e.Salary > 50000)
+            .OrderBy(e => e.FirstName)
+            .Select(e => e.FirstName)
+            .ToList();
+        foreach (var employee in employees)
+        {
+            Console.WriteLine(employee);
+        }
+    }
+}
--- Databases-Advanced-Entity-Framework-October-2017/04. Introduction-To-Entity-Framework-Exercises/Exercises/Database-First/StartUp.cs
+++ Databases-Advanced-Entity-Framework-October-2017/04. Introduction-To-Entity-Framework-Exercises/Exercises/Database-First/StartUp.cs
@@ -3,5 +3,6 @@
     public static void Main()
     {
         // new EmployeesFullInformation().Run();
+        // new EmployeesWithSalaryOverFiftyThousand().Run();
     }
 }
