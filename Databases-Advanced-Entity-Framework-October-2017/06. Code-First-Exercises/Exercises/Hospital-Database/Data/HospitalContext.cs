64070c4f86c2f1dceb1d28757654c4994046de4f
Adrian Yordanov <AdrianYordanov@users.noreply.github.com>
2017-11-30 13:25:58 +0200

    01. Hospital Database
    Judge - 100/100.

---
+++ Databases-Advanced-Entity-Framework-October-2017/06. Code-First-Exercises/Exercises/Hospital-Database/Data/HospitalContext.cs
@@ -0,0 +1,76 @@
+﻿namespace P01_HospitalDatabase.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using Models;
+
+    public class HospitalContext : DbContext
+    {
+        public HospitalContext()
+        {
+        }
+
+        public HospitalContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Visitation> Visitations
+        {
+            get;
+            set;
+        }
+
+        public DbSet<Diagnose> Diagnoses
+        {
+            get;
+            set;
+        }
+
+        public DbSet<Patient> Patients
+        {
+            get;
+            set;
+        }
+
+        public DbSet<Medicament> Medicaments
+        {
+            get;
+            set;
+        }
+
+        public DbSet<PatientMedicament> PatientsMedicaments
+        {
+            get;
+            set;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Patient>()
+                .HasMany(p => p.Visitations)
+                .WithOne(v => v.Patient)
+                .HasForeignKey(v => v.PatientId);
+            modelBuilder.Entity<Patient>()
+                .HasMany(p => p.Diagnoses)
+                .WithOne(d => d.Patient)
+                .HasForeignKey(d => d.PatientId);
+            modelBuilder.Entity<PatientMedicament>()
+                .HasKey(
+                    pm => new
+                    {
+                        pm.PatientId,
+                        pm.MedicamentId
+                    });
+        }
+    }
+}
---
+++ Databases-Advanced-Entity-Framework-October-2017/06. Code-First-Exercises/Exercises/Hospital-Database/Migrations/20171130105350_Initial.cs
@@ -0,0 +1,130 @@
+﻿namespace HospitalDatabase.Migrations
+{
+    using System;
+    using Microsoft.EntityFrameworkCore.Metadata;
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                "Medicaments",
+                table => new
+                {
+                    MedicamentId = table.Column<int>(nullable: false)
+                        .Annotation(
+                            "SqlServer:ValueGenerationStrategy",
+                            SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Medicaments", x => x.MedicamentId);
+                });
+            migrationBuilder.CreateTable(
+                "Patients",
+                table => new
+                {
+                    PatientId = table.Column<int>(nullable: false)
+                        .Annotation(
+                            "SqlServer:ValueGenerationStrategy",
+                            SqlServerValueGenerationStrategy.IdentityColumn),
+                    Address = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    FirstName = table.Column<string>(nullable: true),
+                    HasInsurance = table.Column<bool>(nullable: false),
+                    LastName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Patients", x => x.PatientId);
+                });
+            migrationBuilder.CreateTable(
+                "Diagnoses",
+                table => new
+                {
+                    DiagnoseId = table.Column<int>(nullable: false)
+                        .Annotation(
+                            "SqlServer:ValueGenerationStrategy",
+                            SqlServerValueGenerationStrategy.IdentityColumn),
+                    Comments = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    PatientId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Diagnoses", x => x.DiagnoseId);
+                    table.ForeignKey(
+                        "FK_Diagnoses_Patients_PatientId",
+                        x => x.PatientId,
+                        "Patients",
+                        "PatientId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+            migrationBuilder.CreateTable(
+                "PatientsMedicaments",
+                table => new
+                {
+                    PatientId = table.Column<int>(nullable: false),
+                    MedicamentId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey(
+                        "PK_PatientsMedicaments",
+                        x => new
+                        {
+                            x.PatientId,
+                            x.MedicamentId
+                        });
+                    table.ForeignKey(
+                        "FK_PatientsMedicaments_Medicaments_MedicamentId",
+                        x => x.MedicamentId,
+                        "Medicaments",
+                        "MedicamentId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        "FK_PatientsMedicaments_Patients_PatientId",
+                        x => x.PatientId,
+                        "Patients",
+                        "PatientId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+            migrationBuilder.CreateTable(
+                "Visitations",
+                table => new
+                {
+                    VisitationId = table.Column<int>(nullable: false)
+                        .Annotation(
+                            "SqlServer:ValueGenerationStrategy",
+                            SqlServerValueGenerationStrategy.IdentityColumn),
+                    Comments = table.Column<string>(nullable: true),
+                    Date = table.Column<DateTime>(nullable: false),
+                    PatientId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Visitations", x => x.VisitationId);
+                    table.ForeignKey(
+                        "FK_Visitations_Patients_PatientId",
+                        x => x.PatientId,
+                        "Patients",
+                        "PatientId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+            migrationBuilder.CreateIndex("IX_Diagnoses_PatientId", "Diagnoses", "PatientId");
+            migrationBuilder.CreateIndex("IX_PatientsMedicaments_MedicamentId", "PatientsMedicaments", "MedicamentId");
+            migrationBuilder.CreateIndex("IX_Visitations_PatientId", "Visitations", "PatientId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable("Diagnoses");
+            migrationBuilder.DropTable("PatientsMedicaments");
+            migrationBuilder.DropTable("Visitations");
+            migrationBuilder.DropTable("Medicaments");
+            migrationBuilder.DropTable("Patients");
+        }
+    }
+}
