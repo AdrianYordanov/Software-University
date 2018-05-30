7e0e13ea7dd57d120a79420b5d743a85ee29df80
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2018-05-30 14:41:03 +0300

    HTTP-Protocol-Exercises
    All the solutions.

---
+++ C#-Web-Development-Basics-May-2018/04. HTTP-Protocol-Exercises/Exercises/Url-Decode/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Net;
+
+public class StartUp
+{
+    private static void Main()
+    {
+        var urlInput = Console.ReadLine();
+        var decodedUrl = WebUtility.UrlDecode(urlInput);
+        Console.WriteLine(decodedUrl);
+    }
+}
---
+++ C#-Web-Development-Basics-May-2018/04. HTTP-Protocol-Exercises/Exercises/Validate-Url/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using System.Net;
+
+public class StartUp
+{
+    private static void Main()
+    {
+        var urlInput = Console.ReadLine();
+        var decodedUrl = WebUtility.UrlDecode(urlInput);
+        try
+        {
+            var parsedUrl = new Uri(decodedUrl);
+            if (string.IsNullOrWhiteSpace(parsedUrl.Scheme) ||
+                string.IsNullOrWhiteSpace(parsedUrl.Host) ||
+                string.IsNullOrWhiteSpace(parsedUrl.LocalPath))
+            {
+                throw new ArgumentException("Invalid URL");
+            }
+
+            var builder = new StringBuilder();
+            builder
+                .AppendLine($"Protocol: {parsedUrl.Scheme}")
+                .AppendLine($"Host: {parsedUrl.Host}")
+                .AppendLine($"Port: {parsedUrl.Port}")
+                .AppendLine($"Path: {parsedUrl.LocalPath}");
+            if (!string.IsNullOrWhiteSpace(parsedUrl.Query))
+            {
+                builder.AppendLine($"Query: {parsedUrl.Query.Substring(1)}");
+            }
+
+            if (!string.IsNullOrWhiteSpace(parsedUrl.Fragment))
+            {
+                builder.AppendLine($"Fragment: {parsedUrl.Fragment.Substring(1)}");
+            }
+
+            Console.WriteLine(builder.ToString().Trim());
+        }
+        catch (Exception ae)
+        {
+            Console.WriteLine(ae.Message);
+        }
+    }
+}
