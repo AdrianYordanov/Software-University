35b03b46e95981c1257f1c08cf1f594bf272a2d1
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2018-06-04 15:10:57 +0300

    01. Even Numbers Thread

---
+++ C#-Web-Development-Basics-May-2018/05. Asynchronous-Processing/Lab/Even-Numbers-Thread/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+public class StartUp
+{
+    private static void Main()
+    {
+        var tokens = Console.ReadLine().Split(' ');
+        var first = int.Parse(tokens[0]);
+        var second = int.Parse(tokens[1]);
+        var thread = new Thread(() => PrintNumbers(first, second));
+        thread.Start();
+        thread.Join();
+        Console.WriteLine("Thread finished work");
+    }
+
+    private static void PrintNumbers(int first, int second)
+    {
+        first = Math.Min(first, second);
+        second = Math.Max(first, second);
+        while (first <= second)
+        {
+            Console.WriteLine(first++);
+        }
+    }
+}
