c335cd36de45de472c19df9b60afad8abc5574c8
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2018-06-14 23:39:19 +0300

    Lab - Asynchronous Processing

---
+++ C#-Web-Development-Basics-May-2018/05. Asynchronous-Processing/Lab/Simple-Web-Server/StartUp.cs
@@ -0,0 +1,42 @@
+﻿namespace Simple_Web_Server
+{
+    using System;
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class StartUp
+    {
+        private static void Main()
+        {
+            var address = IPAddress.Parse("127.0.0.1");
+            var port = 130;
+            var listener = new TcpListener(address, port);
+            listener.Start();
+            Console.WriteLine("Server started.");
+            Console.WriteLine($"Listening to TCP clients at {address}:{port}");
+            ConnectWithTcpListener(listener).Wait();
+        }
+
+        private static async Task ConnectWithTcpListener(TcpListener listener)
+        {
+            while (true)
+            {
+                Console.WriteLine("Waiting for client...");
+                var client = await listener.AcceptTcpClientAsync();
+                Console.WriteLine("Client connected.");
+                var buffer = new byte[1024];
+                client.GetStream().Read(buffer, 0, buffer.Length);
+                var message = Encoding.UTF8.GetString(buffer);
+                Console.WriteLine(message);
+                buffer = Encoding.UTF8.GetBytes("Hello from server!");
+                client.GetStream().Write(buffer, 0, buffer.Length);
+                Console.WriteLine("Closing connection.");
+                client.GetStream().Dispose();
+            }
+
+            // ReSharper disable once FunctionNeverReturns
+        }
+    }
+}
---
+++ C#-Web-Development-Basics-May-2018/05. Asynchronous-Processing/Lab/Slice-File/StartUp.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+public class StartUp
+{
+    private static void Main()
+    {
+        var sourceFile = Console.ReadLine();
+        var destinationPath = Console.ReadLine();
+        var parts = int.Parse(Console.ReadLine());
+        SliceAssync(sourceFile, destinationPath, parts);
+        Console.WriteLine("Anything else?");
+        while (true)
+        {
+            Console.ReadLine();
+        }
+
+        // ReSharper disable once FunctionNeverReturns
+    }
+
+    private static void SliceAssync(string sourceFile, string destinationPath, int parts)
+    {
+        Task.Run(() => Slice(sourceFile, destinationPath, parts));
+    }
+
+    private static void Slice(string sourceFie, string destinationPath, int parts)
+    {
+        if (!Directory.Exists(destinationPath))
+        {
+            Directory.CreateDirectory($"{destinationPath}");
+        }
+
+        using (var source = new FileStream(sourceFie, FileMode.Open))
+        {
+            var fileInfo = new FileInfo(sourceFie);
+            var bufferLengthPart = (source.Length / parts) + 1L;
+            var currentByte = 0L;
+            for (var currentPart = 1; currentPart <= parts; currentPart++)
+            {
+                var outputFilePath = $"{destinationPath}/Part-{currentPart}{fileInfo.Extension}";
+                using (var destination = new FileStream(outputFilePath, FileMode.Create))
+                {
+                    var buffer = new byte[bufferLengthPart];
+                    while (currentByte <= (bufferLengthPart * currentPart))
+                    {
+                        var readBytesCount = source.Read(buffer, 0, buffer.Length);
+                        if (readBytesCount == 0)
+                        {
+                            break;
+                        }
+
+                        destination.Write(buffer, 0, buffer.Length);
+                        currentByte += readBytesCount;
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine("Slice complete.");
+    }
+}
