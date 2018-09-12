b7e8676b2062ee1417beac8170d25311d392f882
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2018-09-12 20:13:04 +0300

    01. Stealer
    Judge - 100/100.

---
+++ C#-OOP-Advanced-July-2017/09. Reflection/Lab/Stealer/Spy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+public class Spy
+{
+    public string StealFieldInfo(string className, params string[] fieldsName)
+    {
+        var classType = Type.GetType(className);
+        var classFields = classType.GetFields(BindingFlags.Instance |
+            BindingFlags.Static |
+            BindingFlags.Public |
+            BindingFlags.NonPublic);
+        var searchedFields = classFields.Where(field => fieldsName.Contains(field.Name));
+        var exampleInstance = Activator.CreateInstance(classType);
+        var result = new StringBuilder();
+        result.AppendLine($"Class under investigation: {classType}");
+        foreach (var field in searchedFields)
+        {
+            result.AppendLine($"{field.Name} = {field.GetValue(exampleInstance)}");
+        }
+
+        return result.ToString();
+    }
+}
---
+++ C#-OOP-Advanced-July-2017/09. Reflection/Lab/Stealer/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace Stealer
+{
+    using System;
+
+    public class StartUp
+    {
+        private static void Main()
+        {
+            var information = new Spy().StealFieldInfo("Hacker", "username", "password");
+            Console.Write(information);
+        }
+    }
+}
