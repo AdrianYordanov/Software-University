8df6541881f01ee06fdda5d199db7db65186cc06
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2018-09-13 22:27:22 +0300

    03. Mission Private Impossible
    Judge - 100/100.

---
+++ C#-OOP-Advanced-July-2017/09. Reflection/Lab/Mission-Private-Impossible/StartUp.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class StartUp
+{
+    private static void Main()
+    {
+        var result = new Spy().RevealPrivateMethods("Hacker");
+        Console.Write(result);
+    }
+}
