fb808243db2613417ef0bd386941dfea1372ccfa
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2018-09-13 15:40:25 +0300

    02. High Quality Mistakes
    Judege - 100/100.

---
+++ C#-OOP-Advanced-July-2017/09. Reflection/Lab/High-Quality-Mistakes/StartUp.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class StartUp
+{
+    private static void Main()
+    {
+        var result = new Spy().AnalyzeAcessModifiers("Hacker");
+        Console.Write(result);
+    }
+}
