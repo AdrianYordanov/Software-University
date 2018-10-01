a3d376522ebd85bc5b0a8d8acedc3e700842d8fe
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2018-10-01 19:31:35 +0300

    First and Second Tasks.
    01. Harvesting Fields
    02. Black Box Integer
    Judge - 100/100 to the both.

---
+++ C#-OOP-Advanced-July-2017/10. Reflection-Exercises/Exercises/Black-Box-Integer/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Reflection;
+
+public class StartUp
+{
+    private static void Main()
+    {
+        var blackBoxClass = typeof(BlackBoxInteger);
+        var blackBoxInstace = (BlackBoxInteger) Activator.CreateInstance(blackBoxClass, true);
+        string input;
+        while ((input = Console.ReadLine()) != "END")
+        {
+            var inputTokens = input.Split(new[] {'_'}, StringSplitOptions.RemoveEmptyEntries);
+            var inputMethod = inputTokens[0];
+            var inputValue = int.Parse(inputTokens[1]);
+            switch (inputMethod)
+            {
+                case "Add":
+                case "Subtract":
+                case "Multiply":
+                case "Divide":
+                case "LeftShift":
+                case "RightShift":
+                    BlackBoxMethodInovkation(blackBoxInstace,
+                        blackBoxClass.GetMethod(inputMethod, BindingFlags.Instance | BindingFlags.NonPublic),
+                        inputValue);
+                    Console.WriteLine(blackBoxClass
+                       .GetField("innerValue", BindingFlags.Instance | BindingFlags.NonPublic)
+                       .GetValue(blackBoxInstace));
+                    break;
+            }
+        }
+    }
+
+    private static void BlackBoxMethodInovkation(BlackBoxInteger instance, MethodInfo method, int value)
+    {
+        method.Invoke(instance, new object[] {value});
+    }
+}
---
+++ C#-OOP-Advanced-July-2017/10. Reflection-Exercises/Exercises/Harvesting-Fields/StartUp.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+public class StartUp
+{
+    private static void Main()
+    {
+        var classType = typeof(HarvestingFields);
+        while (true)
+        {
+            var input = Console.ReadLine();
+
+            if (input.ToLower() == "harvest")
+            {
+                break;
+            }
+
+            IEnumerable<FieldInfo> fields = null;
+            switch (input)
+            {
+                case "private":
+                    fields = classType.GetFields(BindingFlags.NonPublic | BindingFlags.Instance).Where(f => f.IsPrivate);
+                    break;
+                case "protected":
+                    fields = classType.GetFields(BindingFlags.NonPublic | BindingFlags.Instance).Where(f => f.IsFamily);
+                    break;
+                case "public":
+                    fields = classType.GetFields(BindingFlags.Public | BindingFlags.Instance);
+                    break;
+                case "all":
+                    fields = classType.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+                       .Where(f=> f.IsPublic || f.IsPrivate || f.IsFamily);
+                    break;
+            }
+
+
+            if (fields != null)
+            {
+                var fieldsAsListString = fields.ToList()
+                   .Select(f => $"{f.Attributes.ToString().ToLower()} {f.FieldType.Name} {f.Name}")
+                   .ToList();
+                var result = fieldsAsListString.Select(str => str.Replace("family", "protected"));
+                Console.WriteLine(string.Join(Environment.NewLine, result));
+            }
+        }
+    }
+}
