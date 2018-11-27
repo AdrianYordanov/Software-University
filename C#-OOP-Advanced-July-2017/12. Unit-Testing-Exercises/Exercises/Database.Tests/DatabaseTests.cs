61f4e197183c38e99baa463f036761aa0551445d
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2018-11-27 17:45:54 +0200

    01. Database

---
+++ C#-OOP-Advanced-July-2017/12. Unit-Testing-Exercises/Exercises/Database.Tests/DatabaseTests.cs
@@ -0,0 +1,79 @@
+﻿namespace Database.Tests
+{
+    using System;
+    using System.Linq;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class DatabaseTests
+    {
+        private const int ExactlyTheInitialCountOfNumbers = 16;
+        private Database database;
+
+        [SetUp]
+        public void DatabaseSetUp()
+        {
+            this.database = new Database(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16);
+        }
+
+        [Test]
+        public void TestIfConstructorDoesNotTakeSixteenNumbers()
+        {
+            var exUnderSixteen = Assert.Throws<InvalidOperationException>(() =>
+            {
+                new Database(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+            });
+            var exAboveSixteen = Assert.Throws<InvalidOperationException>(() =>
+            {
+                new Database(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17);
+            });
+            Assert.AreEqual(exUnderSixteen.Message, "There are not exactly sixteen numbers.");
+            Assert.AreEqual(exUnderSixteen.Message, exAboveSixteen.Message);
+        }
+
+        [Test]
+        public void TestFetchMethod()
+        {
+            var expectedArray = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+            Assert.IsTrue(expectedArray.SequenceEqual(this.database.Fetch()), "The database is not same as it was initialize.");
+        }
+
+        [Test]
+        public void TestRemoveMethd()
+        {
+            var expectedArray = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+            this.database.Remove();
+            Assert.IsTrue(expectedArray.SequenceEqual(this.database.Fetch()),
+                          "The add method hasn't been added the two numbers correctly.");
+        }
+
+        [Test]
+        public void TestRemoveMethodIfThereIsNoDatabase()
+        {
+            for (var i = 0; i < ExactlyTheInitialCountOfNumbers; i++)
+            {
+                this.database.Remove();
+            }
+
+            var ex = Assert.Throws<InvalidOperationException>(() => this.database.Remove());
+            Assert.AreEqual(ex.Message, "You can't remove number, there aren't any numbers.");
+        }
+
+        [Test]
+        public void TestAddMethod()
+        {
+            var expectedArray = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 18 };
+            this.database.Remove();
+            this.database.Add(18);
+            Assert.IsTrue(expectedArray.SequenceEqual(this.database.Fetch()),
+                          "The add method hasn't been added the two numbers correctly.");
+        }
+
+        [Test]
+        public void TestAddMethodIfDbIsFull()
+        {
+            var ex = Assert.Throws<InvalidOperationException>(() => this.database.Add(18));
+            Assert.AreEqual(ex.Message, "You can't add number, there are already sixteen numbers.");
+        }
+    }
+}
---
+++ C#-OOP-Advanced-July-2017/12. Unit-Testing-Exercises/Exercises/Database/Database.cs
@@ -0,0 +1,58 @@
+﻿namespace Database
+{
+    using System;
+
+    public class Database
+    {
+        private const int MaxArrayLength = 16;
+        private int?[] array;
+        private int currentIndex;
+
+        public Database(params int[] numbers)
+        {
+            if (numbers.Length != MaxArrayLength)
+            {
+                throw new InvalidOperationException("There are not exactly sixteen numbers.");
+            }
+
+            this.array = new int?[MaxArrayLength];
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                this.array[i] = numbers[i];
+            }
+
+            this.currentIndex = MaxArrayLength - 1;
+        }
+
+        public void Add(int number)
+        {
+            if ((this.currentIndex + 1) >= MaxArrayLength)
+            {
+                throw new InvalidOperationException("You can't add number, there are already sixteen numbers.");
+            }
+
+            this.array[++this.currentIndex] = number;
+        }
+
+        public void Remove()
+        {
+            if (this.currentIndex < 0)
+            {
+                throw new InvalidOperationException("You can't remove number, there aren't any numbers.");
+            }
+
+            this.array[this.currentIndex--] = null;
+        }
+
+        public int[] Fetch()
+        {
+            var result = new int[this.currentIndex + 1];
+            for (var i = 0; i <= this.currentIndex; i++)
+            {
+                result[i] = (int) this.array[i];
+            }
+
+            return result;
+        }
+    }
+}
